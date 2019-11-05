b59dbb1737b671a52ff3c88e8deed3e6fbe96a30
koushik K M <koushik@avesorg.com>
2019-11-05 19:29:50 +0530

    merge conflicts resolved

--- your-name/yourName.test.js
+++ your-name/yourName.test.js
@@ -2,10 +2,6 @@ import { yourName } from './yourName';
 
 describe('Write your name in the test', () => {
   test('yourName should return my name', () => {
-<<<<<<< HEAD
     expect(yourName()).toBe('koushik');
-=======
-    expect(yourName()).toBe('Harish');
->>>>>>> e06ae71db7418e6b89998682319b9747560a6e14
   });
 });
