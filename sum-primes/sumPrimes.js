b5f502fa059788d0d1b2f03a276e27e73b520611
yashpandit <yashpandit1995@gmail.com>
2019-11-06 16:18:04 +0530

    Solve sumPrimes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,25 @@
+function isPrime(num) {
+  let i = 2;
+  while (i < num) {
+    if (num % i === 0) return false;
+    i += 1;
+  }
+  return true;
+}
+
+function getPrimeList(number) {
+  let num = number;
+  const primes = [];
+  while (num >= 2) {
+    if (isPrime(num)) primes.push(Number(num));
+    num -= 1;
+  }
+  return primes;
+}
+
 
 function sumPrimes(...args) {
-  return args;
+  return getPrimeList(args).reduce((a, b) => a + b, 0);
 }
 
 export {
