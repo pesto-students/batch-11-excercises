47e3b389c67631815a3276769008175daae1c1c1
yashpandit <yashpandit1995@gmail.com>
2019-11-07 00:27:19 +0530

    Solve sumAll

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,14 @@
+/* eslint-disable prefer-const */
 
-function sumAll(...args) {
-  return args;
+function sumAll(listOfTwoNumbers) {
+  listOfTwoNumbers.sort((currentValue, nextValue) => currentValue > nextValue);
+  let [firstNumber, secondNumber] = listOfTwoNumbers;
+  let sumOfRange = 0;
+  while (firstNumber <= secondNumber) {
+    sumOfRange += firstNumber;
+    firstNumber += 1;
+  }
+  return sumOfRange;
 }
 
 export {
