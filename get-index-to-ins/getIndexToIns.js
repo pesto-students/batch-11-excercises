31920fe77d22ff2403478b5902704d313c2261c3
yashpandit <yashpandit1995@gmail.com>
2019-11-07 00:57:04 +0530

    Solve getIndexToIns

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -3,11 +3,11 @@ function getIndexToIns(numbersArray, valueToBeInserted) {
   let insertionIndex = -1;
 
   if (numbersArray.includes(valueToBeInserted)) return numbersArray.indexOf(valueToBeInserted);
-  numbersArray.sort();
   for (let i = 0; i < numbersArray.length; i += 1) {
-    if (numbersArray[i] > valueToBeInserted && numbersArray.indexOf(numbersArray[i - 1] !== 0)) {
-      insertionIndex = i - 1;
-    } else insertionIndex = i + 1;
+    if ((numbersArray[i] < valueToBeInserted && numbersArray[i + 1] > valueToBeInserted)
+          || numbersArray[i] < valueToBeInserted) {
+      insertionIndex = i + 1;
+    }
   }
   return insertionIndex;
 }
