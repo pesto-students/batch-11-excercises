9b210a8e0bc936a2e3775fb6eeb8b369634e30d2
yashpandit <yashpandit1995@gmail.com>
2019-11-07 00:11:03 +0530

    Solve largeOfFour

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,18 @@
-
-function largeOfFour(...args) {
-  return args;
+function getMaxFromList(list) {
+  let maximumNumber = -Infinity;
+  list.forEach((number) => {
+    if (number > maximumNumber) maximumNumber = number;
+  });
+  return maximumNumber;
+}
+function largeOfFour(lists) {
+  const listOfMaxValues = [];
+  const [listOne, listTwo, listThree, listFour] = lists;
+  listOfMaxValues.push(getMaxFromList(listOne));
+  listOfMaxValues.push(getMaxFromList(listTwo));
+  listOfMaxValues.push(getMaxFromList(listThree));
+  listOfMaxValues.push(getMaxFromList(listFour));
+  return listOfMaxValues;
 }
 
 export {
