74b6dfb18e232847de483dcac25fd12b2d7aece2
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 16:21:42 +0530

    Fix test for rectangle-diagonal
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-restricted-properties */
 
-function rectangleDiagonal(...args) {
-  return args;
+function rectangleDiagonal(...sides) {
+  const [length, width] = sides;
+  const squareOfLength = Math.pow(length, 2);
+  const squareOfWidth = Math.pow(width ** 2);
+  const lengthOfDiagonal = Math.sqrt(squareOfLength + squareOfWidth);
+  return lengthOfDiagonal;
 }
 
 export {
