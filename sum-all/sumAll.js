03079af1272252533b77e760a84eb8bb3e58d1b6
Sourav Das <souravdasslg95@gmail.com>
2019-11-06 22:48:48 +0530

    Sumall Problem Is Completed

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,11 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(listOfNumber) {
+  listOfNumber.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
+  const [lowerNumber, higherNumber] = listOfNumber;
+  const sumOfNumbersFromLowerNumberToHigherNumber = ((higherNumber * (higherNumber + 1)) / 2)
+                                                    - ((lowerNumber - 1) * (lowerNumber / 2));
+  return sumOfNumbersFromLowerNumberToHigherNumber;
 }
-
 export {
   sumAll,
 };
