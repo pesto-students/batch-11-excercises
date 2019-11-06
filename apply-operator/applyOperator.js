3ab056316fa2456a65b9db3fb9caf8b73fb7fc98
Sourav Das <souravdasslg95@gmail.com>
2019-11-06 16:56:32 +0530

    WIP Apply Operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,8 +1,62 @@
-
 function applyOperator(...args) {
-  return args;
+  let result = 0;
+  const passedArgs = [].concat(args);
+  passedArgs.splice(0, 1);
+  if (args[0] !== '+' && args[0] !== '-' && args[0] !== '/' && args[0] !== '%' && args[0] !== '%') {
+    result = 0;
+  } else {
+    throw Error('Operator Is Not Specified');
+  }
+  switch (args[0]) {
+    case '+':
+      passedArgs.map((el) => {
+        result += el;
+        return null;
+      });
+      break;
+    case '-':
+      passedArgs.map((el) => {
+        result -= el;
+        return 0;
+      });
+      break;
+    case '*':
+      passedArgs.map((el, index) => {
+        if (index === 0) {
+          result = el;
+        } else {
+          result *= el;
+        }
+        return 0;
+      });
+      break;
+    case '/':
+      passedArgs.map((el, index) => {
+        if (index === 0) {
+          result = el;
+        } else {
+          result /= el;
+        }
+        return 0;
+      });
+      result = parseFloat(result.toFixed(4));
+      break;
+    case '%':
+      passedArgs.map((el, index) => {
+        if (index === 0) {
+          result = el;
+        } else {
+          result /= el;
+        }
+        return 0;
+      });
+      result = Math.ceil(result);
+      break;
+    default:
+      throw new Error('No Operator');
+  }
+  return result;
 }
-
 export {
   applyOperator,
 };
