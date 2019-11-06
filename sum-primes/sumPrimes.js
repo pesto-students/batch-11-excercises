4e776097dc1126df6e02b0b93f83cd67b7fdea94
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-06 19:21:10 +0530

    sum primes not yet completed

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,21 @@
 
 function sumPrimes(...args) {
-  return args;
+  var result = 0;
+  for (var i=2;i<args[0];i++){
+    if(isPrime(i)) {
+      result = result + i;
+    }
+  }
+  return result;
+}
+
+function isPrime(value){
+
+  for(var i=2;i<value/2; i++){
+    if(value%i === 0 && value !== i){
+      return false;
+    }
+  }
 }
 
 export {
