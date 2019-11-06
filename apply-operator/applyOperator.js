b0f5f6648e2ccf7dacee0861dfa1156a76b8b544
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-06 16:10:43 +0530

    Completed apply operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,50 @@
-
 function applyOperator(...args) {
-  return args;
+
+  const operator = args[0];
+  args.shift();
+  let result = 0;
+  if (operator === undefined) {
+    throw new Error('Error');
+  }
+  if (args.length !== 0) {
+    switch (operator) {
+      case '+':
+        result = 0;
+        for (let i = 0; i < args.length; i++) {
+          result = result + args[i];
+        }
+        break;
+      case '-':
+        result = 0;
+        for (let i = 0; i < args.length; i++) {
+          result = result - args[i];
+        }
+        break;
+      case '*':
+        result = 1;
+        for (let i = 0; i < args.length; i++) {
+          result = result * args[i];
+        }
+        break;
+      case '/':
+        result = 1;
+        for (let i = 0; i < args.length; i++) {
+          result = result / args[i];
+        }
+        result = parseFloat(result.toFixed(4))
+        break;
+      case '%':
+        result = args[0];
+        for (let i = 1; i < args.length; i++) {
+          result = result % args[i];
+        }
+        result = parseFloat(result.toFixed(4))
+        break;
+      default:
+        return result;
+    }
+  }
+  return result;
 }
 
 export {
