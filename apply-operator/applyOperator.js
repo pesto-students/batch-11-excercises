67b4a02838fc4a358ac8eb887c0501e291324749
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 15:34:37 +0530

    Fix test for apply-operator
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,25 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator(operator, ...operands) {
+  if (!operator) throw new Error("Operator not specified. Possible values '+', '-', '*', '/', '%'");
+  if (!operands) return 0;
+
+  switch (operator) {
+    case '+':
+      return operands.reduce((result, element) => result + element, 0);
+    case '-':
+      return operands.reduce((result, element) => result - element, 0);
+    case '*':
+      return operands.reduce((result, element) => result * element, 1);
+    case '/': {
+      const highDecimalPlaces = operands.reduce((result, element) => result / element, 1);
+      const precisionOfTwo = Number.parseFloat(Number.parseFloat(highDecimalPlaces).toPrecision(2));
+      return precisionOfTwo;
+    }
+    case '%':
+      return operands.reduce((result, element) => result % element);
+    default:
+      throw new Error("Invalid operator specified. Possible values '+', '-', '*', '/', '%'");
+  }
 }
 
 export {
