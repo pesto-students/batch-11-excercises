8802215ec0aa0c5bc7d0a08b827c8f977d3ae8c0
lordtien <nitinpawarme@gmail.com>
2019-11-06 20:08:20 +0530

    Solve large-of-four

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,19 @@
+function findLargestNumberInArray(numberArray) {
+  let largestNumber = numberArray[0];
+  for (let i = 0; i < numberArray.length; i += 1) {
+    if (numberArray[i] > largestNumber) {
+      largestNumber = numberArray[i];
+    }
+  }
+  return largestNumber;
+}
 
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(nestedArrayOfNumbers) {
+  const arrayOfLargestNumbers = [];
+  for (let i = 0; i < nestedArrayOfNumbers.length; i += 1) {
+    arrayOfLargestNumbers.push(findLargestNumberInArray(nestedArrayOfNumbers[i]));
+  }
+  return arrayOfLargestNumbers;
 }
 
 export {
