71db98e089f96c7b9166f8940facfd48912a8ea3
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 16:09:56 +0530

    Fix test for is-triangle
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,9 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(...lengthOfSides) {
+  // A triangle is valid if sum of its two sides is greater than the third side.
+  const [smallestSide, mediumSide, largestSide] = lengthOfSides.sort();
+
+  return (smallestSide + mediumSide > largestSide);
 }
 
 export {
