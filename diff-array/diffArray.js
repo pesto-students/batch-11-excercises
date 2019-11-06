0e336497047635e8cf6a3a0fbfa7f477d3dcef16
Harish Rana <r.shivi@yahoo.com>
2019-11-06 23:02:42 +0530

    Solve diff array problem

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,8 @@
 
-function diffArray(...args) {
-  return args;
+function diffArray(array1, array2) {
+  const array1Filtered = array1.filter((item) => !array2.includes(item));
+  const array2Filtered = array2.filter((item) => !array1.includes(item));
+  return array1Filtered.concat(array2Filtered);
 }
 
 export {
