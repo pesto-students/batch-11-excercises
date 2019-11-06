0e02d9ae14fa0ca990a5e7e8cd39c78eccd7df71
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 19:14:15 +0530

    Fix test for binary-agent
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,17 @@
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binaryString) {
+  // One liner
+  // return binaryString.split(' ').map(x => String.fromCharCode(Number.parseInt(x, 2))).join('');
+
+  const binaryLetters = binaryString.split(' ');
+  function convertBinaryToAscii(binaryRepresentation) {
+    const integerRepresentation = Number.parseInt(binaryRepresentation, 2);
+    const asciiCharacter = String.fromCharCode(integerRepresentation);
+    return asciiCharacter;
+  }
+  const asciiLetters = binaryLetters.map(convertBinaryToAscii);
+  const asciiString = asciiLetters.join('');
+  return asciiString;
 }
 
 export {
