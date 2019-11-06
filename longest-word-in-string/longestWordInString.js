627fdf493f9883274c894adfa7be475eaac35d56
lordtien <nitinpawarme@gmail.com>
2019-11-06 20:13:53 +0530

    Solve longest-word-in-string

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,14 @@
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(sentence) {
+  let largestWordLength = 0;
+  const wordArray = sentence.split(' ');
+  for (let i = 0; i < wordArray.length; i += 1) {
+    const currentWordLength = wordArray[i].length;
+    if (currentWordLength > largestWordLength) {
+      largestWordLength = currentWordLength;
+    }
+  }
+  return largestWordLength;
 }
 
 export {
