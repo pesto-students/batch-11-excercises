0c8a147309e453569d8c5ac4dafd8d866ce4006a
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-06 16:45:09 +0530

    Completed is odd

--- is-odd/isOdd.js
+++ is-odd/isOdd.js
@@ -1,6 +1,12 @@
 
 function isOdd(...args) {
-  return args;
+  var value = args[0];
+  var divideby2 = parseInt(args[0]/2)
+  if(divideby2 * 2 !== value){
+    return true
+  }else {
+    return false
+  }
 }
 
 export {
