c73bc1b2969350c7ea9d8a6736f675e9e91bf382
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-07 01:06:01 +0530

    sumFibs solution

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,19 @@
+function sumFibs(num) {
+  if (num < 0) return -1;
+  if (num === 0 || num === 1) return 1;
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
 
-function sumFibs(...args) {
-  return args;
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
 }
 
 export {
