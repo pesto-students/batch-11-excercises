83738a501b28af337ad97bac4faedca294049e03
lordtien <nitinpawarme@gmail.com>
2019-11-06 20:01:14 +0530

    Solve get-index-to-ins

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,15 @@
+function sortCondition(a, b) {
+  return a - b;
+}
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(numberArray, valueToInsert) {
+  const sortedArray = numberArray.sort(sortCondition);
+  for (let i = 0; i < sortedArray.length; i += 1) {
+    if (sortedArray[i] >= valueToInsert) {
+      return i;
+    }
+  }
+  return sortedArray.length;
 }
 
 export {
