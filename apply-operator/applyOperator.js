27302877ab7daee985ef92586c13495b44ed6a6b
yashpandit <yashpandit1995@gmail.com>
2019-11-06 16:58:27 +0530

    Solve applyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,20 @@
 
 function applyOperator(...args) {
-  return args;
+  let ans;
+  const [operator, ...operands] = args;
+  if (operator === undefined) {
+    throw new Error();
+  }
+  if (operands.length === 0) return 0;
+  if (operands.length === 1 && operator !== '-') return operands[0];
+
+  if (operator === '*') ans = operands.reduce((a, b) => a * b, 1);
+  if (operator === '/') ans = Number.parseFloat(operands.reduce((a, b) => a / b, 1).toFixed(4));
+  if (operator === '+') ans = operands.reduce((a, b) => a + b, 0);
+  if (operator === '-') ans = operands.reduce((a, b) => a - b, 0);
+  if (operator === '%') ans = operands.reduce((a, b) => a % b, 1);
+
+  return ans;
 }
 
 export {
--- counter/counter.js
+++ counter/counter.js
@@ -1,4 +1,3 @@
-
 function counter(...args) {
   return args;
 }
