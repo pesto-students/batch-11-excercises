af7d97860c44aa0a2a014d4a350984e03b684e89
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-07 00:14:10 +0530

    limitFunctionCall solution

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,14 @@
+function limitFunctionCallCount(fncallback, limit) {
+  let fncount = limit;
 
-function limitFunctionCallCount(...args) {
-  return args;
+  function invokeTheFunc(...args) {
+    if (fncount) {
+      fncount -= 1;
+      return fncallback(...args);
+    }
+    return null;
+  }
+  return invokeTheFunc;
 }
 
 export {
