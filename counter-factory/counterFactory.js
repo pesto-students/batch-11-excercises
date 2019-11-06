f36a5499b65b35c599ea6ec258bcff60c19ad556
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 15:59:55 +0530

    Fix test for counter-factory
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,18 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  const counterFactoryObject = {
+    count: 0,
+    increment() {
+      this.count += 1;
+      return this.count;
+    },
+    decrement() {
+      this.count -= 1;
+      return this.count;
+    },
+  };
+
+  return counterFactoryObject;
 }
 
 export {
