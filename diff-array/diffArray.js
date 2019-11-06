77d9d019937f60ef5b2b7ba85c9f7f9798dadedc
yashpandit <yashpandit1995@gmail.com>
2019-11-06 19:45:36 +0530

    Solve diffArray

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,8 +1,15 @@
 
-function diffArray(...args) {
-  return args;
+function getArrayDifference(firstArrayToCompare, secondArrayToCompare) {
+  const arrayWithDifference = [];
+  firstArrayToCompare.forEach((arrayItem) => {
+    if (!secondArrayToCompare.includes(arrayItem)) arrayWithDifference.push(arrayItem);
+  });
+  secondArrayToCompare.forEach((arrayItem) => {
+    if (!firstArrayToCompare.includes(arrayItem)) arrayWithDifference.push(arrayItem);
+  });
+  return arrayWithDifference;
 }
 
 export {
-  diffArray,
+  getArrayDifference,
 };
