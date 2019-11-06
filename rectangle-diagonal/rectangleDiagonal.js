7ed62ef5c7f6a76648e23ef3afbaa38ec78cb129
yashpandit <yashpandit1995@gmail.com>
2019-11-06 15:42:14 +0530

    Solve rectangleDiagonal

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,6 +1,9 @@
 
 function rectangleDiagonal(...args) {
-  return args;
+  const [a, b] = args;
+  let diagonal = (a ** 2) + (b ** 2);
+  diagonal = Math.sqrt(diagonal);
+  return diagonal;
 }
 
 export {
