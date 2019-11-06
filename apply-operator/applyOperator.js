a70a10003406b77e12e7dd4950902f29859c118b
yashpandit <yashpandit1995@gmail.com>
2019-11-06 22:11:21 +0530

    Modify applyOperator with appropriate names and error message

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,20 +1,20 @@
 
 function applyOperator(...args) {
-  let ans;
+  let answer;
   const [operator, ...operands] = args;
   if (operator === undefined) {
-    throw new Error();
+    throw new Error('Please specify one of the mentioned operators : +, -, *, /, %');
   }
   if (operands.length === 0) return 0;
   if (operands.length === 1 && operator !== '-') return operands[0];
 
-  if (operator === '*') ans = operands.reduce((a, b) => a * b, 1);
-  if (operator === '/') ans = Number.parseFloat(operands.reduce((a, b) => a / b, 1).toFixed(4));
-  if (operator === '+') ans = operands.reduce((a, b) => a + b, 0);
-  if (operator === '-') ans = operands.reduce((a, b) => a - b, 0);
-  if (operator === '%') ans = operands.reduce((a, b) => a % b, 1);
+  if (operator === '*') answer = operands.reduce((a, b) => a * b, 1);
+  if (operator === '/') answer = Number.parseFloat(operands.reduce((a, b) => a / b, 1).toFixed(4));
+  if (operator === '+') answer = operands.reduce((a, b) => a + b, 0);
+  if (operator === '-') answer = operands.reduce((a, b) => a - b, 0);
+  if (operator === '%') answer = operands.reduce((a, b) => a % b, 1);
 
-  return ans;
+  return answer;
 }
 
 export {
