5841c071c0c4610236624647d14a5c7779b1f6f5
yashpandit <yashpandit1995@gmail.com>
2019-11-06 15:51:23 +0530

    Solve sumFibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,22 @@
 
 function sumFibs(...args) {
-  return args;
+  let i = 2;
+  let sum = 0;
+
+  const fibo = [];
+  fibo.push(0);
+  fibo.push(1);
+
+  while (i <= args) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+    i += 1;
+  }
+
+  fibo.forEach((ele) => {
+    if (ele % 2 !== 0 && ele < args) sum += ele;
+  });
+
+  return sum;
 }
 
 export {
