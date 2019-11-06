ca03fb2bcc92831cc60989956c5d767fa8de0a26
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 16:41:45 +0530

    Fix test for three-sum
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,6 +1,31 @@
 
-function threeSum(...args) {
-  return args;
+function threeSum(numberArray, targetSum) {
+  if (numberArray.length < 3) {
+    throw new Error('Given array should contain at least 3 numbers');
+  }
+
+  if (typeof targetSum !== 'number') {
+    throw new Error('Target sum should be a number');
+  }
+
+  function loopThroughArray() {
+    for (const [firstLoopIndex, firstLoopValue] of numberArray.entries()) {
+      for (const [secondLoopIndex, secondLoopValue] of numberArray.entries()) {
+        for (const [thirdLoopIndex, thirdLoopValue] of numberArray.entries()) {
+          if ((firstLoopIndex !== secondLoopIndex)
+            && (secondLoopIndex !== thirdLoopIndex)
+            && (firstLoopIndex !== thirdLoopIndex)) {
+            if ((firstLoopValue + secondLoopValue + thirdLoopValue) === targetSum) {
+              return [firstLoopValue, secondLoopValue, thirdLoopValue];
+            }
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  return loopThroughArray() || null;
 }
 
 export {
