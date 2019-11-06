db72c02b4d1fc91a437366db5c683f8c3f0ab13a
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 16:22:25 +0530

    Fix typo for rectangle-diagonal
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -3,7 +3,7 @@
 function rectangleDiagonal(...sides) {
   const [length, width] = sides;
   const squareOfLength = Math.pow(length, 2);
-  const squareOfWidth = Math.pow(width ** 2);
+  const squareOfWidth = Math.pow(width, 2);
   const lengthOfDiagonal = Math.sqrt(squareOfLength + squareOfWidth);
   return lengthOfDiagonal;
 }
