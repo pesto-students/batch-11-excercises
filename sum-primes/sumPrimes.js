a10fbfacf1b9c10b1f4f1728f7a2436cbd812af9
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 16:33:43 +0530

    Fix test for sum-primes
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,24 @@
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(integer) {
+  function isPrime(maybePrime) {
+    const divisors = [];
+    for (let i = 1; i <= maybePrime; i += 1) {
+      if (maybePrime % i === 0) {
+        divisors.push(i);
+      }
+    }
+    return divisors.length === 2;
+  }
+
+  const listOfPrimesTillGivenInteger = [];
+  for (let i = 1; i <= integer; i += 1) {
+    if (isPrime(i)) {
+      listOfPrimesTillGivenInteger.push(i);
+    }
+  }
+
+  const sum = listOfPrimesTillGivenInteger.reduce((result, current) => result + current);
+  return sum;
 }
 
 export {
