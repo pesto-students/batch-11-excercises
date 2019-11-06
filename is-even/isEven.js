f785aaec476d0172ee4ba1e7318813be6f20e337
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 16:04:29 +0530

    Fix test for is-even
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,6 +1,14 @@
 
-function isEven(...args) {
-  return args;
+function isEven(integer) {
+  const binaryString = integer.toString(2);
+  const lastBit = binaryString[binaryString.length - 1];
+
+  // If bitwise AND between last bit and 1 is equal to 1, then it is odd, otherwise even
+  // eslint-disable-next-line no-bitwise
+  if (lastBit & 1) {
+    return false;
+  }
+  return true;
 }
 
 export {
