c111eac4a8d1faa57be3375b128b1f5743c23d65
yashpandit <yashpandit1995@gmail.com>
2019-11-07 01:13:22 +0530

    Commit incomplete changes

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-console */
+/* eslint-disable quotes */
 
-function bouncer(...args) {
-  return args;
+function bouncer(array) {
+  const listOfFalsyValues = [false, null, undefined, 0, NaN, '', "", ``];
+  for (let i = 0; i < array.length; i += 1) {
+    if (listOfFalsyValues.includes(array[i])) {
+      array.splice(i, 1);
+    }
+  }
+  return array;
 }
 
 export {
