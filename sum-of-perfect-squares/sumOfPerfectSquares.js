e1d68d5ef448e6bde0a4b3fc0e7abd58b62e2bcc
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-06 16:32:19 +0530

    Fix test for sum-of-perfect-squares
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,7 +1,52 @@
 
-function sumOfPerfectSquares(...args) {
-  return args;
+function sumOfPerfectSquares(integer) {
+  const listOfPerfectSquares = [];
+
+  let loopItem = integer;
+  while (loopItem >= 1) {
+    if ((loopItem ** 2) <= integer) {
+      if ((loopItem ** 2) === integer) {
+        return 1;
+      }
+      listOfPerfectSquares.push((loopItem ** 2));
+    }
+    loopItem -= 1;
+  }
+
+  const listOfAnswers = [];
+  function generateSumTillInteger(element, index, array) {
+    const currentAnswer = [];
+    let currentElement = element;
+    let currentSum = 0;
+    let currentDifference = 0;
+
+    do {
+      if (currentDifference) {
+        let currentMaxLessThanDifference = 0;
+        for (const value of array) {
+          if (value > currentMaxLessThanDifference
+            && value <= currentDifference
+            && value <= element) {
+            currentMaxLessThanDifference = value;
+          }
+        }
+        currentElement = currentMaxLessThanDifference;
+      }
+
+      currentSum += currentElement;
+      currentDifference = integer - currentSum;
+      currentAnswer.push(currentElement);
+    } while (currentDifference > 0);
+    listOfAnswers.push(currentAnswer);
+  }
+
+  listOfPerfectSquares.forEach(generateSumTillInteger);
+  const lenOfAnswersArray = [];
+  listOfAnswers.every((x) => { lenOfAnswersArray.push(x.length); return true; });
+  const smallestAnswerArray = lenOfAnswersArray.sort((a, b) => a - b)[0];
+  return smallestAnswerArray;
 }
+
 
 export {
   sumOfPerfectSquares,
