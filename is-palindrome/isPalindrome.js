2c309527d1de149e101f211dad18685ea65f7c2e
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-06 23:37:53 +0530

    isPalindrome solution

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,9 @@
-
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(str) {
+// Regex To remove unwanted characters
+  const regex = /[\W_]/g;
+  const cleanStr = String(str).toLowerCase().replace(regex, '');
+  const reversecleanStr = cleanStr.split('').reverse().join('');
+  return reversecleanStr === cleanStr;
 }
 
 export {
