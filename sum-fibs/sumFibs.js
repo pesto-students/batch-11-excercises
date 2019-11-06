fed3a2b79c125839f57c29de724f8b748caae59c
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-07 02:59:06 +0530

    Solved sumFibs with all test cases

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,23 @@
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(number) {
+  let firstFibonacciSequenceNumber = 1;
+  let secondFibonacciSequenceNumber = 1;
+  let sumOfOddFibonaccis = 0;
+  sumOfOddFibonaccis += firstFibonacciSequenceNumber + secondFibonacciSequenceNumber;
+
+  for (let i = 2; i < number; i++) {
+    let latestFibonacciSequencenumber = firstFibonacciSequenceNumber + secondFibonacciSequenceNumber;
+    firstFibonacciSequenceNumber = secondFibonacciSequenceNumber;
+    secondFibonacciSequenceNumber = latestFibonacciSequencenumber;
+    if (isOdd(latestFibonacciSequencenumber) && latestFibonacciSequencenumber <= number) {
+      sumOfOddFibonaccis += latestFibonacciSequencenumber;
+    }
+  }
+  return sumOfOddFibonaccis;
+}
+
+function isOdd(number) {
+  return number % 2 !== 0;
 }
 
 export {
