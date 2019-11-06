fcfb76e150c184800eb6a26d54cfc4d3116f5244
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-06 21:35:58 +0530

    CounterFactory solved

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,16 @@
-
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  const counterBox = {
+    counts: 0,
+    increment() {
+      this.counts += 1;
+      return this.counts;
+    },
+    decrement() {
+      this.counts -= 1;
+      return this.counts;
+    },
+  };
+  return counterBox;
 }
 
 export {
