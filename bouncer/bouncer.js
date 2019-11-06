d344ab40dac00b723748df30180c87bb69bf99ad
yashpandit <yashpandit1995@gmail.com>
2019-11-07 01:18:34 +0530

    Solve bouncer

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,14 +1,6 @@
-/* eslint-disable no-console */
-/* eslint-disable quotes */
 
-function bouncer(array) {
-  const listOfFalsyValues = [false, null, undefined, 0, NaN, '', "", ``];
-  for (let i = 0; i < array.length; i += 1) {
-    if (listOfFalsyValues.includes(array[i])) {
-      array.splice(i, 1);
-    }
-  }
-  return array;
+function bouncer(randomArray) {
+  return randomArray.filter(Boolean);
 }
 
 export {
