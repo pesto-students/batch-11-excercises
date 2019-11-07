6e066ac8337ad296099b74c380d13aeed412811c
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-07 09:46:54 +0530

    threesum solution

--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,6 +1,22 @@
-
-function threeSum(...args) {
-  return args;
+function threeSum(arr, target) {
+  const result = [];
+  const len = arr.length;
+  if (len < 3 || typeof target !== 'number') {
+    throw new Error();
+  }
+  for (let i = 0; i < len; i += 1) {
+    for (let j = i + 1; j < len; j += 1) {
+      for (let k = j + 1; k < len; k += 1) {
+        if (arr[i] + arr[j] + arr[k] === target) {
+          result.push(arr[i]);
+          result.push(arr[j]);
+          result.push(arr[k]);
+          return result;
+        }
+      }
+    }
+  }
+  return null;
 }
 
 export {
