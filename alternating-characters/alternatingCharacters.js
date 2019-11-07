bc9d38f9165fc42cdfe11f0e19a9603527008dcd
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-07 20:02:29 +0530

    Fix test for alternating-characters
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,19 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(arrayOfString) {
+  function countAlterationOfCharacters(unsanitizedString) {
+    function predicate(element, index, array) {
+      return element !== array[index + 1];
+    }
+    const sanitizedString = unsanitizedString
+      .split('')
+      .filter(predicate)
+      .join('');
+
+    const count = unsanitizedString.length - sanitizedString.length;
+    return count;
+  }
+
+  return arrayOfString.map(countAlterationOfCharacters);
 }
 
 export {
