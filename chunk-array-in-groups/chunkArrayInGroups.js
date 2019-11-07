dc8949708787e5c6dc97c6cdaea418c913af5853
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-08 02:20:42 +0530

    Solved chunkArrayInGroups with all test cases

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,15 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(elements, groupSize) {
+  let chunks = [];
+  let beginChunk = 0;
+  let endChunk = groupSize;
+  while (endChunk < elements.length) {
+    chunks.push(elements.slice(beginChunk, endChunk));
+    beginChunk += groupSize;
+    endChunk += groupSize;
+  }
+  chunks.push(elements.slice(beginChunk));
+  return chunks;
 }
 
 export {
