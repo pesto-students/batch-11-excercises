54ea707412519815c96ac8b0cf0f333510430729
Lordtien <nitinpawarme@gmail.com>
2019-11-07 19:04:05 +0530

    Solve alternating-characters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,22 @@
+function countDeletionsRequired(stringToCheck) {
+  let currentCharacter = stringToCheck[0];
+  let deletionsRequired = 0;
+  for (let i = 1; i < stringToCheck.length; i += 1) {
+    if (stringToCheck[i] === currentCharacter) {
+      deletionsRequired += 1;
+    } else {
+      currentCharacter = stringToCheck[i];
+    }
+  }
+  return deletionsRequired;
+}
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  const numberOfDeletions = [];
+  for (let i = 0; i < strings.length; i += 1) {
+    numberOfDeletions.push(countDeletionsRequired(strings[i]));
+  }
+  return numberOfDeletions;
 }
 
 export {
