0dcd0f29359311758a23eca53f697bb58f6ead06
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-07 17:01:08 +0530

    Breaking array based on chunk size

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,14 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(completeArray,chunkSize) {
+
+  let chunkGroupArray = [];
+
+  while(completeArray.length !== 0){
+    chunkGroupArray.push(completeArray.splice(0,chunkSize));
+  }
+
+  return chunkGroupArray;
+
 }
 
 export {
