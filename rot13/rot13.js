c07efd9df03236ccfe7a55f4ae17e68f159a747e
yashpandit <yashpandit1995@gmail.com>
2019-11-07 10:25:03 +0530

    WIP rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,31 @@
+/* eslint-disable no-continue */
+function getDecryptedWord(encryptedWord) {
+  let decryptedWord = '';
+  const listOfAlphabetsFromWord = encryptedWord.split('');
+  const ALPHABETS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  for (let i = 0; i < listOfAlphabetsFromWord.length; i += 1) {
+    if (!ALPHABETS.includes(listOfAlphabetsFromWord[i])) continue;
+    else {
+      let asciiValueOfCharacter = listOfAlphabetsFromWord.charCodeAt(i);
+      if (asciiValueOfCharacter + 13 > 90) {
+        asciiValueOfCharacter = asciiValueOfCharacter + 13 - 90 + 65;
+        decryptedWord += String.fromCharCode(asciiValueOfCharacter);
+      } else {
+        decryptedWord += String.fromCharCode(asciiValueOfCharacter);
+      }
+    }
+  }
+  return decryptedWord;
+}
 
-function rot13(...args) {
-  return args;
+function rot13(encryptedText) {
+  const encryptedTextList = encryptedText.split(' ');
+  const decryptedTextList = [];
+  encryptedTextList.forEach((word) => {
+    decryptedTextList.push(getDecryptedWord(word));
+  });
+  const decryptedText = decryptedTextList.join(' ');
+  return decryptedText;
 }
 
 export {
