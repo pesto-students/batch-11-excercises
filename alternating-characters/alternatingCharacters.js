8e1f3e89a78d335256ed1e8f035a45cc8eec0579
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-07 19:29:18 +0530

    Solved alternatingCharacters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,15 @@
-
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  const deletionCountArray = [];
+  strings.forEach((string) => {
+    let numberOfDeletions = 0;
+    for (let i = 1; i < string.length; i += 1) {
+      if (string[i] === string[i - 1]) {
+        numberOfDeletions += 1;
+      }
+    }
+    deletionCountArray.push(numberOfDeletions);
+  });
+  return deletionCountArray;
 }
 
 export {
