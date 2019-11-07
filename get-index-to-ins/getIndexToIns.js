dd4130f781c45e45b64d376399903175b1b64b01
Harish Rana <r.shivi@yahoo.com>
2019-11-07 11:54:31 +0530

    Rename variable a and b

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -3,7 +3,7 @@ function getIndexToIns(numberArray, numberToBeInserted) {
   if (!numberArray) {
     return -1;
   }
-  numberArray.sort((a, b) => a - b);
+  numberArray.sort((number1, number2) => number1 - number2);
   let index = 0;
   while (numberArray[index] < numberToBeInserted) {
     // eslint-disable-next-line no-plusplus
