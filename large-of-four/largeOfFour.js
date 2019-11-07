e9fbb8ccff6fe5acc1475664e757e3763243a727
Basant Soni <basantech89@gmail.com>
2019-11-07 06:24:01 +0530

    Fix largeOfFour test

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,11 @@
 
 function largeOfFour(...args) {
-  return args;
+  const numArrays = args[0];
+
+  const largestOfFour = numArrays.map((subArray) => {
+    return Math.max(...subArray);
+  });
+  return largestOfFour;
 }
 
 export {
