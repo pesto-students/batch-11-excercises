a88eca773c3adc74c84dd9e23c9609d14ef65979
Basant Soni <basantech89@gmail.com>
2019-11-07 21:11:42 +0530

    Fix is-triangle test

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,7 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(...triangleSides) {
+  const [small, medium, large] = triangleSides.sort((a, b) => a - b);
+  return small + medium >= large;
 }
 
 export {
