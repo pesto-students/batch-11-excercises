f5f76f68dfe1dc1ac689ff18dc68363eca3fa98b
lordtien <nitinpawarme@gmail.com>
2019-11-07 15:56:37 +0530

    Solve find-element

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,11 @@
 
-function findElement(...args) {
-  return args;
+function findElement(numbers, truthTest) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (truthTest(numbers[i])) {
+      return numbers[i];
+    }
+  }
+  return undefined;
 }
 
 export {
