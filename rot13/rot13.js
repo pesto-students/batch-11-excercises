d76ecc31e54e55f126000f6a9b19c439b35dcce0
Basant Soni <basantech89@gmail.com>
2019-11-07 07:40:29 +0530

    Fix telephoneCheck test

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,17 @@
 
 function rot13(...args) {
-  return args;
+  const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const encodedString = args[0];
+  let decodedString = '';
+  const nonAlphabeticPattern = /[_ !,.]/;
+  for (let i = 0; i < encodedString.length; i++) {
+    const encodedChar = encodedString[i];
+    if (nonAlphabeticPattern.test(encodedChar)) continue;
+    let shiftedIndex = alphabets.indexOf(encodedChar) + 13;
+    if (shiftedIndex > 26) shiftedIndex = shiftedIndex % 26;
+    decodedString += alphabets[shiftedIndex];
+  }
+  return decodedString;
 }
 
 export {
--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,17 @@
 
 function telephoneCheck(...args) {
-  return args;
+  const telephoneNum = args[0];
+  const areaCodePattern = /\(\d{3}\)/gi;
+  const specialCharPattern = /[*?&#!a-zA-Z]/gi;
+  const countryCodePattern = /\d /gi;
+  if (!areaCodePattern.test(telephoneNum)) {
+    return false;
+  } else if (specialCharPattern.test(telephoneNum)) {
+    return false;
+  } else if (countryCodePattern.test(telephoneNum)) {
+    if (telephoneNum[0] !== '1') return false;
+  }
+  return true;
 }
 
 export {
