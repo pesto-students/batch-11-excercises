7d0fe6a3b1c7eaa2f8b1abb3311df295d0e19b30
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-07 19:36:38 +0530

    Complet Day 4 segment 2 curry solution

--- curry/curry.js
+++ curry/curry.js
@@ -1,8 +1,14 @@
-
-function curry(...args) {
-  return args;
+function curry(func) {
+  return function curriedFunction(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curriedFunction.apply(this, args.concat(args2));
+      }
+    }
+  };
 }
-
 export {
   curry,
 };
