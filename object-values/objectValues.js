bb397e14263d22164d5602718cdcb8c3160f68bf
hitesh <hiteshchauhan.669@gmail.com>
2019-11-07 16:33:38 +0530

    Solved objectValues Problem

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,18 +1,11 @@
-
 function objectValues(object) {
   const values = [];
-  const invlaidValues = [null, undefined, 0, ""];
+
   for (let key in object) {
-    const property = object[key];
-    if (invlaidProperties.includes(property)) {
-      properties.push(key.toString());
-    } else {
-      properties.push(property.toString());
-    }
+    let value = object[key];
+    values.push(value);
   }
-  return properties;
+  return values;
 }
 
-export {
-  objectValues,
-};
+export { objectValues };
