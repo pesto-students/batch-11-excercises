5d9f4ccb9d74304a8a603e729a82b9b6bf579dfc
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-07 16:10:00 +0530

    completed map-object

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,12 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(inputMap,transformInput) {
+  var transformedMap = {}
+
+  Object.keys(inputMap).forEach(key=>{
+    transformedMap[key] = transformInput(inputMap[key])
+  });
+
+  return transformedMap;
 }
 
 export {
