c55d01cba2caeeef8a174457695e4a60e7b8ccc8
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-07 16:00:41 +0530

    Day 4 segment 1 solution

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,11 @@
-
-function findElement(...args) {
-  return args;
+function findElement(elementsArray) {
+  const result = [];
+  for (let i = 0; i < elementsArray.length; i += 1) {
+    if (elementsArray[i] % 2 === 0) {
+      result.push(elementsArray[i]);
+    }
+  }
+  return result[0];
 }
 
 export {
