83fae8238079be3d9d1cdd86f577c7968fd2742a
yashpandit <yashpandit1995@gmail.com>
2019-11-07 16:12:08 +0530

    Solve chunkArrayInGroups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,10 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(array, chunkSize) {
+  const chunks = [];
+  while (array.length) {
+    chunks.push(array.splice(0, chunkSize));
+  }
+  return chunks;
 }
 
 export {
