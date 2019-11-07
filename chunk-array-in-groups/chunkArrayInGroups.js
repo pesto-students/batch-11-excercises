663652aea31eb74a04a55605e7acc85f5e7a17d1
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-07 16:20:30 +0530

    Day 4 segment 1 solution of chunked Array

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,13 @@
+function chunkArrayInGroups(originalArray, size) {
+  const chunkedArray = [];
+  let index = 0;
+  while (index < originalArray.length) {
+    chunkedArray.push(originalArray.slice(index, index + size));
+    index += size;
+  }
 
-function chunkArrayInGroups(...args) {
-  return args;
+  return chunkedArray;
 }
-
 export {
   chunkArrayInGroups,
 };
