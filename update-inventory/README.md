92671f09a09f8454cb8bee4aa7ca5c41c3081c72
Harish Rana <r.shivi@yahoo.com>
2019-11-07 16:43:21 +0530

    Solve update inventory problem

--- update-inventory/README.md
+++ update-inventory/README.md
@@ -1,4 +1,4 @@
-Compare and update the inventory stored in a 2D array against a second 2D array of a
+Compare and update the inventory stored in a first 2D array against a second 2D array of a
 fresh delivery. Update the current existing inventory item quantities (in arr1).
 If an item cannot be found, add the new item and quantity into the inventory array.
 The returned inventory array should be in alphabetical order by item.
--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,6 +1,42 @@
 
-function updateInventory(...args) {
-  return args;
+function createMapInventory(inventory) {
+  return inventory.reduce((accumulator, itemInfoArray, index) => {
+    const name = itemInfoArray[1];
+    accumulator[name] = index.toString();
+    return accumulator;
+  }, {});
+}
+
+function sortInventory(inventory) {
+  return inventory.sort((inventoryItem1, inventoryItem2) => {
+    const nameA = inventoryItem1[1];
+    const nameB = inventoryItem2[1];
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+function updateInventory(currentInventory, newInventory) {
+  const currentInventoryMap = createMapInventory(currentInventory);
+  newInventory.forEach((itemInfoArray) => {
+    const name = itemInfoArray[1];
+    const quantity = itemInfoArray[0];
+    const indexOfCurrentItemInMap = currentInventoryMap[name];
+    if (indexOfCurrentItemInMap) {
+      const itemInCurrentInventory = currentInventory[parseInt(indexOfCurrentItemInMap, 10)];
+      let currentItemQuantity = itemInCurrentInventory[0];
+      currentItemQuantity += quantity;
+      itemInCurrentInventory[0] = currentItemQuantity;
+    } else {
+      currentInventory.push(itemInfoArray);
+    }
+  });
+  return sortInventory(currentInventory);
 }
 
 export {
