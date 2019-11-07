0ff243528ea578cbd8b102395689edf0149ee3c8
Basant Soni <basantech89@gmail.com>
2019-11-07 14:32:05 +0530

    Fix rot13 test

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,15 +1,18 @@
 
 function rot13(...args) {
-  const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const encodedString = args[0];
   let decodedString = '';
-  const nonAlphabeticPattern = /[ !,.]/gi;
+  const alphabetPattern = /[A-Z]+/gi;
   for (let i = 0; i < encodedString.length; i++) {
     const encodedChar = encodedString[i];
-    if (nonAlphabeticPattern.test(encodedChar)) continue;
-    let shiftedIndex = alphabets.indexOf(encodedChar) + 13;
-    if (shiftedIndex > 26) shiftedIndex = shiftedIndex % 26;
-    decodedString += alphabets[shiftedIndex];
+    let decodedChar = encodedChar;
+    if (encodedChar.match(alphabetPattern)) {
+      let shiftedIndex = alphabets.indexOf(encodedChar) + 13;
+      if (shiftedIndex > 25) shiftedIndex %= 26;
+      decodedChar = alphabets[shiftedIndex];
+    }
+    decodedString += decodedChar;
   }
   return decodedString;
 }
