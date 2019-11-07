7bd9167ff08163a00879db8a141d46e2b57a15b0
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-08 01:13:21 +0530

    Repeating String N times for positive numbers

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,15 @@
 
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(StringValue,repeatationTimes) {
+
+  let repetitiveStringArray;
+
+  if(repeatationTimes < 0){
+    return '';
+  }else{
+    repetitiveStringArray = new Array(repeatationTimes).fill(StringValue).join('');
+  }
+
+  return repetitiveStringArray;
 }
 
 export {
