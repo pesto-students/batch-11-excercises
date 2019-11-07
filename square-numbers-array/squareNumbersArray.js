0aaf95c6695c0e18cf94b6b7e949bbcbe5c23983
yashpandit <yashpandit1995@gmail.com>
2019-11-07 19:06:17 +0530

    Started working on the exercises

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,13 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(numbers) {
+  const squaredNumbers = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (typeof numbers[i] !== 'number') {
+      throw new Error('Every element in the array must be a number.');
+    }
+    squaredNumbers.push(numbers[i] ** 2);
+  }
+  return squaredNumbers;
 }
 
 export {
--- square-numbers-array/squareNumbersArray.test.js
+++ square-numbers-array/squareNumbersArray.test.js
@@ -3,9 +3,10 @@ import { squareNumbersArray } from './squareNumbersArray';
 describe('squareNumbersArray', () => {
   test('should throw an error if array of numbers is not passed as argument', () => {
     const myArray = [1, 2, 'string', 4];
-    expect(() => squareNumbersArray(...myArray)).toThrow('My custom error');
+    expect(() => squareNumbersArray(...myArray)).toThrow('Every element in the array must be a number.');
   });
   test('should return square array', () => {
-    
+    const myArray = [1, 2, 4];
+    expect(() => squareNumbersArray(...myArray)).toBe([1, 4, 16]);
   });
 });
