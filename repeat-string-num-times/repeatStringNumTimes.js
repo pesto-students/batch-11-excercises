ed4e27af2a5cfb0e22256fb9eac724a7ae3a6238
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-07 11:11:25 +0530

    Solved repeatStringNumTimes with all test cases

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,16 @@
 
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(stringToRepeat, frequency) {
+  const EMPTY_STRING = '';
+  if (frequency < 0) {
+    return EMPTY_STRING;
+  } else {
+    let repeatedString = '';
+    while (frequency > 0) {
+      repeatedString += stringToRepeat;
+      frequency--;
+    }
+    return repeatedString;
+  }
 }
 
 export {
