929112e532ed809643c3da4d1e74aaa42569ff4a
Basant Soni <basantech89@gmail.com>
2019-11-07 22:16:54 +0530

    Fix mapObject test

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,13 @@
 
 function mapObject(...args) {
-  return args;
+  const [numbers, action] = args;
+  const mappedObject = {};
+  for (const key in numbers) {
+    if (Object.prototype.hasOwnProperty.call(numbers, key)) {
+      mappedObject[key] = action(numbers[key]);
+    }
+  }
+  return mappedObject;
 }
 
 export {
