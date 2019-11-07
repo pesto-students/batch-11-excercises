247a624f0c7a5dc551941c4bc014396a46f59223
yashpandit <yashpandit1995@gmail.com>
2019-11-07 17:06:37 +0530

    Solve slasher

--- slasher/slasher.js
+++ slasher/slasher.js
@@ -1,6 +1,11 @@
 
-function slasher(list, ) {
-  return args;
+function slasher(list, itemsToSlash) {
+  let slashCount = itemsToSlash;
+  while (slashCount) {
+    list.splice(0, 1);
+    slashCount -= 1;
+  }
+  return list;
 }
 
 export {
