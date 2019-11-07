0baee07455a11e15ceeca15fb0a02d38089d2588
hitesh <hiteshchauhan.669@gmail.com>
2019-11-07 16:20:47 +0530

    Solved object-keys Problem

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -1,8 +1,15 @@
-
-function objectKeys(...args) {
-  return args;
+function objectKeys(object) {
+  const properties = [];
+  const invlaidProperties = [null, undefined, 0, ""];
+  for (let key in object) {
+    const property = object[key];
+    if (invlaidProperties.includes(property)) {
+      properties.push(key.toString());
+    } else {
+      properties.push(property.toString());
+    }
+  }
+  return properties;
 }
 
-export {
-  objectKeys,
-};
+export { objectKeys };
