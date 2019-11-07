fa47879d9fadf9ea969d9f4b7b9d8d61524a8a93
Harish Rana <r.shivi@yahoo.com>
2019-11-07 11:53:12 +0530

    Solve get index to ins problem

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,15 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(numberArray, numberToBeInserted) {
+  if (!numberArray) {
+    return -1;
+  }
+  numberArray.sort((a, b) => a - b);
+  let index = 0;
+  while (numberArray[index] < numberToBeInserted) {
+    // eslint-disable-next-line no-plusplus
+    index += 1;
+  }
+  return index;
 }
 
 export {
