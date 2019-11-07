d49d337d6b018a0eaeba401190c8277df283f60f
Basant Soni <basantech89@gmail.com>
2019-11-07 06:08:24 +0530

    Fix getIndexToIns test

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,15 @@
 
 function getIndexToIns(...args) {
-  return args;
+  const [numArray, toInsert] = args;
+  const sortedNumArray = numArray.sort((a, b) => a - b);
+  let lowestIns = sortedNumArray.length;
+  for (let i = 0; i < sortedNumArray.length; i++) {
+    if (sortedNumArray[i] >= toInsert) {
+      lowestIns = i;
+      return lowestIns;
+    }
+  }
+  return lowestIns;
 }
 
 export {
