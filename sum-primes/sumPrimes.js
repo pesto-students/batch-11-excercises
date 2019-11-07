79e8f2c9de4c58db17ba6843111af9138ee0d0d7
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-07 10:18:45 +0530

    Sum of Prime Numbers in Given Range

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,29 @@
 
-function sumPrimes(...args) {
-  return args;
+function checkPrimeNumber(numbertoCheck){
+
+    if(numbertoCheck === 1){
+      return false;
+    }
+
+    for(let i = 2; i*i <= numbertoCheck; i++){
+        if(numbertoCheck % i === 0) {
+          return false;
+        }
+    }
+    return true;
+}
+
+function sumPrimes(numberRange) {
+
+  let primeAddition = 0;
+
+  for(let i = 2; i <= numberRange; i++){
+      if(checkPrimeNumber(i)){
+        primeAddition += i;
+      }
+  }
+
+  return primeAddition;
 }
 
 export {
