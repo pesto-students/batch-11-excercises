845728670edbeb7a6e2139564a510f93a5c7f921
Sourav Das <souravdasslg95@gmail.com>
2019-11-07 08:34:50 +0530

    Exercise named user-check-password is completed

--- user-check-password/user.js
+++ user-check-password/user.js
@@ -1,15 +1,18 @@
 
 // (*)
 // Follow the instructions and fill in the blank sections.
-function User() {
+function User(userName, password) {
   // set a username and password property on the user object that is created
+  this.username = userName;
+  this.password = password;
+  return this;
 }
 
 // create a method on User called `checkPassword`
 // this method should take in a string and compare it to the object's password property
 // return `true` if they match, otherwise return `false`
-User.prototype.checkPassword = function checkPassword() {
-
+User.prototype.checkPassword = function checkPassword(passwordProvidedByUser) {
+  return this.password === passwordProvidedByUser;
 };
 
 export {
