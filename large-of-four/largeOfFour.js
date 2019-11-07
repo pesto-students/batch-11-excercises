a52eca4e53e982640cadaf8bc275ec3672a1d507
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-08 01:34:08 +0530

    Create large number array from four arrays

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,14 @@
 
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(largeOfFourArray) {
+  
+  let finalLargeofFourArray = [];
+
+  for(let i = 0; i < 4; i++){
+      let [highestBound] = [...largeOfFourArray[i]].sort((x,y) => {return y-x;});
+      finalLargeofFourArray.push(Number([highestBound]));
+  }
+
+  return finalLargeofFourArray;
 }
 
 export {
