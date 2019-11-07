03576cc570493e620ef998e89733199e7c658e98
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-07 18:29:13 +0530

    Fix test for chunk-array-in-groups
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,12 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(wholeArray, chunkSize) {
+  const copyOfWholeArray = [...wholeArray];
+  const chunked = [];
+  while (copyOfWholeArray.length > 0) {
+    const chunk = copyOfWholeArray.splice(0, chunkSize);
+    chunked.push(chunk);
+  }
+  return chunked;
 }
 
 export {
