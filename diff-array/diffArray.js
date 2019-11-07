2cec05bfe5b3a9c1a4144064e508c36930fe094d
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-07 23:55:16 +0530

    Symantic Array

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,21 @@
 
-function diffArray(...args) {
-  return args;
+function diffArray(firstArrayForCompare, secondArrayForCompare) {
+
+  const listWithAllDifferences = [];
+
+  firstArrayForCompare.forEach((arrayElement) => {
+    if (!secondArrayForCompare.includes(arrayElement)){ 
+      listWithAllDifferences.push(arrayElement);
+    }
+  });
+
+  secondArrayForCompare.forEach((arrayElement) => {
+    if (!firstArrayForCompare.includes(arrayElement)){
+      listWithAllDifferences.push(arrayElement);
+    }
+  });
+
+  return listWithAllDifferences;
 }
 
 export {
