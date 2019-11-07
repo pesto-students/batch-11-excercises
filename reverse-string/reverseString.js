5a9f389d03fce9dcd5e346c34bfc87c72341d5f5
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-08 00:37:13 +0530

    Reverse String without reverse()

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,14 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(OriginalString) {
+
+  let ReverseString = [];
+
+  let SplittedStringArray = OriginalString.split('');
+
+  for(let i = SplittedStringArray.length - 1; i >= 0;i--){
+          ReverseString.push(SplittedStringArray[i]);
+  }
+    return ReverseString.join('');
 }
 
 export {
