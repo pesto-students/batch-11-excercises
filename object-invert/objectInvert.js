3b28130f3f2b2e329a5a34f287e778b29f4d30c9
Basant Soni <basantech89@gmail.com>
2019-11-07 22:30:01 +0530

    Fix objectInvert test

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,6 +1,12 @@
 
-function objectInvert(...args) {
-  return args;
+function objectInvert(simpleObject) {
+  const invertedObject = {};
+  for (const key in simpleObject) {
+    if (Object.prototype.hasOwnProperty.call(simpleObject, key)) {
+      invertedObject[simpleObject[key]] = key;
+    }
+  }
+  return invertedObject;
 }
 
 export {
