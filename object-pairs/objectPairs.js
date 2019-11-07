21419910372c6061d9ede62eed43062893dc1ff2
yashpandit <yashpandit1995@gmail.com>
2019-11-07 17:02:06 +0530

    Solve objectPairs

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,6 +1,10 @@
 
-function objectPairs(...args) {
-  return args;
+function objectPairs(object) {
+  const pairs = [];
+  for (const key of Object.keys(object)) {
+    pairs.push([key, object[key]]);
+  }
+  return pairs;
 }
 
 export {
