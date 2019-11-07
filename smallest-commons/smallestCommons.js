4d8ec14d33a690e4dd607286fd36f7b533872ca9
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-08 01:28:58 +0530

    Solved smallestCommons with all test cases

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,23 @@
 
-function smallestCommons(...args) {
-  return args;
+function smallestCommons(range) {
+  const [low, high] = range.sort((a, b) => a - b);
+  let lcmOfRange = low;
+  for (let i = low; i < high; i++) {
+    lcmOfRange = calculateLCM(lcmOfRange, i + 1);
+  }
+  return lcmOfRange;
+}
+
+function calculateGCD(firstNumber, secondNumber) {
+  if (firstNumber === 0) {
+    return secondNumber;
+  } else {
+    return calculateGCD(secondNumber % firstNumber, firstNumber);
+  }
+}
+
+function calculateLCM(firstNumber, secondNumber) {
+  return (firstNumber * secondNumber) / calculateGCD(firstNumber, secondNumber);
 }
 
 export {
