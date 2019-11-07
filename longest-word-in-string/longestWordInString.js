5d8bea3a8b4ca0720c7c7a1e87b65ea990f12795
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-08 00:16:06 +0530

    Longest String Among Sentence

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,8 @@
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(largestString) {
+
+  const [largestStringLength] = [...largestString.split(' ').sort((a,b) => {return b.length - a.length})];
+  return largestStringLength.length;
 }
 
 export {
