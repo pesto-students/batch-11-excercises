cf5695db675b9b7fc5d3a4766e100aec383675b2
Basant Soni <basantech89@gmail.com>
2019-11-08 00:41:11 +0530

    Fix alternatingCharacters test

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,16 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  const deletions = [];
+  strings.forEach((string) => {
+    let repeated = 0;
+    for (let i = 0; i < string.length - 1; i++) {
+      if (string[i] === string[i + 1]) {
+        repeated += 1;
+      }
+    }
+    deletions.push(repeated);
+  });
+  return deletions;
 }
 
 export {
