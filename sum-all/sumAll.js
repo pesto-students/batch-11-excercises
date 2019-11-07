3f568aa50496ca1729db2b2a4f175003bbb429a5
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-08 01:01:30 +0530

    Sum of PairValue Array

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,7 +1,17 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(pairValueArray){
+
+  let totalSum = 0;
+
+  const [lowerBound,upperBound] = [...pairValueArray].sort((x,y) => {return x-y});
+
+  for(let i = lowerBound; i <= upperBound;i++){
+        totalSum += i;
+  }
+
+  return totalSum;
 }
+
 
 export {
   sumAll,
