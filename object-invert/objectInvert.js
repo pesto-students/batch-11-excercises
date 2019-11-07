b019830c074a7112d5106616f58ecc3fbb032b48
Sourav Das <souravdasslg95@gmail.com>
2019-11-07 16:26:59 +0530

    Exercise Named objectInvert Is Completed

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,6 +1,12 @@
 
-function objectInvert(...args) {
-  return args;
+function objectInvert(originalObject) {
+  const keysOfOriginalObject = Object.keys(originalObject);
+  const valuesOfOriginalObject = Object.values(originalObject);
+  const invertedObject = {};
+  valuesOfOriginalObject.forEach((aValueFromOriginalObject, currentIndex) => {
+    invertedObject[aValueFromOriginalObject] = keysOfOriginalObject[currentIndex];
+  });
+  return invertedObject;
 }
 
 export {
