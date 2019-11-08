fe75644cdcf1b68a51b9348656701c33dfe21175
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-08 09:32:18 +0530

    Fix test for square-numbers-array
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,11 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...maybeNumbersArray) {
+  if (!maybeNumbersArray.every(Number)) {
+    throw new Error('Unexpected element in array. An array with only numbers will be accepted, like - [1,2,3]');
+  }
+
+  // eslint-disable-next-line no-restricted-properties
+  return maybeNumbersArray.map((element) => Math.pow(element, 2));
 }
 
 export {
