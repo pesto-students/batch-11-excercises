0a10ff3259ea76dc9e81386fefee0ec2a673a471
Sourav Das <souravdasslg95@gmail.com>
2019-11-08 16:33:45 +0530

    Exercise named mathFns is completed

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,8 +1,16 @@
 
-function mathFns(...args) {
-  return args;
+function sqrt(number) {
+  return Math.sqrt(number);
 }
-
+function power(number, powerOfNumber) {
+  // eslint-disable-next-line no-restricted-properties
+  return Math.pow(number, powerOfNumber);
+}
+function round(number) {
+  return Math.round(number);
+}
 export {
-  mathFns,
+  sqrt,
+  power,
+  round,
 };
