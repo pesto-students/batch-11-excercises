260b48acdfd44a46e32f20fbc82054edca19214b
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-08 16:15:08 +0530

    Solved abbreviateString with all test cases passing

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,13 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(sentence) {
+  if (typeof sentence !== 'string') {
+    throw new Error('Please provide alphabetical string!')
+  }
+  const words = sentence.split(' ');
+  const firstWord = words[0];
+  const lastWordInitials = words[words.length - 1].toUpperCase()[0];
+  const abbreviatedString = `${firstWord} ${lastWordInitials}.`;
+  return abbreviatedString;
 }
 
 export {
