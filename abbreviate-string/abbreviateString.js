3b8d3c8ea6495903707751c74191685e091fb0ac
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-08 17:08:05 +0530

    abbreviate the string solved

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,10 @@
-
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if (typeof string !== 'string') {
+    throw new Error();
+  }
+  const splitString = string.split(' ');
+  const abbreviateTheString = `${splitString[0]} ${splitString[splitString.length - 1][0].toUpperCase()}.`
+  return abbreviateTheString;
 }
 
 export {
--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,6 +1,10 @@
 
-function roundTo(...args) {
-  return args;
+function roundTo(value,decimals) {
+  const sign = value >= 0 ? 1 : -1;
+
+  // return Number(Math.round(value+'e'+decimals * num_sign)+'e-'+decimals * num_sign);
+  const roundedTo =  (Math.round((value*Math.pow(10,decimals))+(sign*0.0001))/Math.pow(10,decimals)).toFixed(decimals);
+   return parseFloat(roundedTo);
 }
 
 export {
