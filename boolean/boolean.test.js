b960f8d641751dc2a03cdb09cbe99140cd6101b6
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-08 16:09:18 +0530

    completed boolean

--- boolean/boolean.test.js
+++ boolean/boolean.test.js
@@ -4,5 +4,7 @@ describe('check for boolean', () => {
   test('should return a boolean', () => {
     expect(typeof boolean(true)).toBe('boolean');
     expect(boolean('')).toBe(false);
+    expect(boolean(NaN)).toBe(false);
+    expect(boolean({})).toBe(true);
   });
 });
