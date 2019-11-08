55d810ece37cadb083cff457dda0e0c6e12fc61d
yashpandit <yashpandit1995@gmail.com>
2019-11-08 17:11:15 +0530

    Solve mathFns

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,8 +1,27 @@
 
-function mathFns(...args) {
-  return args;
+function sqrt(number) {
+  for (let i = 0; i * i <= number; i += 1) {
+    if (i * i === number) {
+      return i;
+    }
+  }
+  return null;
+}
+
+function round(decimalNumber) {
+  const stringifiedNumber = decimalNumber.toString().split('');
+  if (stringifiedNumber[stringifiedNumber.length - 1] > 5) {
+    return Number(stringifiedNumber[0]) + 1;
+  }
+  return Number(stringifiedNumber[0]);
+}
+
+function power(number, exponent) {
+  return number ** exponent;
 }
 
 export {
-  mathFns,
+  sqrt,
+  round,
+  power,
 };
