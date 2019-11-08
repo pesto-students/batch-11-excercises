11b4b97e3f9eae086d3b83bbc389a9cfd03b0880
Harish Rana <r.shivi@yahoo.com>
2019-11-08 15:45:56 +0530

    Solve square numbers array problem partially

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,14 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(numbers) {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] !== 'number') {
+      throw new Error('Each item should be of type number');
+    } else if (numbers[i] !== (i + 1) * (i + 1)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
