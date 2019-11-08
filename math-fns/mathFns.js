fc73c48df9873ac1dc1cede83416cd115dee69f2
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-09 00:44:22 +0530

    Maths functions solved

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,8 +1,17 @@
+function sqrt(number) {
+  return Math.sqrt(number);
+}
 
-function mathFns(...args) {
-  return args;
+function power(number, powerOfNumber) {
+  return number ** powerOfNumber;
+}
+
+function round(number) {
+  return Math.round(number);
 }
 
 export {
-  mathFns,
+  sqrt,
+  power,
+  round,
 };
