43f150e21c3cfd9276d4c9b784f097b8c26d948a
yashpandit <yashpandit1995@gmail.com>
2019-11-08 15:52:33 +0530

    Solve abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,15 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if (typeof string !== 'string') {
+    throw new Error('The input must be a string');
+  }
+  const words = string.split(' ');
+  const firstName = words[0];
+  const lastInitial = words[words.length - 1].split('').splice(0, 1).join('');
+  const lastInitialCapitalized = lastInitial.toUpperCase();
+  const period = '.';
+  const space = ' ';
+  return firstName + space + lastInitialCapitalized + period;
 }
 
 export {
