425ae5b90f200779b88e56c59998d80e55489d5a
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-08 17:08:20 +0530

    abbreviate the string solved

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,12 +1,11 @@
 function abbreviateString(string) {
   if (typeof string !== 'string') {
-    throw new Error();
+    throw new Error(); 
   }
   const splitString = string.split(' ');
   const abbreviateTheString = `${splitString[0]} ${splitString[splitString.length - 1][0].toUpperCase()}.`
   return abbreviateTheString;
 }
-
 export {
   abbreviateString,
 };
--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,10 +1,6 @@
 
-function roundTo(value,decimals) {
-  const sign = value >= 0 ? 1 : -1;
-
-  // return Number(Math.round(value+'e'+decimals * num_sign)+'e-'+decimals * num_sign);
-  const roundedTo =  (Math.round((value*Math.pow(10,decimals))+(sign*0.0001))/Math.pow(10,decimals)).toFixed(decimals);
-   return parseFloat(roundedTo);
+function roundTo(value,decimals) { 
+  return (Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 
 export {
