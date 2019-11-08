d3f0d2e3378938ac6c546ebbd54906718519b8f4
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-08 10:58:19 +0530

    Solved objectDefaults with all test cases

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,12 @@
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(object, defaultObject) {
+  const mergedObject = { ...object };
+  for (let [key, value] of Object.entries(defaultObject)) {
+    if (!mergedObject.hasOwnProperty(key)) {
+      mergedObject[key] = value;
+    }
+  }
+  return mergedObject;
 }
 
 export {
