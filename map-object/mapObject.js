5bf802c95016d9aa9a9575334b10524449207cc7
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-08 11:08:50 +0530

    Solved mapObject with all test cases

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,13 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(object, transformer) {
+  const transformedObject = { ...object };
+  for (let key of Object.keys(transformedObject)) {
+    let updatedValue = transformer(transformedObject[key]);
+    if (updatedValue) {
+      transformedObject[key] = updatedValue;
+    }
+  }
+  return transformedObject;
 }
 
 export {
