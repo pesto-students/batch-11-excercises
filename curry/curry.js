23dc544121b47f3f5955723beee562a2ac09b1ee
Harish Rana <r.shivi@yahoo.com>
2019-11-08 08:42:47 +0530

    Solve curry question

--- curry/curry.js
+++ curry/curry.js
@@ -1,6 +1,14 @@
 
 function curry(fn) {
-  console.log(fn.arguments);
+  return function curried(...args) {
+    if (args.length < fn.length) {
+      return function pass(...args2) {
+        return curried.apply(this, [...args, ...args2]);
+      };
+    }
+    // eslint-disable-next-line prefer-spread
+    return fn.apply(this, args);
+  };
 }
 
 export {
