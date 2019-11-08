73dc0e2ff41b4d8dcead097ee6eccbc84fdbf104
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-08 19:10:27 +0530

    Abbreviate String

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,11 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(fullNameString) {
+
+  let splittedString = fullNameString.split(' ');
+  let firstName = splittedString[0];
+  let lastName = splittedString[splittedString.length - 1][0];
+
+  return firstName.concat(' ').concat(lastName.toUpperCase()).concat('.');
 }
 
 export {
