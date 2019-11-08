c71ae8f1528b788f58ca1a3c8641d0240bcaacd1
Harish Rana <r.shivi@yahoo.com>
2019-11-08 09:30:57 +0530

    Solve memoization problem

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,15 @@
 
-function memoize(...args) {
-  return args;
+function memoize(fn) {
+  const resultMap = {};
+  return function memoized(...args) {
+    const toReturnSavedResult = resultMap[args];
+    if (toReturnSavedResult) {
+      return resultMap[args];
+    }
+    const result = fn(...args);
+    resultMap[args] = result;
+    return result;
+  };
 }
 
 export {
