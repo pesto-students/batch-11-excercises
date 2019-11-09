4265eeff9ccc8c4278714c6fb994ee8f1889347c
hitesh <hiteshchauhan.669@gmail.com>
2019-11-09 11:55:37 +0530

    Solved abbrebiateString Problem

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,8 +1,20 @@
+function abbreviateString(argument) {
+  const isString = typeof argument === "string";
 
-function abbreviateString(...args) {
-  return args;
+  if (!isString) {
+    throw new Error("Expected String as an argument");
+  }
+
+  const splitString = argument.trim().split(" ");
+  const firstWord = splitString[0];
+
+  if (splitString.length > 1) {
+    const lastWord = splitString[splitString.length - 1];
+    const abbreviatedLastWord = lastWord.toUpperCase().charAt(0);
+    return firstWord + " " + abbreviatedLastWord + ".";
+  }
+
+  return firstWord;
 }
 
-export {
-  abbreviateString,
-};
+export { abbreviateString };
