c42421ef5db478705410f4a8b6d5b4628e597621
hitesh <hiteshchauhan.669@gmail.com>
2019-11-09 12:57:18 +0530

    Solved daysInMonth Problem

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,8 +1,15 @@
+function daysInMonth(year, month) {
+  const isLeap = year % 4 === 0;
+  if (month === 2) {
+    return isLeap ? 29 : 28;
+  }
 
-function daysInMonth(...args) {
-  return args;
+  const isEvenIndexMonth = month % 2 === 0;
+  if (isEvenIndexMonth) {
+    return 30;
+  } else {
+    return 31;
+  }
 }
 
-export {
-  daysInMonth,
-};
+export { daysInMonth };
