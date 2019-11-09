34e742308d5ceaf43d9aa361da76210f437774f7
Sourav Das <souravdasslg95@gmail.com>
2019-11-09 13:07:11 +0530

    Exercise named counter factory is completed

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,15 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  let count = 0;
+  function increment() {
+    count += 1;
+    return count;
+  }
+  function decrement() {
+    count -= 1;
+    return count;
+  }
+  return { increment, decrement };
 }
 
 export {
