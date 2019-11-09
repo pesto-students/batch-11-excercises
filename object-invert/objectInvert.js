bc0143088bf7e1265eac90e7e9acfe51de9b3ab0
hitesh <hiteshchauhan.669@gmail.com>
2019-11-09 19:15:12 +0530

    Solved objectInvert Problem

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,12 +1,11 @@
 function objectInvert(object) {
   const inverted = {};
   for (let key in object) {
-    let value = object[key]
+    let value = object[key];
     inverted[value] = key;
   }
 
   return inverted;
 }
-console.log(objectInvert({ x: 25 }));
 
-// export { objectInvert };
+export { objectInvert };
