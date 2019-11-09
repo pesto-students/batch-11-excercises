0318dd5aaff29aeb86f8cd362bbdfa61ae8bb2bd
Basant Soni <basantech89@gmail.com>
2019-11-10 03:52:07 +0530

    Fix updateInventory test

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,6 +1,25 @@
 
-function updateInventory(...args) {
-  return args;
+function updateInventory(oldInventory, newInventory) {
+  newInventory.forEach((newinventory) => {
+    const item = newinventory[1];
+    const quantity = newinventory[0];
+    let found = false;
+    oldInventory.forEach((oldinventory) => {
+      if (item === oldinventory[1]) {
+        oldinventory[0] += quantity;
+        found = true;
+      }
+    });
+    if (!found) {
+      oldInventory.push(newinventory);
+    }
+  });
+  // return oldInventory.sort((arr1, arr2) => arr1[1].localeCompare(arr2[1]));
+  return oldInventory.sort((arr1, arr2) => {
+    if (arr1[1] > arr2[1]) return 1;
+    if (arr1[1] < arr2[1]) return -1;
+    return 0;
+  });
 }
 
 export {
