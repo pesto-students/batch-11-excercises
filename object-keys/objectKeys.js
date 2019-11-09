461ace81b3c79a1605e6579abf8996312fef3c9d
Basant Soni <basantech89@gmail.com>
2019-11-10 04:57:43 +0530

    Fix objectKeys test

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -3,7 +3,7 @@ function objectKeys(object) {
   const objectProperties = [];
   for (const key in object) {
     if (Object.prototype.hasOwnProperty.call(object, key)) {
-      objectProperties.push(object[key]);
+      objectProperties.push(key);
     }
   }
   return objectProperties;
