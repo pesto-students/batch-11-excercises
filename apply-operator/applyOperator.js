b168d0a0e879c4067a84fe0cee58772d0249ca3b
Basant Soni <basantech89@gmail.com>
2019-11-09 05:33:53 +0530

    Fix applyOperator test

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,15 @@
 
 function applyOperator(...args) {
-  return args;
+  if (typeof args[0] !== 'string') throw new Error('Missing Operator: possible operators are +, -, *, /, %');
+  else if (args.length === 1) return 0;
+  else {
+    const [operator, firstOperand, ...operands] = args;
+    let result = firstOperand;
+    for (const operand of operands) {
+      result = eval(`${result} ${operator} ${operand}`);
+    }
+    return result % 1 !== 0 ? Number(result.toFixed(4)) : result;
+  }
 }
 
 export {
