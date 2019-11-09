53aa06016d209511bca34d35cf98880342b205eb
Basant Soni <basantech89@gmail.com>
2019-11-10 03:18:32 +0530

    Fix truthCheck test

--- truth-check/truthCheck.js
+++ truth-check/truthCheck.js
@@ -1,6 +1,11 @@
 
-function truthCheck(...args) {
-  return args;
+function truthCheck(collection, predicate) {
+  for (const object of collection) {
+    if (!object[predicate]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
