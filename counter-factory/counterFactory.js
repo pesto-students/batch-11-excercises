44db4753cd89cc15a07e5ddcd517b8e9280b321c
Basant Soni <basantech89@gmail.com>
2019-11-09 06:20:14 +0530

    Fix counterFactory test

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,16 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  return {
+    count: 0,
+    increment() {
+      this.count += 1;
+      return this.count;
+    },
+    decrement() {
+      this.count -= 1;
+      return this.count;
+    },
+  };
 }
 
 export {
