a1fbb760100a2a7c50210814502137c2b7b817d9
Basant Soni <basantech89@gmail.com>
2019-11-10 02:50:06 +0530

    Update steamrollArray test

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,16 @@
 
 function steamrollArray(rolledArray) {
-  const flattenedArray = rolledArray.flat(Infinity);
+  const flattenedArray = [];
+  function flatten(rolled) {
+    if (Array.isArray(rolled)) {
+      for (const item of rolled) {
+        flatten(item);
+      }
+    } else {
+      flattenedArray.push(rolled);
+    }
+  }
+  flatten(rolledArray);
   return flattenedArray;
 }
 
