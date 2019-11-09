cf4678ae6c75da161073031fc598b8040c67f06e
Harish Rana <r.shivi@yahoo.com>
2019-11-09 18:36:19 +0530

    Solve round to problem

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,6 +1,7 @@
 
-function permutateWithRepetitions(...args) {
-  return args;
+function permutateWithRepetitions(alphabets) {
+  const numberOfAlphabets = alphabets.length;
+  return alphabets.reduce((accumulator) => numberOfAlphabets * accumulator, 1);
 }
 
 export {
--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,7 +1,22 @@
 
-function roundTo(...args) {
-  return args;
+function roundTo(number, decimalPlaces) {
+  const numberAbsolute = Math.abs(number);
+  const tensMultiplier = 10 ** (decimalPlaces + 1);
+  const testDivisor = 10 ** decimalPlaces;
+  return Math.sign(number) * (
+    Math.round((Math.round(numberAbsolute * tensMultiplier) / 10)) / testDivisor);
 }
+
+roundTo.down = function down(number, decimalPlaces) {
+  const tensMultiplier = 10 ** decimalPlaces;
+  return Math.floor(number * tensMultiplier) / tensMultiplier;
+};
+
+roundTo.up = function up(number, decimalPlaces) {
+  const tensMultiplier = 10 ** decimalPlaces;
+  return Math.ceil(number * tensMultiplier) / tensMultiplier;
+};
+
 
 export {
   roundTo,
