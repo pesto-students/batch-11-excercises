44d465264918d217adb8e67702d95359e6c39724
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-09 21:40:44 +0530

    counter factory using object

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,17 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  const countObject = {
+    count: 0,
+    increment() {
+      this.count += 1;
+      return this.count;
+    },
+    decrement() {
+      this.count -= 1;
+      return this.count;
+    },
+  };
+  return countObject;
 }
 
 export {
