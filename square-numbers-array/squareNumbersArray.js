a551338a3664b1b7cbf71b21a93bb4952b0e83b6
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-09 10:32:08 +0530

    squareNumber test fixes

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,14 +1,11 @@
-
-
 function squareNumbersArray(numbers) {
-    
-     const result = numbers.map((number) => {
-       if(typeof number != 'number') {
-         throw new Error('My custom error');      
-       }
-       return number * number;
-      });
-     return result;
+  const result = numbers.map((number) => {
+    if (typeof number !== 'number') {
+      throw new Error('My custom error');
+    }
+    return number * number;
+  });
+  return result;
 }
 
 export {
