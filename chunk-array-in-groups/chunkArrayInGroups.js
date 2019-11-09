1202e10bc129b086f1f00323c7e9831ea5fd18a0
hitesh <hiteshchauhan.669@gmail.com>
2019-11-09 18:54:17 +0530

    Solved chunk array Problem

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,10 @@
-
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(array, chunkSize) {
+  const chunkCollection = [];
+  while (array.length !== 0) {
+    const chunk = array.splice(0, chunkSize);
+    chunkCollection.push(chunk);
+  }
+  return chunkCollection;
 }
 
-export {
-  chunkArrayInGroups,
-};
+export { chunkArrayInGroups };
