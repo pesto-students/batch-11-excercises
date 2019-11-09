1249a01f2233e2a86a18cfc37ce575c0eac60941
Harish Rana <r.shivi@yahoo.com>
2019-11-10 01:01:58 +0530

    Solve map objects function

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,11 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(obj, modifyFn) {
+  const keys = Object.keys(obj);
+  const mappedObj = {};
+  keys.forEach((key) => {
+    mappedObj[key] = modifyFn(obj[key]);
+  });
+  return mappedObj;
 }
 
 export {
