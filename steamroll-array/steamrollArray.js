1e9d59bbbf6b32f9000413740d9bb1d3d8cf957a
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-10 00:46:30 +0530

    Flatten Array using flat()

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,14 +1,6 @@
 
 function steamrollArray(flattenArray) {
-  let returnedArray = [];
-  for (let i = 0; i < flattenArray.length; i++) {
-    if (Array.isArray(flattenArray[i])) {
-      returnedArray = returnedArray.concat(steamrollArray(flattenArray[i]));
-    } else {
-      returnedArray.push(flattenArray[i]);
-    }
-  }
-  return returnedArray;
+  return flattenArray.flat();
 }
 
 export {
