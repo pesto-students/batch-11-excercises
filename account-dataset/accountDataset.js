e96f3c101b3b76986f23ab909d3c450c85cea4f1
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-09 10:28:17 +0530

    account dataset fixed test fixes

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,65 +1,47 @@
 import accountDataset from './dataset.json';
 
 function hundredThousandairs() {
-  const {
-    bankBalances
-  } = accountDataset;
-  const result = bankBalances.filter((key) => parseInt(key['amount']) > 100000);
+  const { bankBalances } = accountDataset;
+  const result = bankBalances.filter((key) => parseInt(key.amount, 10) > 100000);
   return result;
 }
 
 function datasetWithRoundedDollar() {
-  const {
-    bankBalances
-  } = accountDataset;
+  const { bankBalances } = accountDataset;
   const result = bankBalances.map((item) => {
-    item['rounded'] = Math.round(parseInt(item['amount']))
+    item.rounded = Math.round(parseInt(item.amount, 10));
     return item;
-  })
+  });
   return result;
 }
 
 function sumOfBankBalances() {
-  const {
-    bankBalances
-  } = accountDataset;
+  const { bankBalances } = accountDataset;
   let totalAmount = 0;
   bankBalances.map((item) => {
-    totalAmount += (parseFloat(item['amount']));
+    totalAmount += (parseFloat(item.amount));
     return item;
-  })
+  });
   return parseFloat(totalAmount.toFixed(2));
 }
 
 function sumOfInterests() {
-  const {
-    bankBalances
-  } = accountDataset;
-  // Wisconsin, 
-  // Illinois
-  // Wyoming
-  // Ohio
-  // Georgia
-  // Delaware
-  const selectedStates = ['WI', 'IL', 'WY', 'OH', 'GE', 'DL'];
+  const { bankBalances } = accountDataset;
+  const selectedStates = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
   const filterAccount = (account) => selectedStates.includes(account.state);
   const filteredStates = bankBalances.filter(filterAccount);
-  // let totalSumAmountOfFilteredStates = 0;
   filteredStates.map((account) => {
-    const addInterest = parseFloat(account.amount) * 18.9;
+    const addInterest = parseFloat(account.amount) * 0.189;
     account.amount = addInterest;
-     return account;
-     })
-  const totalSumInterest = filteredStates.reduce(( accumulator, currentValue ) => accumulator + currentValue.amount,
-  0);
-  // const totalSumInterest = totalSumAmountOfFilteredStates + (totalSumAmountOfFilteredStates * 18.9 / 100);
+    return account;
+  });
+  const totalSumInterest = filteredStates.reduce((accumulator, currentValue) => accumulator + currentValue.amount,
+    0);
   return parseFloat(totalSumInterest.toFixed(2));
 }
 
 function higherStateSums() {
-  const {
-    bankBalances
-  } = accountDataset;
+  const { bankBalances } = accountDataset;
   let totalHigherStateSum = 0;
   const sumAmountBystateObj = bankBalances.reduce((states, account) => {
     if (account.state in states) {
@@ -70,7 +52,7 @@ function higherStateSums() {
     return states;
   }, {});
 
-  Object.keys(sumAmountBystateObj).forEach(state => {
+  Object.keys(sumAmountBystateObj).forEach((state) => {
     if (sumAmountBystateObj[state] > 1000000) {
       totalHigherStateSum += sumAmountBystateObj[state];
     }
