152fa415a4307d8999d1b976a7ce5753eebf5919
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-10 00:40:17 +0530

    Solved alternatingCharacters for all test cases

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,17 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(repeatingCharactersWords) {
+  const alternatingStringLengths = [];
+  for (let i = 0; i < repeatingCharactersWords.length; i++) {
+    let minimumDeletions = 0;
+    let currentWord = repeatingCharactersWords[i];
+    for (let j = 0; j < currentWord.length; j++) {
+      if (currentWord[j] === currentWord[j + 1]) {
+        minimumDeletions += 1;
+      }
+    }
+    alternatingStringLengths.push(minimumDeletions);
+  }
+  return alternatingStringLengths;
 }
 
 export {
