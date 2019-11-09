a8b972bea61cc97163336d060fb61cb0b46c20d2
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-09 21:55:57 +0530

    Finding Diaognal of Rectangle

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,6 +1,8 @@
 
-function rectangleDiagonal(...args) {
-  return args;
+function rectangleDiagonal(rectangleSide1, rectangleSide2) {
+  const rectangleDiagonalSide = rectangleSide1 ** 2 + rectangleSide2 ** 2;
+
+  return Math.sqrt(rectangleDiagonalSide);
 }
 
 export {
