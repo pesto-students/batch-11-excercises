5c341e822ef788520f1611377a11cc2e86b2edeb
hitesh <hiteshchauhan.669@gmail.com>
2019-11-10 04:05:31 +0530

    Solved sumPrime problem

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,8 +1,22 @@
-
-function sumPrimes(...args) {
-  return args;
+function isPrime(value) {
+  if (value === 1) {
+    return false;
+  }
+  for (let i = 2; i < value; i++) {
+    if (value % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+function sumPrimes(number) {
+  let sum = 0;
+  for (let i = 2; i <= number; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
-export {
-  sumPrimes,
-};
+export { sumPrimes };
