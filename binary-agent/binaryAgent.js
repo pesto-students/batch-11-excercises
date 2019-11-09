612bf8f2f76c7ea90a32241c91dc79627975f993
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-10 00:24:04 +0530

    Binary to String using Charcode

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,14 @@
+import { parse } from "path";
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binaryString) {
+
+  let splittedBinary = binaryString.split(' ');
+  let binCode = [];
+
+  for(let i = 0; i < splittedBinary.length; i++) {
+    binCode.push(String.fromCharCode(parseInt(splittedBinary[i], 2)));
+  }
+  return binCode.join('');
 }
 
 export {
