60642520834470740eb54265782022505e3c851e
Basant Soni <basantech89@gmail.com>
2019-11-09 06:30:10 +0530

    Fix limitFunctionCallCount test

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,13 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(action, n) {
+  let times = 0;
+  return (...args) => {
+    if (times < n) {
+      times += 1;
+      return action(...args);
+    }
+    return null;
+  };
 }
 
 export {
