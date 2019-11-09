c69c50794eb3beb77de7a31306218c9823b01d1d
hitesh <hiteshchauhan.669@gmail.com>
2019-11-10 03:35:06 +0530

    Solved threeSum test

--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,8 +1,22 @@
+function threeSum(array, target) {
+  let triplet = [];
+  if (array.length < 3 || typeof target !== "number") {
+    return new Error("Invalid Inputs");
+  }
 
-function threeSum(...args) {
-  return args;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      for (let z = j + 1; z < array.length; z++) {
+        if (array[i] + array[j] + array[z] === target) {
+          triplet = triplet.concat([array[i], array[j], array[z]]);
+          return triplet;
+        }
+      }
+    }
+  }
+  if (triplet.length === 0) {
+    return null;
+  }
 }
 
-export {
-  threeSum,
-};
+export { threeSum };
