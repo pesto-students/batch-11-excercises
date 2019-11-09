6fccdb7ca39b741d18c24995abc179eb623f9d08
Basant Soni <basantech89@gmail.com>
2019-11-10 04:58:25 +0530

    Fix objectDefaults test

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,15 @@
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(obj, defaultObj) {
+  const objWithDefaults = {};
+  const defaultEntries = Object.entries(defaultObj);
+  const entries = Object.entries(obj);
+  for (const [key, val] of defaultEntries) {
+    objWithDefaults[key] = val;
+  }
+  for (const [key, val] of entries) {
+    objWithDefaults[key] = val;
+  }
+  return objWithDefaults;
 }
 
 export {
