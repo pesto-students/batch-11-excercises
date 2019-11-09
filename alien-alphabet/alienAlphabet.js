35094fc8c03dc5d7d50b9c7e340ed4418828e50c
Harish Rana <r.shivi@yahoo.com>
2019-11-09 13:27:53 +0530

    Solve alien alphabet problem

--- alien-alphabet/alienAlphabet.js
+++ alien-alphabet/alienAlphabet.js
@@ -1,6 +1,15 @@
 
-function alienAlphabet(...args) {
-  return args;
+function alienAlphabet(words) {
+  const alienAlphabetMap = {};
+  const alienAlphabets = [];
+  words.forEach((word, index) => {
+    const startingLetter = word[0];
+    if (!Object.prototype.hasOwnProperty.call(alienAlphabetMap, startingLetter)) {
+      alienAlphabetMap[startingLetter] = index;
+      alienAlphabets.push(startingLetter);
+    }
+  });
+  return alienAlphabets;
 }
 
 export {
