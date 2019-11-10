41019f65e3ade491c5b4e1123feb666abda1a2dc
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-10 23:44:46 +0530

    Solved lowercaseKeys with all test cases passing

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,6 +1,11 @@
 
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(object) {
+  const objectWithLowerCaseKeys = {};
+  for (let [nonLowerCaseKey, value] of Object.entries(object)) {
+    let lowerCaseKey = nonLowerCaseKey.toLowerCase();
+    objectWithLowerCaseKeys[lowerCaseKey] = value;
+  }
+  return objectWithLowerCaseKeys;
 }
 
 export {
