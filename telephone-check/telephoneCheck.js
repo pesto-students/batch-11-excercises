a7192455204e4040b5b115f92896ba5e8495d791
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-10 17:15:27 +0530

    Telephone number checking

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,7 @@
 
-function telephoneCheck(...args) {
-  return args;
+function telephoneCheck(telephoneNumber) {
+  const patternMatch = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(telephoneNumber);
+  return patternMatch;
 }
 
 export {
