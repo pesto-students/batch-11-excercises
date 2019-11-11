ee91abbc5602f474281945182f7808484a0e2028
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-11 17:00:21 +0530

    Fixed doubleOddNumbers problem with all test cases

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,8 +1,12 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(numbers) {
+  const oddNumbers = numbers.filter(isOdd);
+  return oddNumbers.map((number) => number * 2);
 }
 
+function isOdd(number) {
+  return number % 2 !== 0;
+}
 export {
   doubleOddNumbers,
 };
