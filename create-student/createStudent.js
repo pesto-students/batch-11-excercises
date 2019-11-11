d98502035ad1807b7ef0dd3620ffa4ad9898e46c
Harish Rana <r.shivi@yahoo.com>
2019-11-12 00:01:56 +0530

    Solved create student problem

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,21 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent({ likesJavaScript = true, likesES2015 = true } = {}) {
+  const likesBoth = likesJavaScript && likesES2015;
+  const likesOnlyJs = likesJavaScript && !likesES2015;
+  const likesOnlyES2015 = !likesJavaScript && likesES2015;
+
+  if (likesBoth) {
+    return 'The student likes JavaScript and ES2015';
+  }
+
+  if (likesOnlyJs) {
+    return 'The student likes JavaScript!';
+  }
+
+  if (likesOnlyES2015) {
+    return 'The student likes ES2015!';
+  }
+  return 'The student does not like much...';
 }
 
 export {
