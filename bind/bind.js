bc824e5f85e8dc13ae24f2eaa4416d043ce0318d
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-11 19:31:48 +0530

    Fix test for bind
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- bind/bind.js
+++ bind/bind.js
@@ -1,10 +1,6 @@
 
-// Traditional
-// function bind(fn, obj, ...args) {
-//   return fn.bind(obj, ...args);
-// }
-
-const bind = (fn, object, ...args) => fn.bind(object, ...args);
+const bind = (unboundFunction, object, ...args) => unboundFunction
+  .bind(object, ...args);
 
 export {
   bind,
