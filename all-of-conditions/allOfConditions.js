305a01ebd7f851b198e8ffc4183e0a6aa4781e90
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-11 19:28:36 +0530

    Fix test for all-of-conditions
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,20 +1,8 @@
 
-function allOfConditions(...predicates) {
-  return function runPredicates(inputToPredicates) {
-    for (const predicate of predicates) {
-      if (predicate(inputToPredicates) === false) {
-        break;
-      }
-      predicate(inputToPredicates);
-    }
-  };
-}
-
-// functional implementation
-// TODO: Check this
-// function allOfConditions(...predicates) {
-//   return (input) => predicates.reduce((acc, predicate) => acc && predicate(input), true);
-// }
+const allOfConditions = (...predicates) => (input) => predicates.reduce(
+  (calledFunctionResult, predicate) => calledFunctionResult && predicate(input),
+  true,
+);
 
 export {
   allOfConditions,
