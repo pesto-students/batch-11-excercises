7761a93fab22b6d440b4e5565e71a9c8fcc5f454
yashpandit <yashpandit1995@gmail.com>
2019-11-11 16:25:31 +0530

    Solve templateLiterals

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,7 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const footballPlayers = ['Rooney', 'Ronaldo', 'Messi', 'Pogba'];
+  return `There are 4 people on the football team. Their names are ${footballPlayers[0]}, ${footballPlayers[1]}, ${footballPlayers[2]}, ${footballPlayers[3]}.`;
 }
 
 export {
