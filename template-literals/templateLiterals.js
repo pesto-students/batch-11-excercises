93f53168d13078b1523894c8ef762055ca76af09
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-11 16:15:59 +0530

    Combine the array and sort it

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,6 @@
 
 function templateLiterals() {
-  const playerNames = ['Rooney','Ronaldo','Messi','Pogba'];
+  const playerNames = ['Rooney', 'Ronaldo', 'Messi', 'Pogba'];
   return `There are 4 people on the football team. Their names are ${playerNames[0]}, ${playerNames[1]}, ${playerNames[2]}, ${playerNames[3]}.`;
 }
 
