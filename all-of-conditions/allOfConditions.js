110dfb2ad46e72fec5530fb0e15c5db4522649c0
Harish Rana <r.shivi@yahoo.com>
2019-11-11 18:38:35 +0530

    Refactored allOfConditions

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,11 +1,19 @@
 
-function allOfConditions(...functions) {
-  return function output(input) {
-    for (const fn of functions) {
-      const toContinue = fn(input);
-      if (!toContinue) return;
-    }
-  };
+// function allOfConditions(...functions) {
+//   return function output(input) {
+//     for (const fn of functions) {
+//       const toContinue = fn(input);
+//       if (!toContinue) {
+//         return
+//       }
+//     }
+//   };
+// }
+
+// functional implementation
+
+function allOfConditions(...predicates) {
+  return (input) => predicates.reduce((acc, predicate) => acc && predicate(input), true);
 }
 
 export {
