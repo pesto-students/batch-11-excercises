4c7c3a2990df9c8482d0d83350fb4d374dfd778f
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-12 02:59:25 +0530

    Fix test for abbreviate-string
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,7 +1,15 @@
 
-function abbreviateString(...args) {
-  return args;
-}
+const firstWord = (words) => words[0];
+const lastWord = (words) => words[words.length - 1];
+const capitalizeFirstLetter = (word) => word.split('')[0].toUpperCase();
+const words = (string) => string.split(' ');
+const isNotString = (input) => typeof input !== 'string';
+const abbreviateString = (maybeString) => {
+  if (isNotString(maybeString)) {
+    throw new Error(`Expected a string. Got ${typeof maybeString}`);
+  }
+  return `${firstWord(words(maybeString))} ${capitalizeFirstLetter(lastWord(words(maybeString)))}.`;
+};
 
 export {
   abbreviateString,
