bca1e4a08a9e94a78248a6e51975d67ed1664d4a
yashpandit <yashpandit1995@gmail.com>
2019-11-11 18:36:08 +0530

    Modify mapFilterReduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-body-style */
+
 function mapFilterAndReduce(names) {
   const filteredNames = names.filter((name) => name.firstName.length === 4);
   return filteredNames.reduce((object, { firstName }) => {
