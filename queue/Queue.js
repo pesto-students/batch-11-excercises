af170afa20a23fe64484be5f96c9ab72178afe11
yashpandit <yashpandit1995@gmail.com>
2019-11-11 19:28:20 +0530

    Solve rejectPromise

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,36 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
 
-function queue(...args) {
-  return args;
+  enqueue(value) {
+    this.items.unshift(value);
+    return this.items;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  toString() {
+    return this.items.reverse().join(',');
+  }
+
+  isEmpty() {
+    return this.items.length > 0;
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
--- reject-promise/rejectPromise.js
+++ reject-promise/rejectPromise.js
@@ -1,6 +1,11 @@
 
-function rejectPromise(...args) {
-  return args;
+function rejectPromise(input) {
+  return new Promise((resolve, reject) => {
+    if (input === undefined) {
+      resolve('REJECTED!');
+    }
+    reject(new Error('Resolved'));
+  });
 }
 
 export {
