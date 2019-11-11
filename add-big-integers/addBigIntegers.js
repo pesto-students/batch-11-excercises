385e7789d1563a6b5ac2fe3cbb2be16a13dbe041
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-11 10:41:47 +0530

    Bigint fixes solved

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,29 @@
-
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(stringOfnumbers) {
+  const numbersArray = stringOfnumbers.split('\n').slice(1);
+  let largestNumber = 0;
+  let remainder = 0;
+  const addBigNumbers = [];
+  const reversednumberStrings = numbersArray.map((numberString) => {
+    if (largestNumber <= numberString.length) {
+      largestNumber = numberString.length;
+    }
+    return numberString.split('').reverse().join('');
+  });
+  for (let i = 0; i <= largestNumber - 1; i += 1) {
+    let sumofStrings = 0;
+    reversednumberStrings.forEach((number) => {
+      if (number[i]) {
+        sumofStrings += parseInt(number[i], 10);
+      }
+    });
+    sumofStrings += remainder;
+    remainder = Math.floor(sumofStrings / 10);
+    addBigNumbers[i] = sumofStrings % 10;
+  }
+  if (remainder > 0) {
+    addBigNumbers[largestNumber] = remainder;
+  }
+  return addBigNumbers.reverse().join('');
 }
 
 export {
