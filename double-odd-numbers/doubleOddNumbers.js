ff7174a125c224d18915e0a2043583c72dcd3760
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-11 16:51:25 +0530

    Completed double odd numbers

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,13 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(numbers) {
+  const doubledOddNumbers = [];
+
+  numbers.forEach((number) => {
+    if (number % 2 !== 0) {
+      doubledOddNumbers.push(number * 2);
+    }
+  });
+  return doubledOddNumbers;
 }
 
 export {
