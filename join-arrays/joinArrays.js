9c6f9a3145a11015a67aee2def16c86e6f7612c2
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-11 15:53:00 +0530

    Using Concat statement

--- join-arrays/joinArrays.js
+++ join-arrays/joinArrays.js
@@ -1,11 +1,5 @@
-import { join } from "path";
-
 function joinArrays(...groupArrays) {
-  const joinedArray = [];
-  for (const singleArray of groupArrays) {
-    joinedArray.push(...singleArray);
-  }
-  return joinedArray;
+  return [].concat(...groupArrays);
 }
 
 export {
