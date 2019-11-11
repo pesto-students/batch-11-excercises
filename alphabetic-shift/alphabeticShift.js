596a84ffc3a438a6ccbd0334a3059327a9b486d9
yashpandit <yashpandit1995@gmail.com>
2019-11-11 18:51:05 +0530

    Solve alphabeticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,11 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(alphabets) {
+  const shiftedAlphabets = [];
+  for (let i = 0; i < alphabets.length; i += 1) {
+    const charCode = alphabets.charCodeAt(i) + 1;
+    shiftedAlphabets.push(String.fromCharCode(charCode));
+  }
+  return shiftedAlphabets.join('');
 }
 
 export {
