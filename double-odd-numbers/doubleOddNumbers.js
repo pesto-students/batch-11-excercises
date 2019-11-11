d3e4967e7d0a03560eca14984b2ba7b4e18ada18
hitesh <hiteshchauhan.669@gmail.com>
2019-11-11 19:17:11 +0530

    Solved double-odd-numbers

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,8 +1,6 @@
-
-function doubleOddNumbers(...args) {
-  return args;
-}
-
-export {
-  doubleOddNumbers,
+const doubleOddNumbers = (collection) => {
+  const oddCollection = collection.filter((i) => i % 2 !== 0);
+  return oddCollection.map((i) => i * 2);
 };
+
+export { doubleOddNumbers };
