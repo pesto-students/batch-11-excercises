dd2ec4658285869da854889f2a64204d3538ae9b
yashpandit <yashpandit1995@gmail.com>
2019-11-11 15:47:29 +0530

    Solve doubleOddNumbers

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,12 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(numbers) {
+  const oddNumberDoubled = [];
+  for (const number of numbers) {
+    if (number % 2 !== 0) {
+      oddNumberDoubled.push(number * 2);
+    }
+  }
+  return oddNumberDoubled;
 }
 
 export {
