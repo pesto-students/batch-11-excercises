2569cb1c8f9ce804fca4e63f47d7ec713ada0b8a
Sourav Das <souravdasslg95@gmail.com>
2019-11-11 15:24:58 +0530

    Exercise named constImmutable is completed

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,6 +1,13 @@
 
-function constImmutable(...args) {
-  return args;
+function constImmutable() {
+  const account = {
+    username: 'pesto',
+    password: 'initialPassword',
+  };
+  const propertyConfiguration = { writable: false };
+  Object.defineProperties(account, 'password', propertyConfiguration);
+  account.password = 's3cret';
+  return account.password;
 }
 
 export {
