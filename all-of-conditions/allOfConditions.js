f0107d12accf389804afcfedb14c3825781aff15
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-11 17:00:22 +0530

    Fix test for all-of-conditions
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,6 +1,13 @@
 
-function allOfConditions(...args) {
-  return args;
+function allOfConditions(...predicates) {
+  return function runPredicates(inputToPredicates) {
+    for (const predicate of predicates) {
+      if (predicate(inputToPredicates) === false) {
+        break;
+      }
+      predicate(inputToPredicates);
+    }
+  };
 }
 
 export {
