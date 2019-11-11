943f047e6ef5333a529ba4e7793876fb46f921d8
Basant Soni <basantech89@gmail.com>
2019-11-11 06:05:27 +0530

    Fix account-dataset and square-numbers-array tests

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,14 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...items) {
+  items.forEach((item) => {
+    if (typeof item !== 'number') {
+      throw new Error('Invalid Type: Array items should be a number');
+    }
+  });
+  const squares = items.map((item) => {
+    return item ** 2;
+  });
+  return squares;
 }
 
 export {
--- square-numbers-array/squareNumbersArray.test.js
+++ square-numbers-array/squareNumbersArray.test.js
@@ -3,9 +3,9 @@ import { squareNumbersArray } from './squareNumbersArray';
 describe('squareNumbersArray', () => {
   test('should throw an error if array of numbers is not passed as argument', () => {
     const myArray = [1, 2, 'string', 4];
-    expect(() => squareNumbersArray(...myArray)).toThrow('My custom error');
+    expect(() => squareNumbersArray(...myArray)).toThrow('Invalid Type: Array items should be a number');
   });
   test('should return square array', () => {
-    
+
   });
 });
