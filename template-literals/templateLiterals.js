e426495194f447d25ce2cb0647de1ebe8aed08c9
hitesh <hiteshchauhan.669@gmail.com>
2019-11-11 16:08:08 +0530

    Solved some eslint errors

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,8 +1,10 @@
+function templateLiterals() {
+  const player1 = 'Rooney';
+  const player2 = 'Ronaldo';
+  const player3 = 'Messi';
+  const palyer4 = 'Pogba';
 
-function templateLiterals(...args) {
-  return args;
+  return `There are 4 people on the football team. Their names are ${player1}, ${player2}, ${player3}, ${palyer4}.`;
 }
 
-export {
-  templateLiterals,
-};
+export { templateLiterals };
