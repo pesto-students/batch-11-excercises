1c7a58294a04a1210c204887262facf2667df834
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-11 16:02:17 +0530

    Fixed placeInMiddle problem with all test cases

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,7 +1,12 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(array, valuesToInsertInMiddle) {
+  const midpoint = Math.floor(array.length / 2);
+  const arrayBeforeMidpoint = array.slice(0, midpoint);
+  const arrayAfterMidpoint = array.slice(midpoint);
+  return arrayBeforeMidpoint.concat(valuesToInsertInMiddle).concat(arrayAfterMidpoint);
 }
+
+placeInMiddle([1, 2, 6, 7], [3, 4, 5]);
 
 export {
   placeInMiddle,
