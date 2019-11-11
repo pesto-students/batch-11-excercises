dd2ef7208e48babe0afd64f438be0db0096b65fd
Harish Rana <r.shivi@yahoo.com>
2019-11-11 22:11:52 +0530

    Solve alphabetic shift problem

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,16 @@
+const ALPHABET_ARRAY = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-function alphabeticShift(...args) {
-  return args;
+// Assuming a proper alphabet will be provided.
+const isUpperCase = (alphabet) => alphabet.charCodeAt() >= 65 && alphabet.charCodeAt() <= 90;
+
+function alphabeticShift(alphabets) {
+  const completeAlphabets = ALPHABET_ARRAY;
+  const shiftAlphabet = (alphabet) => {
+    const alphabetCode = alphabet.toLowerCase().charCodeAt();
+    const incrementedChar = completeAlphabets[((alphabetCode - 97) % 26) + 1];
+    return isUpperCase(alphabet) ? incrementedChar.toUpperCase() : incrementedChar;
+  };
+  return alphabets.split('').map(shiftAlphabet).join('');
 }
 
 export {
