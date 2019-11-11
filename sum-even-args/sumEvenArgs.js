2664b25ba9337bbb77846c2fe240a160a8551cfe
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-11 16:20:00 +0530

    Fixed sumEvenArgs problem with all test cases

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,8 +1,14 @@
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(args) {
+  const numbers = [...arguments];
+  const evenNumbers = numbers.filter(isEven);
+  const sumOfEvenNumbers = evenNumbers.reduce((acc, curr) => acc + curr, 0);
+  return sumOfEvenNumbers;
 }
 
+function isEven(number) {
+  return number % 2 === 0;
+}
 export {
   sumEvenArgs,
 };
