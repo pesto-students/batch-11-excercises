ea7553a77c35b73cf149279051cfed666031231a
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-11 16:52:07 +0530

    Fixed joinArrays problem with all test cases

--- join-arrays/joinArrays.js
+++ join-arrays/joinArrays.js
@@ -1,6 +1,11 @@
 
-function joinArrays(...args) {
-  return args;
+function joinArrays() {
+  const nestedArrays = [...arguments];
+  let joinedArray = [];
+  for (let i = 0; i < nestedArrays.length; i++) {
+    joinedArray = joinedArray.concat([...nestedArrays[i]]);
+  }
+  return joinedArray;
 }
 
 export {
