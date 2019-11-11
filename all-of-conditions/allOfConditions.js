6d2c445cce07e850f3b5f7a8b8dd5a80cabc6d12
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-11 16:22:07 +0530

    exercise allofcondition solved

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,6 +1,11 @@
-
-function allOfConditions(...args) {
-  return args;
+function allOfConditions(cbFunctions) {
+  return function checkFn(input) {
+    for (let i = 0; i < cbFunctions.length; i += 1) {
+      if (cbFunctions[i](input) === false) {
+        return;
+      }
+    }
+  };
 }
 
 export {
