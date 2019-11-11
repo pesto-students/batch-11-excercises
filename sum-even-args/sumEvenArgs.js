4bbc2daebe850b3c28b701bbc6f7c80a9b149f3b
yashpandit <yashpandit1995@gmail.com>
2019-11-11 16:18:53 +0530

    Solve sumEvenArgs

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-param-reassign */
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(...numbers) {
+  return numbers.filter((number) => number % 2 === 0).reduce((total, number) => {
+    total += number;
+    return total;
+  }, 0);
 }
 
 export {
