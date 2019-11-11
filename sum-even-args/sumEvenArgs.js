39155c4dea59c10931657d8ab7c2ea2b8631607f
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-11 19:13:19 +0530

    Fix test for sum-even-args
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,14 +1,7 @@
 
 function sumEvenArgs(...args) {
   const isEven = (element) => element % 2 === 0;
-  const add = (sum, element) => {
-    let currentTotal = sum;
-    currentTotal += element;
-    return currentTotal;
-  };
-  // const add = (sum, element) => sum += element;
-  // Arrow function should not return assigment,
-  // what is the correct fix?
+  const add = (partialSum, element) => partialSum + element;
   return args.filter(isEven).reduce(add, 0);
 }
 
