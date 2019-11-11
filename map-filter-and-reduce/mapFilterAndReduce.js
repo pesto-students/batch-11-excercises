139897a6b645753f825ef5f5641f7ad938176804
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-11 16:39:39 +0530

    Fixed mapFilterAndReduce problem with all test cases

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,11 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(members) {
+  const membersWithSmallNames = members.filter((member) => member.firstName.length < 5);
+  const membersNameLength = membersWithSmallNames.reduce((acc, curr) => {
+    acc[curr.firstName] = curr.firstName.length;
+    return acc;
+  }, {});
+  return membersNameLength;
 }
 
 export {
