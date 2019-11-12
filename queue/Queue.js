a7552cdbe1a7965df9c3937f997628bfb1ff0fe2
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-12 17:02:16 +0530

    Solved Queue with all test cases

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,48 @@
 
-function queue(...args) {
-  return args;
+class Queue {
+  constructor() {
+    this.linkedList = [];
+  }
+
+  enqueue(value) {
+    this.linkedList.push(value);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      const removedValue = this.linkedList.shift();
+      return removedValue;
+    }
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      const START = 0;
+      return this.linkedList[START];
+    }
+  }
+
+  isEmpty() {
+    return this.linkedList.length === 0;
+  }
+
+  toString(formatter) {
+    if (typeof formatter === 'function') {
+      let formattedData = [];
+      for (let value of this.linkedList) {
+        formattedData.push(formatter(value));
+      }
+      return formattedData.join(',');
+    } else {
+      return this.linkedList.toString();
+    }
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
