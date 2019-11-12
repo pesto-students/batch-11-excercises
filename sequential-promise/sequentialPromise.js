5586b9629935611925202b0d4234d538029f2668
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-12 09:17:59 +0530

    completed sequential amd timeout

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,6 +1,14 @@
 
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise(promiseFnList) {
+  const sequentialisePromise = (promiseList) => promiseList.reduce((acc, promiseFn) => acc.then((resolvedAccStr) => promiseFn(resolvedAccStr)), Promise.resolve(''));
+
+  const resultPromise = new Promise((resolve) => {
+    sequentialisePromise(promiseFnList).then((resolvedString) => {
+      resolve(resolvedString);
+    });
+  });
+
+  return resultPromise;
 }
 
 export {
--- timeout/timeout.js
+++ timeout/timeout.js
@@ -1,6 +1,9 @@
 
-function timeout(...args) {
-  return args;
+function timeout(name) {
+  const timeOutPromise = new Promise((resolve) => {
+    setTimeout(() => resolve(`Hello ${name}`), 300);
+  });
+  return timeOutPromise;
 }
 
 export {
