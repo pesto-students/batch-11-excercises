7e61aa734fcaa84f3b1e9e35fd5fd2fc5defc9b4
hitesh <hiteshchauhan.669@gmail.com>
2019-11-13 00:29:11 +0530

    Solved animal-sort problem

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,8 +1,17 @@
-
-function animalSort(...args) {
-  return args;
-}
-
-export {
-  animalSort,
+const comparator = (animal1, animal2) => {
+  if (animal1.numberOfLegs === animal2.numberOfLegs) {
+    return animal1.name > animal2.name;
+  }
+  return animal1.numberOfLegs - animal2.numberOfLegs;
 };
+const animalSort = (animalList) => {
+  if (animalList.length === 0) {
+    const emptyList = [];
+    return emptyList;
+  }
+  const sortedAnimal = animalList.sort(comparator);
+
+  return sortedAnimal;
+};
+
+export { animalSort };
