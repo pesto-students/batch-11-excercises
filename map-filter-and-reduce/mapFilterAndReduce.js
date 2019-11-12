c98151659087845641f1ed9ec31a7588907c05f4
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-12 09:50:33 +0530

    completed 3 s1 problems

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,14 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(names) {
+  const nameList = names.map((nameObj) => nameObj.firstName);
+  const isLengthLessThanFive = (name) => name.length < 5;
+  const filteredNameList = nameList.filter(isLengthLessThanFive);
+  const reducedNameObj = filteredNameList.reduce((acc, name) => {
+    acc[name] = name.length;
+    return acc;
+  }, {});
+
+  return reducedNameObj;
 }
 
 export {
--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,9 @@
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(...numbers) {
+  const isEvenNumber = (number) => number % 2 === 0;
+  const evenNumbers = numbers.filter(isEvenNumber);
+  const sumOfEvenNumbers = evenNumbers.reduce((acc, number) => acc + number, 0);
+  return sumOfEvenNumbers;
 }
 
 export {
