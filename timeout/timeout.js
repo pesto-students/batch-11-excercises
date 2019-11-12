6134b46116be9c4f4ae1658c743363025c6d69cb
yashpandit <yashpandit1995@gmail.com>
2019-11-12 09:27:05 +0530

    Solve timeout

--- timeout/timeout.js
+++ timeout/timeout.js
@@ -1,6 +1,8 @@
 
-function timeout(...args) {
-  return args;
+function timeout(string) {
+  return new Promise((resolve) => {
+    setTimeout(resolve(`Hello ${string}`), 300);
+  });
 }
 
 export {
