17deeac68ac98a5b4c04244a9eda1d12c9890fee
yashpandit <yashpandit1995@gmail.com>
2019-11-12 09:23:25 +0530

    Solve placeInMiddle

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,11 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(firstArray, secondArray) {
+  const middleElement = Math.floor(firstArray.length / 2);
+  return [
+    ...firstArray.slice(0, middleElement),
+    ...secondArray,
+    ...firstArray.slice(middleElement),
+  ];
 }
 
 export {
