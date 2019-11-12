a4463cd35b3ca0c808c6774d1e166afd81de29ca
Basant Soni <basantech89@gmail.com>
2019-11-12 07:51:03 +0530

    Fix promiseAllProps test

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,17 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(promises) {
+  // Promises.all(promises);
+  const resolveObj = {};
+  return new Promise((resolve, reject) => {
+    for (const key of Object.keys(promises)) {
+      promises[key]
+        .then(result => {
+          resolveObj[key] = result;
+        })
+        .catch(err => reject(err));
+    }
+    resolve(resolveObj);
+  });
 }
 
 export {
