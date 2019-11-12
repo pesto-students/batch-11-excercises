660efa479a291fa98ab251fea9ece02f11816ef2
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-12 07:15:30 +0530

    Completed person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,25 @@
 
-function person(...args) {
-  return args;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 }
 
+Person.prototype.addDobDigits = function addDobDigits() {
+  const sanitizedDobString = this.dateOfBirth.replace('/', '');
+  const sumChars = (string) => [...string].reduce((sum, currentChar) => {
+    const number = parseInt(currentChar, 10);
+    if (!isNaN(number)) {
+      sum += +number;
+    }
+    return sum;
+  }, 0);
+
+  return sumChars(sanitizedDobString);
+};
+
 export {
-  person,
+  Person,
 };
