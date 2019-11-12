d03b28aac410d251ea89d212ff81c2db0ab9c202
Harish Rana <r.shivi@yahoo.com>
2019-11-12 22:03:36 +0530

    Solve animal sort problem

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,23 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(animals) {
+  const compareByName = (animal1Name, animal2Name) => {
+    let sorted = 0;
+    let i = 0;
+    while (sorted === 0 && i < Math.min(animal1Name.length, animal2Name.length)) {
+      sorted = animal1Name[i].charCodeAt() - animal2Name[i].charCodeAt();
+      i += 1;
+    }
+    return sorted;
+  };
+
+  const compareByLegs = (animal1, animal2) => {
+    const difference = animal1.numberOfLegs - animal2.numberOfLegs;
+    if (difference === 0) {
+      return compareByName(animal1.name, animal2.name);
+    }
+    return difference;
+  };
+  return animals.sort(compareByLegs);
 }
 
 export {
