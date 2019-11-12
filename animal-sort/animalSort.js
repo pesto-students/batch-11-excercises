a570cbecbb4f2fdd4d1a94b92067f76214c9ef1e
yashpandit <yashpandit1995@gmail.com>
2019-11-12 21:36:06 +0530

    Solve animalSort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,12 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(animals) {
+  const animalComparator = ((value1, value2) => {
+    if (value1.numberOfLegs === value2.numberOfLegs) {
+      return value1.name > value2.name;
+    }
+    return value1.numberOfLegs > value2.numberOfLegs;
+  });
+  return animals.sort(animalComparator);
 }
 
 export {
