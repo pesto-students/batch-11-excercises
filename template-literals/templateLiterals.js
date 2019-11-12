631bad4c0e4c18225e4d2d448a500b23418b59d2
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-12 15:25:22 +0530

    templateliterals done

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,4 +1,5 @@
-const templateLiterals = () => `There are 4 people on the football team. Their names are Rooney, Ronaldo, Messi, Pogba.`
+/* eslint-disable quotes */
+const templateLiterals = () => `There are 4 people on the football team. Their names are Rooney, Ronaldo, Messi, Pogba.`;
 
 export {
   templateLiterals,
