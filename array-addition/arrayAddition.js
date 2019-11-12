73f367a722300aac8ffb92876a35b26ae02ec88f
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-13 01:41:45 +0530

    Solved arrayAddition with all test cases

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,7 +1,33 @@
 
-function arrayAddition(...args) {
-  return args;
+function arrayAddition(firstArray, secondArray) {
+  if (firstArray.length === 0 || secondArray.length === 0) {
+    throw new Error('Arrays cannot be empty!');
+  }
+  if (!Array.isArray(firstArray) || !Array.isArray(secondArray)) {
+    throw new Error('Inputs can only be arrays!');
+  }
+  if (!isNumeric(firstArray) || !isNumeric(secondArray)) {
+    throw new Error('Arrays can only contain numbers!');
+  }
+
+  let addedArray = [];
+  if (firstArray.length < secondArray.length) {
+    let addedArray = addOneByOne(firstArray, secondArray);
+    let remainingArray = secondArray.slice(firstArray.length);
+    return addedArray.concat(remainingArray);
+  } else {
+    return addOneByOne(firstArray, secondArray);
+  }
 }
+
+const addOneByOne = (firstArray, secondArray) => {
+  let addedArray = [];
+  for (let i = 0; i < firstArray.length; i++) {
+    addedArray.push(firstArray[i] + secondArray[i]);
+  }
+  return addedArray;
+}
+const isNumeric = (array) => array.every((element) => typeof element === 'number');
 
 export {
   arrayAddition,
