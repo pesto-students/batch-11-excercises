c3c0e7f052c93d3683e78cd0fb018a34f645a7be
Harish Rana <r.shivi@yahoo.com>
2019-11-12 22:25:30 +0530

    Solve args string problem

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,6 +1,7 @@
 
-function argsString(...args) {
-  return args;
+function argsString(placeHolder, words) {
+  const predicate = (accumulator, word) => accumulator.replace('{}', word);
+  return words.reduce(predicate, placeHolder);
 }
 
 export {
