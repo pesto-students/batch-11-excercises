aea98ab1cc9381dcccc582480a288dab2428d716
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-13 00:54:52 +0530

    argsString solved with test cases

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,31 @@
+function legsComparer(a, b) {
+  const legA = a.numberOfLegs;
+  const legB = b.numberOfLegs;
+  const nameA = a.name.toLowerCase();
+  const nameB = b.name.toLowerCase();
 
-function animalSort(...args) {
-  return args;
+  let comparison = 0;
+  if (legA > legB) {
+    comparison = 1;
+  } else if (legA < legB) {
+    comparison = -1;
+  }
+  if (legA === legB) {
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  }
+  return comparison;
+}
+
+function animalSort(animalsArray) {
+  if (animalsArray.length < 1) {
+    return animalsArray;
+  }
+  return animalsArray.sort(legsComparer);
 }
 
 export {
