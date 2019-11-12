0ed0cbc044f2c745f86ae0117cb82fd49f60fd99
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-12 19:34:55 +0530

    Solved animal-sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,19 @@
-
-function animalSort(...args) {
-  return args;
+function animalSort(animals) {
+  return animals.sort((firstEl, secondEl) => {
+    if (firstEl.numberOfLegs > secondEl.numberOfLegs) {
+      return 1;
+    }
+    if (firstEl.numberOfLegs < secondEl.numberOfLegs) {
+      return -1;
+    }
+    if (firstEl.name > secondEl.name) {
+      return 1;
+    }
+    if (firstEl.name < secondEl.name) {
+      return -1;
+    }
+    return 0;
+  });
 }
 
 export {
