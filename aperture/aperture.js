7af73c1f496313fe91d6a622d84e28795148077e
Harish Rana <r.shivi@yahoo.com>
2019-11-12 22:12:08 +0530

    Solve aperture problem

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,14 @@
 
-function aperture(...args) {
-  return args;
+function aperture(tupleLength, numbers) {
+  let upperBound = tupleLength;
+  let lowerBound = 0;
+  const tuples = [];
+  while (upperBound <= numbers.length) {
+    tuples.push(numbers.slice(lowerBound, upperBound));
+    upperBound += 1;
+    lowerBound += 1;
+  }
+  return tuples;
 }
 
 export {
