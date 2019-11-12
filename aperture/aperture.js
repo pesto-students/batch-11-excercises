c78bf1eedb71d548d599420cbb187f48f9db5dea
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-13 01:00:15 +0530

    Solved aperture with all test cases

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,17 @@
 
-function aperture(...args) {
-  return args;
+function aperture(sizeOfSubArray, array) {
+  if (sizeOfSubArray > array.length) {
+    return [];
+  } else {
+    let subArrays = [];
+    for (let i = 0; i < array.length; i++) {
+      if (i + sizeOfSubArray <= array.length) {
+        let subArray = array.slice(i, i + sizeOfSubArray);
+        subArrays.push(subArray);
+      }
+    }
+    return subArrays;
+  }
 }
 
 export {
