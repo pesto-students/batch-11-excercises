5f1b64f63758069d3b576e9d443fd4b66cf5697e
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-12 19:35:05 +0530

    Animal Sort using comparison

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,33 @@
+function compareLegs(a, b) {
+  const legsA = a.numberOfLegs;
+  const legsB = b.numberOfLegs;
 
-function animalSort(...args) {
-  return args;
+  const nameA = a.name.toLowerCase();
+  const nameB = b.name.toLowerCase();
+  let comparison = 0;
+  if (legsA > legsB) {
+    comparison = 1;
+  } else if (legsA < legsB) {
+    comparison = -1;
+  }
+
+  if (legsA === legsB) {
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  }
+
+  return comparison;
+}
+
+function animalSort(animalArray) {
+  if (animalArray.length === 0) {
+    return [];
+  }
+  return animalArray.sort(compareLegs);
 }
 
 export {
