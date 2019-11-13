1247bfa4d66b47b8d7c3567a67dfc59ebd5c7ae9
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-13 17:12:52 +0530

    negativeindex solved

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,36 @@
+function negativeIndex(array) {
+  if (!Array.isArray(array)) {
+    throw new TypeError('Expected an array');
+  }
 
-function negativeIndex(...args) {
-  return args;
+  return new Proxy(array, {
+    get(target, prop) {
+      if (typeof prop === 'symbol') {
+        return target[prop];
+      }
+      const index = Number(prop);
+
+      if (Number.isNaN(index)) {
+        return Reflect.get(target, prop);
+      }
+
+      return target[index < 0 ? target.length + index : index];
+    },
+    set(target, prop, value) {
+      if (typeof prop === 'symbol') {
+        return target[prop];
+      }
+      const index = Number(prop);
+
+      if (Number.isNaN(index)) {
+        return Reflect.set(target, prop, value);
+      }
+
+      target[index < 0 ? target.length + index : index] = value;
+
+      return true;
+    },
+  });
 }
 
 export {
