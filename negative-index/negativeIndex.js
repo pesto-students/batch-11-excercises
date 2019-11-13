64237e1fd2310bf55c9842705528a1f0112dce67
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-13 19:19:20 +0530

    Solve Negative Index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,49 @@
+import { type } from "os";
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(numberArray) {
+  if (!Array.isArray(numberArray)) {
+    throw new TypeError('Only Arrays are supported');
+  }
+  const handler = {
+    get: (target, index) => {
+      // eslint-disable-next-line no-param-reassign
+      if (typeof index === 'symbol') {
+        return Reflect.get(target, index);
+      }
+      if(isNaN(index)) {
+        return Reflect.get(target, index);
+      }
+      index = parseInt(index, 10);
+      if (index < 0) {
+        // eslint-disable-next-line no-param-reassign
+        index += target.length;
+        return target[index];
+      }
+      return target[index];
+    },
+    set: (target, index, value) => {
+
+      if (typeof index === 'symbol') {
+        return Reflect.get(target, index, value);
+      }
+      if(isNaN(index)) {
+        return Reflect.get(target, index, value);
+      }
+      // eslint-disable-next-line no-param-reassign
+      index = parseInt(index, 10);
+      // eslint-disable-next-line no-param-reassign
+      if (index < 0) {
+        // eslint-disable-next-line no-param-reassign
+        index += target.length;
+        target[index] = value;
+        return target;
+      }
+      // eslint-disable-next-line no-param-reassign
+      target[index] = value;
+      return target;
+    },
+  };
+  return new Proxy(numberArray, handler);
 }
 
 export {
