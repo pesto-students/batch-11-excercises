23a01f570406566e716585f7b732e234a4f706cc
Harish Rana <r.shivi@yahoo.com>
2019-11-13 11:44:32 +0530

    Solve generatorIterale partially

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,6 +1,23 @@
 
-function generatorIterable(...args) {
-  return args;
+function generatorIterable() {
+  let i = 1;
+  return {
+    [Symbol.iterator]: () => ({
+      next: () => {
+        if (i < 6) {
+          i += 1;
+          return {
+            value: i - 1,
+            done: false,
+          };
+        }
+        return {
+          value: undefined,
+          done: true,
+        };
+      },
+    }),
+  };
 }
 
 export {
