51b162d8aa5bdd4351ea42e2baf52274ed68c71b
yashpandit <yashpandit1995@gmail.com>
2019-11-13 16:36:06 +0530

    Solve setDefaultProperty

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,36 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(randomArray) {
+  if (!Array.isArray(randomArray)) {
+    throw new TypeError('Only arrays are supported');
+  }
+
+  const handleNegativeIndex = {
+    get(target, prop) {
+      const index = parseInt(prop, 10);
+      if (typeof target[Symbol.iterator] !== 'function') {
+        throw new TypeError('Only arrays are supported');
+      }
+      if (index < 0) {
+        return target[target.length + index];
+      }
+      return target[index];
+    },
+
+    set(target, prop, value) {
+      const array = target;
+      const index = parseInt(prop, 10);
+      if (index < 0) {
+        array[array.length + index] = value;
+        return array;
+      }
+      array[index] = value;
+      return array;
+    },
+
+  };
+
+  const proxyArray = new Proxy(randomArray, handleNegativeIndex);
+  return proxyArray;
 }
 
 export {
--- set-default-property/setDefaultProperty.js
+++ set-default-property/setDefaultProperty.js
@@ -1,6 +1,15 @@
 
-function setDefaultProperty(...args) {
-  return args;
+function setDefaultProperty(object, defaultValue) {
+  const handleDefaultProp = {
+    get(target, prop) {
+      if (!(prop in target)) {
+        return defaultValue;
+      }
+      return target[prop];
+    },
+  };
+
+  return new Proxy(object, handleDefaultProp);
 }
 
 export {
