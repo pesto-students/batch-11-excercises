a02fcdefa7d5ff40d6c46b192f28adf9e4e6461b
Sourav Das <souravdasslg95@gmail.com>
2019-11-13 22:24:46 +0530

    Completed exercise named search sorted array

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,6 +1,10 @@
 
-function searchSortedMatrix(...args) {
-  return args;
+function searchSortedMatrix({ search, matrix }) {
+  // eslint-disable-next-line arrow-body-style
+  const joinedMatrix = matrix.reduce((partialArray, currentArray) => {
+    return partialArray.concat(currentArray);
+  }, []);
+  return joinedMatrix.indexOf(search) > -1;
 }
 
 export {
