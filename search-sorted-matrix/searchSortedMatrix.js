2488c37dec34882d2efcd785e3a77627da214abf
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-13 19:34:33 +0530

    searchSoetedMatrix solved with test cases

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,6 +1,16 @@
-
-function searchSortedMatrix(...args) {
-  return args;
+function searchSortedMatrix(object) {
+  const {
+    search,
+    matrix,
+  } = object;
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      if (matrix[i][j] === search) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 export {
