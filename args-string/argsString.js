f6f01b2a95f5e27a4b34d9849d2e94231466dfaf
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-13 08:26:21 +0530

    completed args-string

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,6 +1,8 @@
 
-function argsString(...args) {
-  return args;
+function argsString(inputString, argStrings) {
+  const argsReplacedString = argStrings.reduce((acc, string) => acc.replace(/{}/, string), inputString);
+
+  return argsReplacedString;
 }
 
 export {
