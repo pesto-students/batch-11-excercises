a6381ad371d97b830fe9e5b124da5afae86abd93
hitesh <hiteshchauhan.669@gmail.com>
2019-11-14 02:04:19 +0530

    Solved searchSorted problem

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,8 +1,15 @@
-
-function searchSortedMatrix(...args) {
-  return args;
-}
-
-export {
-  searchSortedMatrix,
+const searchSortedMatrix = ({ search, matrix }) => {
+  let findRowIndex = 1;
+  if (matrix.length > 1) {
+    findRowIndex = matrix.findIndex((i) => i[0] > search);
+  }
+  const row = matrix[findRowIndex - 1];
+  for (let i = 0; i < row.length; i += 1) {
+    if (row[i] === search) {
+      return true;
+    }
+  }
+  return false;
 };
+
+export { searchSortedMatrix };
