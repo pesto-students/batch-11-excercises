d575d4536caa500a8c8a9119ad8b4dd75dbb0da0
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-13 09:00:52 +0530

    Completed aperture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,14 @@
 
-function aperture(...args) {
-  return args;
+function aperture(apertureLength, inputArray) {
+  const resultArray = inputArray.reduce((acc, element) => {
+    const index = inputArray.indexOf(element);
+    if (inputArray.length - index + 1 > apertureLength) {
+      acc.push(inputArray.slice(index, index + apertureLength));
+    }
+    return acc;
+  }, []);
+
+  return resultArray;
 }
 
 export {
