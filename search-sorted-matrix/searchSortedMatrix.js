84136c17eb6bf1b0288ec202b136c016a64b4de4
Basant Soni <basantech89@gmail.com>
2019-11-14 01:34:23 +0530

    Fix search-sorted-matrix test

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,6 +1,16 @@
 
-function searchSortedMatrix(...args) {
-  return args;
+function searchSortedMatrix({ search, matrix }) {
+  for (const row of matrix) {
+    for (const number of row) {
+      if (search < number) {
+        return false;
+      }
+      if (!(search > number)) {
+        return row.includes(search);
+      }
+    }
+  }
+  return false;
 }
 
 export {
