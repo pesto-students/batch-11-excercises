2fc47e7a42a3336169a981ff399084dbbe10db9e
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-13 09:52:35 +0530

    Solved few more questions

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,11 @@
-
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(object) {
+  return new Promise(() => {
+    const returnObject = {};
+    for (const [key, value] of Object.entries(object)) {
+      returnObject[key] = value.then((promiseValue) => promiseValue);
+    }
+    return returnObject;
+  });
 }
 
 export {
--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,6 +1,9 @@
-
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise(inputPromises) {
+  const accumulator = (acc, el) => acc.then((str) => el(str));
+  const sequentialPromiseList = inputPromises.reduce(accumulator, Promise.resolve(1));
+  return new Promise((resolve) => {
+    sequentialPromiseList.then((resolvedString) => resolve(resolvedString));
+  });
 }
 
 export {
