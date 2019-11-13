41789523d8087a04cd5cb62c5065f7839fc2b5f2
Harish Rana <r.shivi@yahoo.com>
2019-11-13 11:23:23 +0530

    Solve fibonacciIter problem

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,32 @@
 
-function fibonacciIter(...args) {
-  return args;
+function fibonacciIter() {
 }
+
+fibonacciIter[Symbol.iterator] = () => {
+  let previousFibonacciNumber = 0;
+  let penultimateFibonacciNumber = 1;
+  let i = 0;
+  return {
+    next() {
+      if (i === 0) {
+        previousFibonacciNumber = 1;
+        i += 1;
+        return {
+          value: 1,
+          done: false,
+        };
+      }
+      i += 1;
+      const fibNumber = previousFibonacciNumber + penultimateFibonacciNumber;
+      penultimateFibonacciNumber = previousFibonacciNumber;
+      previousFibonacciNumber = fibNumber;
+      return {
+        value: fibNumber,
+        done: false,
+      };
+    },
+  };
+};
 
 export {
   fibonacciIter,
