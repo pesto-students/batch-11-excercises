5aa81840d6faf55fdb6cfdddd94440c7f00b2d92
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-13 17:04:21 +0530

    Solved till negativeIndex

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,12 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(object) {
+  return new Proxy(object, {
+    get(obj, prop) {
+      if (prop in obj) {
+        return obj[prop];
+      }
+      throw TypeError('Unknown property');
+    },
+  });
 }
 
 export {
--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,37 @@
-
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(array) {
+  if (!Array.isArray(array)) {
+    throw TypeError('Only arrays are supported');
+  }
+  return new Proxy(array, {
+    get(obj, prop) {
+      if (typeof prop === 'symbol') {
+        return obj[prop];
+      }
+      const index = parseInt(prop, 10);
+      if (isNaN(index)) {
+        return obj[prop];
+      }
+      let newIndex = index;
+      if (prop < 0) {
+        newIndex = obj.length + index;
+      }
+      return obj[newIndex];
+    },
+    set(obj, prop, value) {
+      if (typeof prop === 'symbol') {
+        return Reflect.set(obj, prop, value);
+      }
+      const index = parseInt(prop, 10);
+      if (isNaN(index)) {
+        return Reflect.set(obj, prop, value);
+      }
+      let newIndex = index;
+      if (prop < 0) {
+        newIndex = obj.length + index;
+      }
+      return Reflect.set(obj, newIndex, value);
+    },
+  });
 }
 
 export {
