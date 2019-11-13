853b94a3ee4edca59e58f263a7e8fd4267a46ac2
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-13 23:05:01 +0530

    Completed exercise types with all test cases

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,8 +1,21 @@
-
-function sudanFn(...args) {
-  return args;
+function sundanFn(object) {
+  const {
+    n,
+    x,
+    y,
+  } = object;
+  if (n === 0 && y !== 0) {
+    return x + y;
+  }
+  if (y === 0 && n >= 0) {
+    return x;
+  }
+  if (y > 0 && n >= 0) {
+    return sundanFn(n - 1, sundanFn(n, x, y - 1), sundanFn(n, x, y - 1) + y);
+  }
+  return -1;
 }
 
 export {
-  sudanFn,
+  sundanFn,
 };
