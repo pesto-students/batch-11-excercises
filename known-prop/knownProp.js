256b377eada3e3008d1715b09971bc8fd07fdc97
Basant Soni <basantech89@gmail.com>
2019-11-13 22:15:11 +0530

    Fix known-prop test

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,14 @@
+const handler = {
+  get(target, propName) {
+    if (propName in target) {
+      return target[propName];
+    }
+    throw new TypeError(`Unknown Property ${propName}`);
+  },
+};
 
-function knownProp(...args) {
-  return args;
+function knownProp(obj) {
+  return new Proxy(obj, handler);
 }
 
 export {
