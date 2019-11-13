8d6a8c35624e4a76a386ff3ef3b5e5c4c97ecc79
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-13 11:46:58 +0530

    arrayCuberoottojson solved with test cases

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,23 @@
-
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(numbers) {
+  const isArray = Array.isArray(numbers);
+  const isNumber = (number) => {
+    if (number && (Number.isInteger(Number(number)) || (number && number === Infinity))) {
+      return true;
+    }
+    return false;
+  };
+  if (!isArray) {
+    throw new Error('Not an Array');
+  }
+  if (numbers.every(isNumber) === false) {
+    throw new Error('Not every element in array is integer');
+  }
+  const jsonOfCubeRoot = {};
+  numbers.map((number) => {
+    jsonOfCubeRoot[number] = Math.cbrt(number);
+    return jsonOfCubeRoot;
+  });
+  return jsonOfCubeRoot;
 }
 
 export {
