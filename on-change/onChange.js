4d6913b7a6a4832bbeb1165de973488979d5e155
Harish Rana <r.shivi@yahoo.com>
2019-11-13 22:52:58 +0530

    Solve private props

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,6 +1,17 @@
 
-function onChange(...args) {
-  return args;
+function onChange(obj, changeCb) {
+  const proxy = new Proxy(obj, {
+    set(...args) {
+      console.log(args[1]);
+      changeCb();
+      return Reflect.set(...args);
+    },
+    defineProperty(...args) {
+      changeCb();
+      return Reflect.defineProperty(...args);
+    },
+  });
+  return proxy;
 }
 
 export {
--- private-props/privateProps.js
+++ private-props/privateProps.js
@@ -1,6 +1,24 @@
 
-function privateProps(...args) {
-  return args;
+function privateProps(obj, privacyFilter) {
+  const proxy = new Proxy(obj, {
+    set(target, property, value, ...args) {
+      if (privacyFilter(property)) {
+        throw TypeError('Can\'t set property "_private"');
+      }
+      return Reflect.set(target, property, value, ...args);
+    },
+    ownKeys(target) {
+      const keys = Reflect.ownKeys(target);
+      return keys.filter((key) => !privacyFilter(key));
+    },
+    has(target, key, ...args) {
+      if (privacyFilter(key)) {
+        return false;
+      }
+      return Reflect.has(target, key, ...args);
+    },
+  });
+  return proxy;
 }
 
 export {
