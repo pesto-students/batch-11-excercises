64b98997638586d79bb6b811d8c02c335b5d4337
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-13 16:04:42 +0530

    Solve Known Prop

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,15 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(object) {
+  const handler = {
+    get: (target, name) => {
+      if (!(name in target)) {
+        throw new TypeError('Unknown Property is accessed.');
+      }
+      return target[name];
+    },
+  };
+
+  return new Proxy(object, handler);
 }
 
 export {
