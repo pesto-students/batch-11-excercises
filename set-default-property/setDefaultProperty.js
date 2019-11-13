e46254e189506ebe8f0e79af430efc5b60e6dfd6
Basant Soni <basantech89@gmail.com>
2019-11-13 22:28:40 +0530

    Fix set-default-property test

--- set-default-property/setDefaultProperty.js
+++ set-default-property/setDefaultProperty.js
@@ -1,6 +1,17 @@
 
-function setDefaultProperty(...args) {
-  return args;
+function setDefaultProperty(obj, defaultValue) {
+  const handler = {
+    get(target, propName) {
+      if (!(propName in target)) {
+        target[propName] = defaultValue;
+      }
+      return target[propName];
+    },
+    set(target, propName, val) {
+      target[propName] = val;
+    },
+  };
+  return new Proxy(obj, handler);
 }
 
 export {
