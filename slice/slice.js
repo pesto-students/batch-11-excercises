b405164d7958d8f8ca98b926ba320cf620f59473
yashpandit <yashpandit1995@gmail.com>
2019-11-13 20:12:45 +0530

    Make PR

--- slice/slice.js
+++ slice/slice.js
@@ -1,6 +1,13 @@
 
-function slice(...args) {
-  return args;
+function slice(from, to, input) {
+  const letters = [];
+  if (typeof input === 'string') {
+    letters = input.split('');
+  }
+  if (Array.isArray(input)) {
+    
+  }
+  return Error('input should be an array or a string');
 }
 
 export {
