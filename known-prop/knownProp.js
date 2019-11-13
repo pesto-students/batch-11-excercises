1735a036ea2d5e7466df4f866cf94533afd949be
Lordtien <nitinpawarme@gmail.com>
2019-11-13 16:25:20 +0530

    Solve known-prop

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,7 +1,12 @@
 
-function knownProp(...args) {
-  return args;
-}
+const knownProp = (proxiedObject) => new Proxy(proxiedObject, {
+  get: (object, property) => {
+    if (property in object) {
+      return object[property];
+    }
+    throw TypeError('/Unknown property');
+  },
+});
 
 export {
   knownProp,
