92a7ca610b90b8b8ef9127c0f638cf0ad4a31b92
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-13 07:48:25 +0530

    completed array cube root to json

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,25 @@
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(numbers) {
+  let cubeRootNumberObj = {};
+  const isNumber = (number) => {
+    if (typeof number === 'number' || !isNaN(parseInt(number, 10))) {
+      return true;
+    }
+    return false;
+  };
+
+  if (!(numbers instanceof Array)) {
+    throw new Error(`Expected an array received ${typeof numbers}`);
+  }
+  cubeRootNumberObj = numbers.reduce((acc, number) => {
+    if (!isNumber(number)) {
+      throw new Error(`Expected numbers in array received ${typeof number}`);
+    }
+    acc[number] = Math.cbrt(number);
+    return acc;
+  }, {});
+
+  return cubeRootNumberObj;
 }
 
 export {
