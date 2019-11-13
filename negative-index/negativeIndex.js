ff6bc239596398d4424429e20c623f71c4a89487
Harish Rana <r.shivi@yahoo.com>
2019-11-13 20:12:40 +0530

    Solve negative index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,32 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(arr) {
+  if (!(arr instanceof Array)) {
+    throw TypeError('Please pass array as argument. Only arrays are supported');
+  }
+  const proxy = new Proxy(arr, {
+    get(target, property) {
+      if (typeof property !== 'symbol') {
+        const index = parseInt(property, 10);
+        if (index < 0) {
+          const actualIndex = index + target.length;
+          return target[actualIndex];
+        }
+        return Reflect.get(target, property);
+      }
+      return Reflect.get(target, property);
+    },
+    set(target, property, value) {
+      const index = parseInt(property, 10);
+      if (index < 0) {
+        const actualIndex = index + target.length;
+        // eslint-disable-next-line no-param-reassign
+        target[actualIndex] = value;
+        return true;
+      }
+      return Reflect.set(target, property, value);
+    },
+  });
+  return proxy;
 }
 
 export {
