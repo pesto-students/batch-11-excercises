ecca12b0e19c36faaa89a5875bd37a59e929fa31
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-14 09:06:30 +0530

    Solved splitEvery problem with all test cases passing

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,7 +1,22 @@
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(subListSize, list) {
+  if (subListSize <= 0 && list.length === 0) {
+    throw new Error('First Argument cannot be zero or negative')
+  }
+  const totalList = [...list];
+  const subLists = [];
+  while (totalList.length > 0) {
+    let subList = totalList.splice(0, subListSize);
+    if (isEveryElementString(subList)) {
+      subLists.push(subList.join(''));
+    } else {
+      subLists.push(subList);
+    }
+  }
+  return subLists;
 }
+
+const isEveryElementString = (array) => array.every((element) => typeof element === 'string');
 
 export {
   splitEvery,
