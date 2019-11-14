47cd2dc5c94ef8807c36e375efdb95b21d5f3b64
Basant Soni <basantech89@gmail.com>
2019-11-14 14:36:32 +0530

    Fix substitution-cipher test

--- substitution-cipher/README.md
+++ substitution-cipher/README.md
@@ -1,3 +1,4 @@
+
 ## Instructions
 
 Substitution Cipher
--- substitution-cipher/substitutionCipher.js
+++ substitution-cipher/substitutionCipher.js
@@ -1,6 +1,25 @@
+const getCipherLetter = (plainChar) => {
+  const upperCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerCases = 'abcdefghijklmnopqrstuvwxyz';
+  const isUpperCase = plainChar.toUpperCase() === plainChar;
+  const plainIndex = isUpperCase ? upperCases.indexOf(plainChar) : lowerCases.indexOf(plainChar);
+  let cipherIndex = plainIndex % 2 ? plainIndex + 6 : plainIndex + 4;
+  if (cipherIndex > 25) {
+    cipherIndex -= 26;
+  }
+  return isUpperCase ? upperCases[cipherIndex] : lowerCases[cipherIndex];
+};
 
-function substitutionCipher(...args) {
-  return args;
+function substitutionCipher(plainText) {
+  let cipherText = '';
+  const nonAlphabeticPattern = /[a-zA-Z]/;
+  [...plainText].forEach((plainChar) => {
+    if (nonAlphabeticPattern.test(plainChar)) {
+      const cipherLetter = getCipherLetter(plainChar);
+      cipherText += cipherLetter;
+    } else cipherText += plainChar;
+  });
+  return cipherText;
 }
 
 export {
