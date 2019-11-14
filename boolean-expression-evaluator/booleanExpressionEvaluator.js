8f99ab3747884c1507063cb908ac088194d61fff
yashpandit <yashpandit1995@gmail.com>
2019-11-14 16:14:51 +0530

    Solve hammingDistance

--- boolean-expression-evaluator/booleanExpressionEvaluator.js
+++ boolean-expression-evaluator/booleanExpressionEvaluator.js
@@ -1,5 +1,8 @@
 
 function booleanExpressionEvaluator(input) {
+  if (typeof input !== 'string') {
+    throw new Error('Input must be a string.');
+  }
   return Boolean(input);
 }
 
--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,20 @@
 
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(firstString, secondString) {
+  if (firstString.length !== secondString.length) {
+    throw new Error('The length of both the strings should be equal.');
+  }
+  const firstStringCharacters = firstString.split('');
+  const secondStringCharacters = secondString.split('');
+  const { length } = firstStringCharacters;
+  let changesRequired = 0;
+
+  for (let i = 0; i < length; i += 1) {
+    if (firstStringCharacters[i] !== secondStringCharacters[i]) {
+      changesRequired += 1;
+    }
+  }
+
+  return changesRequired;
 }
 
 export {
