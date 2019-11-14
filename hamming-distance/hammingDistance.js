fb608aff2c1955b66aace0e8965b39433bb95ce9
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-14 17:02:34 +0530

    Solved hamming-distance

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,13 @@
-
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(string1, string2) {
+  if (string1.length !== string2.length) {
+    throw Error('String are of different length');
+  }
+  return [...string1].reduce((acc, val, index) => {
+    if (val !== string2[index]) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
 }
 
 export {
