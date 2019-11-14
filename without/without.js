7060c4c48c7434de71015782608b3323c89bfa80
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-14 12:58:53 +0530

    exercise without sloved

--- without/without.js
+++ without/without.js
@@ -1,6 +1,20 @@
+function without(argFirst, argSecond) {
+  const resultingArray = [];
+  const isMinusZero = (value) => 1 / value === -Infinity;
+  argSecond.map((value) => {
+    if (value === 0 || isMinusZero(value)) {
+      return resultingArray.push(value);
+    }
+    if (Number.isNaN(value)) {
+      return [...resultingArray];
+    }
+    if (argFirst.indexOf(value) === -1) {
+      resultingArray.push(value);
+    }
 
-function without(...args) {
-  return args;
+    return [...resultingArray];
+  });
+  return resultingArray;
 }
 
 export {
