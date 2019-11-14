37fa61f239a6d27ac0592a72ed3139e5340b827e
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-14 09:02:08 +0530

    Temporary changes

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,18 @@
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(sliceLength, collection) {
+  const resultArray = [];
+  if (sliceLength < 1) {
+    throw new Error('Slice length should be a positive number');
+  }
+  collection.reduce((acc, currentValue, index, actualArray) => {
+    acc.push(collection.splice(0, sliceLength));
+
+    console.log(collection);
+    //actualArray = actualArray.slice(sliceLength, actualArray.length);
+    return resultArray;
+  }, resultArray);
+
+  return resultArray;
 }
 
 export {
--- theatre/theatre.js
+++ theatre/theatre.js
@@ -1,6 +1,19 @@
-
-function theatre(...args) {
-  return args;
+function theatre(theatreQueue) {
+  let balance = 0;
+  for (let i = 0; i < theatreQueue.length; i += 1) {
+    const ele = theatreQueue[i];
+    if (ele > 10) {
+      if ((ele - 10) <= balance) {
+        balance -= ele - 10;
+      } else {
+        return false;
+      }
+    } else {
+      balance += ele;
+    }
+    console.log(balance);
+  }
+  return true;
 }
 
 export {
