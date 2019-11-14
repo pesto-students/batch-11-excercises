d3f35033109a2d16e95d8a71d1bde4aab054fd0a
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-14 15:22:49 +0530

    partial without

--- without/without.js
+++ without/without.js
@@ -1,6 +1,11 @@
 
-function without(...args) {
-  return args;
+function without(filterArray, actualArray) {
+  const withoutFilter = (number) => {
+    const index = filterArray.indexOf(number);
+    return !Object.is(number, filterArray[index]);
+  };
+
+  return actualArray.filter(withoutFilter);
 }
 
 export {
