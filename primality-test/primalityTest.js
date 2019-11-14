a16d10a612b88547dc4f2219836d360b21801aab
Basant Soni <basantech89@gmail.com>
2019-11-14 22:28:35 +0530

    Fix primality test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,19 @@
+function primeFactorization(number) {
+  let dividend = number;
+  const factors = [];
+  let divisor = 2;
+  while (dividend > 1) {
+    if (dividend % divisor === 0) {
+      factors.push(divisor);
+      dividend /= divisor;
+    } else divisor += 1;
+  }
+  return factors;
+}
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  const factors = primeFactorization(number);
+  return factors.length === 1;
 }
 
 export {
