83a29014f8b323f293369f06823925ee715d798a
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-14 16:43:35 +0530

    Hamming Distance

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,6 +1,24 @@
 
-function binarySearch(...args) {
-  return args;
+function binarySearch(numberArray, searchElement) {
+  let low = 0;
+  let high = numberArray.length - 1;
+  let mid = Math.floor((low + high) / 2);
+  console.log(mid);
+
+  if (numberArray.length === 0) {
+    return -1;
+  }
+  if (searchElement === numberArray[mid]) {
+    return numberArray.indexOf(searchElement);
+  }
+
+  if(searchElement > numberArray[low]) {
+    low = mid + 1;
+    binarySearch(numberArray.slice(low), searchElement);
+  }else{
+    high = mid - 1;
+    binarySearch(numberArray.slice(low, high + 1), searchElement);
+  }
 }
 
 export {
--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,16 @@
 
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(string1,string2) {
+  if(string1.length !== string2.length){
+    throw new Error('Strings are of different length');
+  }
+  let count = 0;
+
+  for(let i = 0; i <= string1.length; i += 1) {
+    if(string1[i] !== string2[i]) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 export {
