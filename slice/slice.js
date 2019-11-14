2a4fc133b6e1b66533bbb1c937023ecd6ccfb2c5
yashpandit <yashpandit1995@gmail.com>
2019-11-14 11:40:20 +0530

    Solve types

--- slice/slice.js
+++ slice/slice.js
@@ -1,13 +1,13 @@
 
-function slice(from, to, input) {
-  const letters = [];
-  if (typeof input === 'string') {
-    letters = input.split('');
-  }
-  if (Array.isArray(input)) {
-    
-  }
-  return Error('input should be an array or a string');
+function slice() {
+  // let letters = [];
+  // if (typeof input === 'string') {
+  //   letters = input.split('');
+  // }
+  // if (Array.isArray(input)) {
+  //   letters = input;
+  // }
+  // return Error('input should be an array or a string');
 }
 
 export {
