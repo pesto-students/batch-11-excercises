3a183fed9af313ad7819c823a27b4fec94946787
Sourav Das <souravdasslg95@gmail.com>
2019-11-14 14:55:28 +0530

    WIP Split Every

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,12 @@
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(length, elements) {
+  if (length < 1) throw new Error('Length should be 1 or greater than 1');
+  const elementsArray = Array.from(elements);
+  const splitArray = [];
+  while (elementsArray[0]) {
+    splitArray.push(elements.splice(0, length));
+  }
+  return splitArray;
 }
 
 export {
