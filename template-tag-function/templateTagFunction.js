e641945fd8847162536f6d96112bf9afecf640bb
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-14 16:42:22 +0530

    Fix test for template-tag-function
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- template-tag-function/templateTagFunction.js
+++ template-tag-function/templateTagFunction.js
@@ -1,8 +1,16 @@
 
-function templateTagFunction(...args) {
-  return args;
+const sanitizedHTML = (unsanitizedString) => unsanitizedString
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;');
+
+function templateTagFunction(template) {
+  return template.map(sanitizedHTML).join('');
 }
 
+const callTemplateTagFunction = () => templateTagFunction`The expression 5 > 4 is "true" & 3 < 1 is false`;
+
 export {
-  templateTagFunction,
+  callTemplateTagFunction,
 };
