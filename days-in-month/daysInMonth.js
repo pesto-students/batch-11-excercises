bf6ad047d8d8842c832aa7c0822e1c64b414e7e1
Basant Soni <basantech89@gmail.com>
2019-11-14 15:00:57 +0530

    Fix days-in-month test

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,6 +1,18 @@
+const isLeapYear = (year) => {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    }
+    return true;
+  }
+  return false;
+};
 
-function daysInMonth(...args) {
-  return args;
+function daysInMonth(year, month) {
+  if (month === 2) {
+    return isLeapYear(year) ? 29 : 28;
+  }
+  return month % 2 ? 31 : 30;
 }
 
 export {
