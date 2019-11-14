5e59858cb9d89c3ec8f803bbe3d927d9cce66ac2
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-14 08:21:16 +0530

    Solved few questions

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,8 +1,29 @@
-
-function sudanFn(...args) {
-  return args;
+function sundanFn({
+  n,
+  x,
+  y,
+}) {
+  if (typeof n === 'number' && n === 0) {
+    return x + y;
+  }
+  if (typeof y === 'number' && y === 0) {
+    return x;
+  }
+  return sundanFn({
+    n: n - 1,
+    x: sundanFn({
+      n,
+      x,
+      y: y - 1,
+    }),
+    y: sundanFn({
+      n,
+      x,
+      y: y - 1,
+    }) + y,
+  });
 }
 
 export {
-  sudanFn,
+  sundanFn,
 };
--- to-pairs/toPairs.js
+++ to-pairs/toPairs.js
@@ -1,6 +1,9 @@
-
-function toPairs(...args) {
-  return args;
+function toPairs(object) {
+  const pairs = [];
+  for (const [key, value] of Object.entries(object)) {
+    pairs.push([key, value]);
+  }
+  return pairs;
 }
 
 export {
