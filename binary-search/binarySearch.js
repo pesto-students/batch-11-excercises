e12c1dc77339028d044d22be33a456cb40111eff
yashpandit <yashpandit1995@gmail.com>
2019-11-14 22:29:33 +0530

    Solve binarySearch

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-console */
 
-function binarySearchAlgo(array, left, right, target) {
+function search(array, left, right, target) {
   const mid = Math.floor((left + right) / 2);
   if (array[mid] === target) {
     return mid;
@@ -9,22 +8,19 @@ function binarySearchAlgo(array, left, right, target) {
     return -1;
   }
   if (target > array[mid]) {
-    return binarySearchAlgo(array, mid + 1, right, target);
+    return search(array, mid + 1, right, target);
   }
-  return binarySearchAlgo(array, left, mid - 1, target);
+  return search(array, left, mid - 1, target);
 }
 
-function binarySearch(object, target, comparator) {
-  if (object.length === undefined) {
-    object.sort(comparator);
-    const numbers = [];
-    Object.keys(object).forEach((key) => {
-      numbers.push(key);
-    });
-    numbers.sort();
-    return binarySearchAlgo(numbers, 0, numbers.length, target.key);
+function binarySearch(array, target, comparator) {
+  if (typeof array[0] === 'object') {
+    const objects = [...array];
+    objects.sort(comparator);
+    const numbers = objects.map((object) => object.key);
+    return search(numbers, 0, numbers.length, target.key);
   }
-  return binarySearchAlgo(object, 0, object.length, target);
+  return search(array, 0, array.length, target);
 }
 
 export {
