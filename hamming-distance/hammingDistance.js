b321e99283f8c4d08d78fbfdfede7ec2d0d18a4b
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-14 16:52:33 +0530

    completed hamming distance

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,16 @@
 
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(firstString, secondString) {
+  if (firstString.length !== secondString.length) {
+    throw new Error('Strings with same length should be provided');
+  }
+  const distance = [...firstString].reduce((acc, currentChar, index) => {
+    if (currentChar !== secondString.charAt(index)) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
+
+  return distance;
 }
 
 export {
