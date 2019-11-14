30acfc35204f8146c9598fb2efba82ed05adcc3f
Harish Rana <r.shivi@yahoo.com>
2019-11-14 16:07:21 +0530

    Solve split every partially

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,27 @@
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(splitLength, list) {
+  if (splitLength < 1) {
+    throw Error('Split length should be positive');
+  }
+  const isListString = typeof list === 'string';
+  const arr = isListString ? list.split('') : list;
+  const accumulator = [];
+  arr.forEach((item, index) => {
+    const splittedArrIndex = Math.floor(index / splitLength);
+    const splittedArray = accumulator[splittedArrIndex] ? accumulator[splittedArrIndex] : [];
+    let splittedValue;
+    if (isListString) {
+      splittedArray.split('').push(item);
+      const concatedList = splittedArray;
+      splittedValue = concatedList.join('');
+    } else {
+      splittedArray.push(item);
+      splittedValue = splittedArray;
+    }
+    accumulator[splittedArrIndex] = splittedValue;
+    // return accumulator;
+  });
+  return accumulator;
 }
 
 export {
