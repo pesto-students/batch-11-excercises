3565b2523031dd6fdf41df1718b43b09e42c89b3
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-16 03:29:38 +0530

    Fix test for counting-anagrams
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- counting-anagrams/countingAnagrams.js
+++ counting-anagrams/countingAnagrams.js
@@ -1,6 +1,30 @@
 
-function countingAnagrams(...args) {
-  return args;
+const alphabetize = (string) => string
+  .split('')
+  .sort()
+  .join('');
+
+function anagramGrouper(words) {
+  return words
+    .filter((word) => word.length >= 2)
+    .reduce((anagrams, word) => {
+      if (Object.keys(anagrams).includes(alphabetize(word))) {
+        return {
+          ...anagrams,
+          [alphabetize(word)]: [...anagrams[alphabetize(word)], word],
+        };
+      }
+      return { ...anagrams, [alphabetize(word)]: [word] };
+    }, {});
+}
+
+function countingAnagrams(mayContainAnagrams) {
+  const words = mayContainAnagrams.split(' ');
+  const groupedWords = anagramGrouper(words);
+  return Object
+    .values(groupedWords)
+    .filter((matchingWords) => matchingWords.length > 1)
+    .reduce((count) => count + 1, 0);
 }
 
 export {
