92d482b4310fb08ccf1b33f840700e233ac76bae
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-16 03:58:53 +0530

    solved primalityTest

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,7 +1,11 @@
-
-function primalityTest(...args) {
-  return args;
-}
+const primalityTest = (number) => {
+  for (let i = 2, squareRoot = Math.sqrt(number); i <= squareRoot; i += 1) {
+    if (number % 2 === 0) {
+      return false;
+    }
+  }
+  return number > 1;
+};
 
 export {
   primalityTest,
