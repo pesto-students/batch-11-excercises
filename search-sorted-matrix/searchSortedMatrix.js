f3d0f776e879c3eb6de0bd244fcd9a6a64fa8ea2
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-16 00:20:43 +0530

    Fix test for search-sorted-matrix
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,7 +1,11 @@
 
-function searchSortedMatrix(...args) {
-  return args;
-}
+const flatten = (array) => array.reduce((flattened, element) => flattened.concat(element), []);
+
+const searchSortedMatrix = (sortedMatrix) => {
+  const { search, matrix } = sortedMatrix;
+  return flatten(matrix).includes(search);
+};
+
 
 export {
   searchSortedMatrix,
