5a3b5afde22dd0278548db2dc9f9ff2737ac6425
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-15 23:52:35 +0530

    Fix test for counting-anagrams
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- counting-anagrams/countingAnagrams.js
+++ counting-anagrams/countingAnagrams.js
@@ -1,32 +1,26 @@
-
 const alphabetize = (string) => string
   .split('')
   .sort()
   .join('');
 
-function anagramGrouper(words) {
-  return words
-    .filter((word) => word.length >= 2)
-    .reduce((anagrams, word) => {
-      if (Object.keys(anagrams).includes(alphabetize(word))) {
-        return {
-          ...anagrams,
-          [alphabetize(word)]: [...anagrams[alphabetize(word)], word],
-        };
-      }
-      return { ...anagrams, [alphabetize(word)]: [word] };
-    }, {});
-}
+const anagramGrouper = (words) => words
+  .filter((word) => word.length >= 2)
+  .reduce((anagrams, word) => {
+    if (Object.keys(anagrams).includes(alphabetize(word))) {
+      return {
+        ...anagrams,
+        [alphabetize(word)]: [...anagrams[alphabetize(word)], word],
+      };
+    }
+    return { ...anagrams, [alphabetize(word)]: [word] };
+  }, {});
 
-function countingAnagrams(mayContainAnagrams) {
+const countingAnagrams = (mayContainAnagrams) => {
   const words = mayContainAnagrams.split(' ');
   const groupedWords = anagramGrouper(words);
-  return Object
-    .values(groupedWords)
+  return Object.values(groupedWords)
     .filter((matchingWords) => matchingWords.length > 1)
     .reduce((count) => count + 1, 0);
-}
-
-export {
-  countingAnagrams,
 };
+
+export { countingAnagrams };
