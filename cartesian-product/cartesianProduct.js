5b187fc27dac150134dc2525d0f78f1fc7d152d3
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-15 23:53:31 +0530

    Fix test for cartesian-product
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- cartesian-product/cartesianProduct.js
+++ cartesian-product/cartesianProduct.js
@@ -1,7 +1,20 @@
 
-function cartesianProduct(...args) {
-  return args;
-}
+const calculateCartesianProduct = (firstSet, secondSet) => {
+  const result = [];
+  for (const elementOfFirstSet of firstSet) {
+    for (const elementOfSecondSet of secondSet) {
+      result.push([elementOfFirstSet, elementOfSecondSet]);
+    }
+  }
+  return result;
+};
+
+const cartesianProduct = (firstSet, secondSet) => {
+  if (!(Array.isArray(firstSet) && Array.isArray(secondSet))) {
+    return null;
+  }
+  return calculateCartesianProduct(firstSet, secondSet);
+};
 
 export {
   cartesianProduct,
