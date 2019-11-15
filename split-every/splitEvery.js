eb4466faa859662d3e473d66b771eb6d2288fed8
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-16 00:49:04 +0530

    Fix test for split-every
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,19 @@
 
-function splitEvery(...args) {
-  return args;
+function* chunk(iterable, n) {
+  for (let i = 0; i < iterable.length; i += n) {
+    yield iterable.slice(i, i + n);
+  }
 }
 
-export {
-  splitEvery,
-};
+const split = (size, iterable) => [...chunk(iterable, size)];
+
+function splitEvery(size, iterable) {
+  if (size <= 0) {
+    throw new Error(
+      `Received invalid size: ${size}. Please enter a positive integer.`,
+    );
+  }
+  return split(size, iterable);
+}
+
+export { splitEvery };
