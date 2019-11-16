5d95dce4b13902dfbca74a7f58f5e08a8c98085d
Basant Soni <basantech89@gmail.com>
2019-11-16 21:35:19 +0530

    Fix triple-and-filter test

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,8 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(numbers) {
+  const triple = number => number * 3;
+  const divisibleBy5 = number => number % 5 === 0;
+  return numbers.map(triple).filter(divisibleBy5);
 }
 
 export {
