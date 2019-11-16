53864f0d64b5fb07a3b01dd06c2c27d1ae1c53f8
hitesh <hiteshchauhan.669@gmail.com>
2019-11-17 03:36:58 +0530

    solved tiles but with for loop

--- tiles/tiles.js
+++ tiles/tiles.js
@@ -1,8 +1,17 @@
-
-function tiles(...args) {
-  return args;
-}
-
-export {
-  tiles,
+const tiles = ({ small, big, target }) => {
+  const smallTileSize = 1;
+  const bigTileSize = 5;
+  if (small * smallTileSize + big * bigTileSize < target) {
+    return false;
+  }
+  for (let i = 1; i <= big; i += 1) {
+    for (let j = 1; j <= small; j += 1) {
+      if (i * 5 + j === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
+
+export { tiles };
