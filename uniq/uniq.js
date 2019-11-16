4ff563e92c09eed394448843f8c146df5ba99dd1
hitesh <hiteshchauhan.669@gmail.com>
2019-11-17 01:19:28 +0530

    completed uniq exercise

--- uniq/uniq.js
+++ uniq/uniq.js
@@ -1,8 +1,34 @@
-
-function uniq(...args) {
-  return args;
-}
-
-export {
-  uniq,
+const negativeZeroHandler = (acc, x) => {
+  for (const zero of acc) {
+    if (1 / zero === -Infinity && 1 / x === Infinity) {
+      return true;
+    }
+    if (1 / zero === Infinity && 1 / x === -Infinity) {
+      return true;
+    }
+  }
+  return false;
 };
+const flattening = (list) => {
+  let flat = [];
+  for (let i = 0; i < list.length; i += 1) {
+    if (Array.isArray(list[i])) {
+      flat = flat.concat(flattening(list[i]));
+    } else {
+      flat.push(list[i]);
+    }
+  }
+  return flat;
+};
+const uniq = (list) => {
+  const flattenedList = flattening(list);
+  const uniqeValues = flattenedList.reduce((acc, val) => {
+    if (!acc.includes(val) || negativeZeroHandler(acc, val)) {
+      acc.push(val);
+    }
+    return acc;
+  }, []);
+  return uniqeValues;
+};
+
+export { uniq };
