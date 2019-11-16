9c666d232a3a5d41ac5995ea5dd441af2658055d
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-16 16:08:24 +0530

    Fix test for union
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- union/union.js
+++ union/union.js
@@ -1,6 +1,12 @@
 
-function union(...args) {
-  return args;
+const isNegativeZero = (number) => Object.is(-0, number);
+
+function union(firstSet, secondSet) {
+  const combined = [...firstSet, ...secondSet];
+  if (firstSet.some(isNegativeZero) || secondSet.some(isNegativeZero)) {
+    return combined;
+  }
+  return [...new Set(combined)];
 }
 
 export {
