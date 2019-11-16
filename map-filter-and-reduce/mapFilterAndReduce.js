caa804ec28d339787cef46ba3aa4efc60bad63db
Basant Soni <basantech89@gmail.com>
2019-11-16 19:16:06 +0530

    Fix map-filter-and-reduce test

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,9 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(array) {
+  const getName = nameObj => nameObj.firstName;
+  const predicate = name => name.length < 5;
+  const createNameObj = (obj, name) => ({ [name]: name.length, ...obj });
+  return array.map(getName).filter(predicate).reduce(createNameObj, {});
 }
 
 export {
