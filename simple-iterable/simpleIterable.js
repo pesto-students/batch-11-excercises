34c4e8ae0f9102145e6f36f7ff943d346dbdea78
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-16 20:06:59 +0530

    Completed SimpleIterable with all test cases passing

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,24 @@
-
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  const iterator = {
+    state: 0,
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      this.state++;
+      if (this.state === 6) {
+        return {
+          done: true
+        }
+      } else {
+        return {
+          value: this.state,
+          done: false
+        }
+      }
+    }
+  };
+  return iterator;
 }
 
 export {
