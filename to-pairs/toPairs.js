64bf53f8833b8babe97e80ebcf98381656cfb41e
hitesh <hiteshchauhan.669@gmail.com>
2019-11-17 01:43:54 +0530

    to-pairs exercise completed

--- to-pairs/toPairs.js
+++ to-pairs/toPairs.js
@@ -1,8 +1,11 @@
-
-function toPairs(...args) {
-  return args;
-}
-
-export {
-  toPairs,
+const toPairs = (object) => {
+  const pair = [];
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    const eachPair = [key, object[key]];
+    pair.push(eachPair);
+  }
+  return pair;
 };
+
+export { toPairs };
