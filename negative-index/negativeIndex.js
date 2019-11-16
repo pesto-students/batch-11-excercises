b73b2c8a1a5a465cb804440fe0f6b095cfd0de29
hitesh <hiteshchauhan.669@gmail.com>
2019-11-16 16:33:49 +0530

    solved negative-index exercise

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,8 +1,31 @@
+const negativeIndex = (array) => {
+  if (!Array.isArray(array)) {
+    throw TypeError('Only arrays are supported as an argument');
+  }
 
-function negativeIndex(...args) {
-  return args;
-}
+  const proxy = new Proxy(array, {
+    get(target, property) {
+      const index = parseInt(property, 10);
+      if (property < 0) {
+        const positiveIndex = index + target.length;
+        return target[positiveIndex];
+      }
+      return target[property];
+    },
 
-export {
-  negativeIndex,
+    set(target, property, value) {
+      const index = parseInt(property, 10);
+      if (index < 0) {
+        const positiveIndex = index + target.length;
+        target[positiveIndex] = value;
+        return true;
+      }
+      target[index] = value;
+      return target;
+    },
+  });
+
+  return proxy;
 };
+
+export { negativeIndex };
