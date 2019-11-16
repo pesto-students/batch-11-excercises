69f26557208692807cbbadfcd0cfd0b8e9b52846
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-16 19:20:24 +0530

    Finished 7 out of 9

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,31 @@
-
-function rangeIter(...args) {
-  return args;
+function rangeIter(lowerBound, upperBound) {
+  if (lowerBound === undefined || upperBound === undefined) {
+    throw TypeError('undefined is not a number');
+  }
+  if (typeof lowerBound === 'string' || typeof upperBound === 'string') {
+    throw TypeError('a is not a number');
+  }
+  const iterable = {
+    [Symbol.iterator]() {
+      this.lb = lowerBound;
+      this.ub = upperBound;
+      return this;
+    },
+    next() {
+      if (this.lb <= this.ub) {
+        const nextObj = {
+          value: this.lb,
+          done: false,
+        };
+        this.lb += 1;
+        return nextObj;
+      }
+      return {
+        done: true,
+      };
+    },
+  };
+  return iterable;
 }
 
 export {
