bc7ed05caabf1b563f93f589cff6cfe9e95ea4f0
hitesh <hiteshchauhan.669@gmail.com>
2019-11-16 18:28:51 +0530

    completed private-props exercise

--- private-props/privateProps.js
+++ private-props/privateProps.js
@@ -1,8 +1,24 @@
-
-function privateProps(...args) {
-  return args;
-}
-
-export {
-  privateProps,
+const privateProps = (object, privateFilter) => {
+  const proxy = new Proxy(object, {
+    ownKeys(target) {
+      const keys = Object.keys(target);
+      const filteredPrivate = keys.filter((key) => !privateFilter(key));
+      return filteredPrivate;
+    },
+    has(target, key) {
+      if (privateFilter(key)) {
+        return false;
+      }
+      return Reflect.has(target, key);
+    },
+    set(target, property, value) {
+      if (privateFilter(property)) {
+        throw TypeError(`Can't set property ${property}`);
+      }
+      return Reflect.set(target, property, value);
+    },
+  });
+  return proxy;
 };
+
+export { privateProps };
