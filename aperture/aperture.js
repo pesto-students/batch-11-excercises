a4e902fc57b97675b55a5688ce3c2c676e9ab914
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-16 23:25:49 +0530

    Solved 7 of 9

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,19 @@
+function aperture(apertureLength, array) {
+  const returnFirstN = (filterElement, filterIndex) => {
+    if (filterIndex < apertureLength) {
+      return true;
+    }
+    return false;
+  };
 
-function aperture(...args) {
-  return args;
+  return array.reduce((acc, reduceElement, reduceIndex) => {
+    const filterResult = array.slice(reduceIndex).filter(returnFirstN);
+
+    if (filterResult.length === apertureLength) {
+      acc.push(filterResult);
+    }
+    return acc;
+  }, []);
 }
 
 export {
