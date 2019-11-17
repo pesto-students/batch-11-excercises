e536a9734e7042e0434f44d9198c8a8ed1acc4b8
Sourav Das <souravdasslg95@gmail.com>
2019-11-17 14:50:33 +0530

    Coplete Question 1

---
+++ question-1/script.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+function changeStyle() {
+  // eslint-disable-next-line no-undef
+  const paragraph = document.getElementById('text');
+  paragraph.style.color = 'yellow';
+  paragraph.style.background = 'black';
+  paragraph.style.fontSize = '60px';
+  paragraph.style.fontWeight = 'bold';
+  paragraph.style.textAlign = 'center';
+}
