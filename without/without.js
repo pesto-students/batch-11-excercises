65d800749e4c7421a9e3be22a26074c49c32afe2
Basant Soni <basantech89@gmail.com>
2019-11-17 20:45:11 +0530

    Fix without test

--- without/without.js
+++ without/without.js
@@ -1,6 +1,9 @@
 
-function without(...args) {
-  return args;
+function without(withouts, list) {
+  const flatWithoutList = withouts.flat();
+  const flatList = list.flat();
+  const withoutList = item => flatWithoutList.every(number => !Object.is(number, item));
+  return flatList.filter(withoutList);
 }
 
 export {
