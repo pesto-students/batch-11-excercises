69594fdd6a876df292c1adfed9468b645091b69c
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-17 16:46:35 +0530

    completed some day 10 s 2 problems

--- deep-copy-object/deepCopyObject.js
+++ deep-copy-object/deepCopyObject.js
@@ -1,6 +1,17 @@
 
-function deepCopyObject(...args) {
-  return args;
+function deepCopyObject(object) {
+  let copiedObject = {};
+  const isObjectInstance = (obj) => obj instanceof Object;
+
+  if (!isObjectInstance(object)) {
+    return object;
+  }
+  copiedObject = Object.entries(object).reduce((acc, [key, value]) => {
+    acc[key] = deepCopyObject(value);
+    return acc;
+  }, {});
+
+  return copiedObject;
 }
 
 export {
