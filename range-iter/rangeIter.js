d6f42e24e268bd2eef5f1e965f9e076ba1eb5623
hitesh <hiteshchauhan.669@gmail.com>
2019-11-17 19:16:27 +0530

    completed range-iter exercise

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,8 +1,30 @@
-
-function rangeIter(...args) {
-  return args;
-}
-
-export {
-  rangeIter,
+const badArgumentHandler = (...range) => {
+  if (range.length < 2) {
+    throw TypeError(
+      'Undefined is not a number. Pass number as range for arguments',
+    );
+  }
+  if (typeof range[0] !== 'number' || typeof range[1] !== 'number') {
+    throw TypeError('Pass number as range for arguments');
+  }
 };
+const rangeIter = (...range) => {
+  badArgumentHandler(...range);
+  let startValue = range[0];
+  const endValue = range[1];
+  if (startValue > endValue) {
+    return [];
+  }
+  if (startValue === endValue) {
+    return [startValue];
+  }
+
+  const iterable = [];
+  while (startValue <= endValue) {
+    iterable.push(startValue);
+    startValue += 1;
+  }
+  return iterable;
+};
+
+export { rangeIter };
