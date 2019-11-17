0f968c618ef33de5bd820e5a0d2d5037017fe112
Basant Soni <basantech89@gmail.com>
2019-11-17 19:55:15 +0530

    Fix xprod test

--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,6 +1,12 @@
 
-function xprod(...args) {
-  return args;
+function xprod(list1, list2) {
+  const makePairs = (list, item1) => {
+    const addToList = item2 => list.push([item1, item2]);
+    list2.forEach(addToList);
+    return list;
+  };
+  const pairs = list1.reduce(makePairs, []);
+  return pairs;
 }
 
 export {
