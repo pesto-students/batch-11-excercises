4bdd6eca38afce2ab6778bfbc7ef343d714ea216
hitesh <hiteshchauhan.669@gmail.com>
2019-11-17 16:19:57 +0530

    completed simple-iterable exercise

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,8 +1,22 @@
-
-function simpleIterable(...args) {
-  return args;
-}
-
-export {
-  simpleIterable,
+const simpleIterable = () => {
+  const iterable = {
+    [Symbol.iterator]() {
+      let it = 0;
+      const iterator = {
+        next() {
+          if (it < 5) {
+            it += 1;
+            for (let i = 1; i <= 5; i += 1) {
+              return { value: it, done: false };
+            }
+          }
+          return { value: undefined, done: true };
+        },
+      };
+      return iterator;
+    },
+  };
+  return iterable;
 };
+
+export { simpleIterable };
