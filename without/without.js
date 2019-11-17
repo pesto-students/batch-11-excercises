bc7c3de9ad26cb4ff7f5249fde5e45f6787da4e5
Harish Rana <r.shivi@yahoo.com>
2019-11-17 22:57:44 +0530

    Solve without partially

--- without/without.js
+++ without/without.js
@@ -1,6 +1,14 @@
 
-function without(...args) {
-  return args;
+function without(operandArray1, operandArray2) {
+  const predicate = (item) => {
+    const indexInArray1 = operandArray1.indexOf(item);
+    const valueInArray1 = operandArray1[indexInArray1];
+    if (valueInArray1) {
+      return !Object.is(valueInArray1, item);
+    }
+    return true;
+  };
+  return operandArray2.filter(predicate);
 }
 
 export {
