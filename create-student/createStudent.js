ca0dc9c429de7f43909c9dd43fd3bb14bcbe5ffb
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-17 18:30:33 +0530

    completed some d6 s1 problem

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,16 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent(inputObject) {
+  const { likesES2015, likesJavaScript } = inputObject || {};
+  if (inputObject === undefined) {
+    return 'The student likes JavaScript and ES2015';
+  }
+  if (!likesES2015 && likesJavaScript === undefined) {
+    return 'The student likes JavaScript!';
+  }
+  if (!likesJavaScript && likesES2015 === undefined) {
+    return 'The student likes ES2015!';
+  }
+  return 'The student does not like much...';
 }
 
 export {
--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,12 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(firstArray, secondArray) {
+  let resultArray = [];
+  if (firstArray.length === 0) {
+    return secondArray;
+  }
+  const indexToBeInserted = firstArray.length / 2;
+  resultArray = firstArray.slice(0, indexToBeInserted).concat(secondArray).concat(firstArray.splice(indexToBeInserted, firstArray.length));
+  return resultArray;
 }
 
 export {
