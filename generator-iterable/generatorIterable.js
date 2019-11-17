cd8a171c68a50a06fb05ce1a1746aebdf32e45e9
Basant Soni <basantech89@gmail.com>
2019-11-17 23:04:19 +0530

    Fix generator-iterable test

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,6 +1,10 @@
 
-function generatorIterable(...args) {
-  return args;
+function* generatorIterable() {
+  let number = 1;
+  while (number <= 5) {
+    yield number;
+    number += 1;
+  }
 }
 
 export {
