98f3f32b74882d6a350e6547cdf700cc7475e260
hitesh <hiteshchauhan.669@gmail.com>
2019-11-17 20:17:05 +0530

    completed fibonaccit-iter exercie

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,8 +1,24 @@
-
-function fibonacciIter(...args) {
-  return args;
-}
-
-export {
-  fibonacciIter,
+const fibonacciIter = {
+  [Symbol.iterator]() {
+    let lastFib = 0;
+    let nextFib = 1;
+    let i = 0;
+    const iterator = {
+      next() {
+        if (i === 0) {
+          lastFib = 1;
+          i += 1;
+          return { value: lastFib, done: false };
+        }
+        i += 1;
+        const fibNumber = lastFib + nextFib;
+        lastFib = nextFib;
+        nextFib = fibNumber;
+        return { value: fibNumber, done: false };
+      },
+    };
+    return iterator;
+  },
 };
+
+export { fibonacciIter };
