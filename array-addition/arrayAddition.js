36c50f0632b4136ebdf620025b499618e89cdf92
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-17 22:49:54 +0530

    completed some d7s2 problems

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,6 +1,31 @@
+function arrayAddition(firstArray, secondArray) {
+  if (firstArray.length === 0 || secondArray.length === 0) {
+    throw new Error('Arrays should not be empty');
+  }
+  if (!(firstArray instanceof Array) || !(secondArray instanceof Array)) {
+    throw new Error('Passed args should be an Array');
+  }
 
-function arrayAddition(...args) {
-  return args;
+  const isNumber = (number) => {
+    if (typeof number === 'number') {
+      return true;
+    }
+    return false;
+  };
+
+  const addArrays = (first, second) => {
+    const result = first.reduce((acc, current) => {
+      if (!isNumber(current)) {
+        throw new Error('Array elements should be a number');
+      }
+      acc.push(current + second.shift());
+      return acc;
+    }, []);
+
+    return result.concat(second);
+  };
+
+  return firstArray.length > secondArray.length ? addArrays(secondArray, firstArray) : addArrays(firstArray, secondArray);
 }
 
 export {
--- object-assign/objectAssign.js
+++ object-assign/objectAssign.js
@@ -1,6 +1,7 @@
 
-function objectAssign(...args) {
-  return args;
+function objectAssign(targetObj, ...sourceObjects) {
+  sourceObjects.reduce((acc, current) => Object.assign(acc, current), targetObj);
+  return targetObj;
 }
 
 export {
