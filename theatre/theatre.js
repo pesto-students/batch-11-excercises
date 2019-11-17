1184b94a5097218d75bb4c6fbd6d558339c06886
hitesh <hiteshchauhan.669@gmail.com>
2019-11-17 15:01:54 +0530

    completed theatre exercise

--- theatre/theatre.js
+++ theatre/theatre.js
@@ -1,8 +1,37 @@
+const theatre = (list) => {
+  const currencyCount = {
+    10: 0,
+    20: 0,
+    50: 0,
+  };
 
-function theatre(...args) {
-  return args;
-}
-
-export {
-  theatre,
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i] === 10) {
+      currencyCount[10] += 1;
+    }
+    if (list[i] === 20) {
+      currencyCount[20] += 1;
+      if (currencyCount[10] >= 1) {
+        currencyCount[10] -= 1;
+      } else {
+        return false;
+      }
+    }
+    if (list[i] === 50) {
+      currencyCount[50] += 1;
+      if (currencyCount[20] >= 2) {
+        currencyCount[20] -= 2;
+      } else if (currencyCount[20] === 1 && currencyCount[10] >= 2) {
+        currencyCount[20] -= 1;
+        currencyCount[10] -= 2;
+      } else if (currencyCount[20] === 0 && currencyCount[10] >= 4) {
+        currencyCount[10] -= 4;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 };
+
+export { theatre };
