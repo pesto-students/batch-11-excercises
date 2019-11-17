e7b34622e7de63647fd2bce9428b5026bdd0b9a7
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-11-17 14:37:46 +0530

    Completed limitFunctionalCallCount with all test cases

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,14 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(callback, timesCalled) {
+  let countOfCalls = timesCalled;
+  return function (...args) {
+    if (countOfCalls > 0) {
+      countOfCalls--;
+      return callback(...args);
+    } else {
+      return null;
+    }
+  }
 }
 
 export {
