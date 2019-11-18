bd3f0a120e077458b4c1445e6866fbf2f942f217
Harish Rana <r.shivi@yahoo.com>
2019-11-18 08:52:30 +0530

    Complete map filter reduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,11 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(objects) {
+  return objects.map((object) => object.firstName)
+    .filter((name) => name.length < 5)
+    .reduce((accumulator, name) => {
+      accumulator[name] = name.length;
+      return accumulator;
+    }, {});
 }
 
 export {
