ea652cbc87f67f1a4200e497e2bb8704447c2946
yashpandit <yashpandit1995@gmail.com>
2019-11-19 00:02:14 +0530

    Add gif for question-5

--- question-5/index.js
+++ question-5/index.js
@@ -1,4 +1,7 @@
-const changeContent = (rowIndex, cellIndex, updateText) => {
+const changeContent = () => {
+  const rowIndex = document.querySelector("#row").value;
+  const cellIndex = document.querySelector("#cell").value;
+  const updateText = document.querySelector("#message").value;
   const table = document.querySelector("#myTable").rows;
   const row = table[rowIndex - 1];
   const cell = row.cells[cellIndex - 1];
