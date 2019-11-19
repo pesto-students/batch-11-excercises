a340fafb6d22602849c62590893b00711d757381
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-19 23:57:31 +0530

    Duplicate Letters

--- duplicate-letters/duplicateLetters.js
+++ duplicate-letters/duplicateLetters.js
@@ -1,8 +1,18 @@
+function duplicateLetters(stringVal) {
+  const stringArray = [...stringVal];
+  const object = {};
+  let count = 1;
 
-function duplicateLetters(...args) {
-  return args;
+  for (let i = 0; i < stringArray.length; i += 1) {
+    if (object[stringArray[i]] === undefined) {
+      object[stringArray[i]] = 1;
+    } else {
+      object[stringArray[i]] += 1;
+    }
+  }
+  const countArray = Object.values(object);
+  count = Math.max(...countArray);
+  return count > 1 ? count : false;
 }
 
-export {
-  duplicateLetters,
-};
+export { duplicateLetters };
