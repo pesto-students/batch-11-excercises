4da23ec8c06b5dfd96541b980e78e3214ffadb57
hitesh <hiteshchauhan.669@gmail.com>
2019-11-20 02:39:37 +0530

    completed balanced-braces exercises

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,8 +1,29 @@
+const balancedBraces = (string) => {
+  const openingBraces = ['{', '[', '('];
+  const closingBraces = ['}', ']', ')'];
+  const openingBracesStack = [];
+  const closingBracesQueue = [];
 
-function balancedBraces(...args) {
-  return args;
-}
-
-export {
-  balancedBraces,
+  for (let i = 0; i < string.length; i += 1) {
+    if (openingBraces.includes(string[i])) {
+      openingBracesStack.push(string[i]);
+    }
+    if (closingBraces.includes(string[i])) {
+      closingBracesQueue.push(string[i]);
+    }
+    if (
+      closingBraces.includes(string[i]) &&
+      closingBraces.indexOf(closingBracesQueue[0]) ===
+        openingBraces.indexOf(openingBracesStack[openingBracesStack.length - 1])
+    ) {
+      openingBracesStack.pop();
+      closingBracesQueue.shift();
+    }
+  }
+  if (openingBracesStack.length === 0 && closingBracesQueue.length === 0) {
+    return true;
+  }
+  return false;
 };
+
+export { balancedBraces };
