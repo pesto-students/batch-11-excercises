50dee4273b25c42693b7968f2209d56d5c23a6a5
Basant Soni <basantech89@gmail.com>
2019-11-20 00:46:21 +0530

    solved task 1 in intermediate-exercises

--- intermediate-exercises/src/components/01-LifeCycle.js
+++ intermediate-exercises/src/components/01-LifeCycle.js
@@ -1,44 +1,40 @@
 import React from 'react';
 
-/*
-  Tasks for this exercise are in comments inside the component class code.
-  In this exercise lifecycle methods will be used to provide convenient debug
-  messages in developer's console.
-  There are more lifecycle methods available.
-  Those three presented are commonly used.
- */
-
 class LifeCycle extends React.Component {
   constructor(props) {
     super(props);
     this.state = { name: 'Pesto' };
+    this.greet = this.greet.bind(this);
+    this.name = React.createRef();
   }
 
-  // This code will be called when the component finishes mounting
-  // (so it is visible for a user).
   componentDidMount() {
-    // Task 1: Display a message "I'm mounted!" in developer's console when the
-    //         component finishes mounting.
-    //         Use `console.log` function for it.
+    console.log("I'm mounted");
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Task 2: Display a message "Updated!" in developer's console
-    //         when the component updates.
-    //         Here you also need to use the console.log function.
-    //         Notice that in this lifecycle method you have an access
-    //         to previous values of properties and state.
-    console.log({ prevProps, prevState });
+    console.log('Updated!');
   }
 
   componentWillUnmount() {
-    // Task 3: Display a message "Good night, Pesto!" in developer's
-    //         console when the component unmounts.
+    console.log('Good night, Pesto!');
   }
 
-  // Task 4: Create a button which triggers ComponentDidUpdate lifecycle method on click.
+  greet(event) {
+    event.preventDefault();
+    this.setState({ name: this.name.current.value });
+  }
+
   render() {
-    return (<p>Good morning, {this.state.name}!</p>);
+    const { name } = this.state;
+    return (
+      <React.Fragment>
+        <p> Good morning, {name}! </p>
+        <label htmlFor="name"> Your Name </label>
+        <input type="text" name="name" id="name" ref={this.name} />
+        <button type="button" onClick={this.greet}> Greet </button>
+      </React.Fragment>
+    );
   }
 }
 
