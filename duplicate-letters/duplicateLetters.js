ec4ed879533cf9a4e7574ee686dd46aaf12a985a
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-19 19:10:24 +0530

    Exercise duplicate-letters done

--- duplicate-letters/duplicateLetters.js
+++ duplicate-letters/duplicateLetters.js
@@ -1,6 +1,20 @@
-
-function duplicateLetters(...args) {
-  return args;
+function duplicateLetters(letters) {
+  const lettersCount = {};
+  Object.values(letters).map((letter) => {
+    if (lettersCount[letter]) {
+      lettersCount[letter] += 1;
+    } else {
+      lettersCount[letter] = 1;
+    }
+    return lettersCount;
+  });
+  let maxCount = 1;
+  for (const letter in lettersCount) {
+    if (lettersCount[letter] > maxCount) {
+      maxCount = lettersCount[letter];
+    }
+  }
+  return maxCount > 1 ? maxCount : false;
 }
 
 export {
