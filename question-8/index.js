59133745c07d49d221442886f21b71e96a53e7c0
yashpandit <yashpandit1995@gmail.com>
2019-11-19 11:30:26 +0530

    Solve question-8

--- question-8/index.js
+++ question-8/index.js
@@ -1,7 +1,14 @@
 const getOptions = () => {
-  const dropDown = document.querySelector("#mySelect").selectedValue;
-  console.log(dropDown);
-  if (dropDown.length > 0) {
-    alert(`${dropDown.length}`);
+  const dropDown = document.querySelector("#mySelect");
+  const optionValues = [];
+
+  for (let i = 0; i < dropDown.length; i += 1) {
+    optionValues.push(dropDown.options[i].text);
   }
+
+  alert(
+    `The total count is: ${dropDown.length} and values are ${optionValues.join(
+      ","
+    )}`
+  );
 };
