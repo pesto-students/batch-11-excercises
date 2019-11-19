a56e57893ac8a459de0cad965b7ed64d334dcac9
hitesh <hiteshchauhan.669@gmail.com>
2019-11-19 23:07:59 +0530

    completed duplicate-letters exercise

--- duplicate-letters/duplicateLetters.js
+++ duplicate-letters/duplicateLetters.js
@@ -1,8 +1,18 @@
-
-function duplicateLetters(...args) {
-  return args;
-}
-
-export {
-  duplicateLetters,
+const duplicateLetters = (string) => {
+  const frequenceyStore = {};
+  for (const char of string) {
+    frequenceyStore[char] = frequenceyStore[char] + 1 || 1;
+  }
+  let maxDuplicateCount = 1;
+  for (const key in frequenceyStore) {
+    if (frequenceyStore[key] > maxDuplicateCount) {
+      maxDuplicateCount = frequenceyStore[key];
+    }
+  }
+  if (maxDuplicateCount === 1) {
+    return false;
+  }
+  return maxDuplicateCount;
 };
+
+export { duplicateLetters };
