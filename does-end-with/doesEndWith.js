d9784165e69e4e817950011e87f8be4c36f1a121
hitesh <hiteshchauhan.669@gmail.com>
2019-11-19 23:31:10 +0530

    completed does-end-with exercise

--- does-end-with/doesEndWith.js
+++ does-end-with/doesEndWith.js
@@ -1,8 +1,19 @@
+const doesEndWith = (value1, value2) => {
+  let subString = value1;
+  let string = value2;
+  if (Array.isArray(value1) && Array.isArray(value2)) {
+    subString = value1.join('');
+    string = value2.join('');
+  }
+  if (subString.length > string.length) {
+    return false;
+  }
 
-function doesEndWith(...args) {
-  return args;
-}
-
-export {
-  doesEndWith,
+  const endsOfString = string.slice(string.length - subString.length);
+  if (endsOfString === subString) {
+    return true;
+  }
+  return false;
 };
+
+export { doesEndWith };
