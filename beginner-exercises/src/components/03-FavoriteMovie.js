3a2e8d02c09bc2fb3f80e1a68cfc93ce4bfc8e72
Sourav Das <souravdasslg95@gmail.com>
2019-11-19 17:04:09 +0530

    Complete beginner exercise

--- beginner-exercises/src/components/03-FavoriteMovie.js
+++ beginner-exercises/src/components/03-FavoriteMovie.js
@@ -1,22 +1,4 @@
 import React, { Component } from 'react';
-
-/*
-You can pass props to components using JSX attributes.
-
-You have two tasks to complete in this exercise:
-Task #1: Someone left broken code in the `onMovieChange` method. It is always triggered
-  after changing the value of `movie` input. This method takes
-  event as its only argument. You need to retrieve the input value from
-  that object and update the `movie` field in the `state` object.
-Warning: Do not try to change the `render` structure.
-
-Task #2: If there is no movie given, we should display a message encouraging
-  the user to insert their favorite movie.
-  Display text: "Hey there. Enter your favorite movie." if movie.length is 0.
-
-Further reading on task #2: https://facebook.github.io/react/tips/if-else-in-JSX.html
-*/
-
 class FavoriteMovie extends Component {
   constructor(props) {
     super(props);
