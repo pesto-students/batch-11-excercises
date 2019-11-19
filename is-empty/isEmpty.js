b96fee6785a57219cff541e554d992b266b49255
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-20 00:24:35 +0530

    isEmpty solve

--- is-empty/isEmpty.js
+++ is-empty/isEmpty.js
@@ -1,5 +1,20 @@
-function isEmpty(...args) {
-  return Boolean(args);
+function isEmpty(args) {
+  if (args === "") {
+    return true;
+  }
+
+  if (Array.isArray(args) && args.length === 0) {
+    return true;
+  }
+
+  if (
+    typeof args === "object" &&
+    args !== null &&
+    Object.entries(args).length === 0
+  ) {
+    return true;
+  }
+  return false;
 }
 
 export { isEmpty };
