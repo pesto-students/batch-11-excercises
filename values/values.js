af3f39a531c89947e5d5f3748fd5fdab3d2a24a2
jatin33 <jatinpanjwani12@gmail.com>
2019-11-20 01:36:15 +0530

    Completed values problem with all test cases

--- values/values.js
+++ values/values.js
@@ -1,6 +1,12 @@
 
-function values(...args) {
-  return args;
+function values(object) {
+  const ownValues = [];
+  for (const property in object) {
+    if (Object.prototype.hasOwnProperty.call(object, property)) {
+      ownValues.push(object[property]);
+    }
+  }
+  return ownValues;
 }
 
 export {
