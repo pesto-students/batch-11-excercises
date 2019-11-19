0f54df0a83bd0ee4dff16e149b7118817f59f1a3
yashpandit <yashpandit1995@gmail.com>
2019-11-19 16:22:53 +0530

    Solve balancedBraces

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,8 +1,29 @@
+function balancedBraces(string) {
+  const brackets = [];
+  const endBrackets = [];
+  const openingBrackets = ["[", "{", "("];
+  const closingBrackets = ["]", "}", ")"];
 
-function balancedBraces(...args) {
-  return args;
+  for (let i = 0; i < string.length; i += 1) {
+    if (openingBrackets.includes(string[i])) {
+      brackets.push(string[i]);
+    }
+    if (closingBrackets.includes(string[i])) {
+      endBrackets.push(string[i]);
+    }
+    if (
+      closingBrackets.includes(string[i]) &&
+      closingBrackets.indexOf(endBrackets[0]) ===
+        openingBrackets.indexOf(brackets[brackets.length - 1])
+    ) {
+      brackets.pop();
+      endBrackets.shift();
+    }
+  }
+  if (brackets.length === 0 && endBrackets.length === 0) {
+    return true;
+  }
+  return false;
 }
 
-export {
-  balancedBraces,
-};
+export { balancedBraces };
