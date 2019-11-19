d449a86d25f661ab33437c3c4b258beaa5d6eb5d
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-20 00:15:46 +0530

    doesEndWith

--- does-end-with/doesEndWith.js
+++ does-end-with/doesEndWith.js
@@ -1,11 +1,21 @@
-function doesEndWith(lastLetter, stringVal) {
-  if (typeof lastLetter === "object" && typeof stringVal === "object") {
-    const stringArray = stringVal.join("");
-    const lastValues = lastLetter.join("");
-
-    return stringArray.endsWith(lastValues);
+function endsWith(source, compare) {
+  let count = source.length - 1;
+  let status = true;
+  for (let i = compare.length - 1; i >= 0; i -= 1) {
+    if (compare[i] !== source[count]) {
+      status = false;
+    } else {
+      count -= 1;
+    }
   }
-  return stringVal.endsWith(lastLetter);
+  return status;
+}
+
+function doesEndWith(compare, source) {
+  if (!Array.isArray(source)) {
+    return endsWith(source.split(""), compare.split(""));
+  }
+  return endsWith(source, compare);
 }
 
 export { doesEndWith };
