e5aed35cadf17fa800e173b901574417885124d3
yashpandit <yashpandit1995@gmail.com>
2019-11-19 23:14:03 +0530

    Solve isomorphic

--- isomorphic/isomorphic.js
+++ isomorphic/isomorphic.js
@@ -1,8 +1,36 @@
+/* eslint-disable no-console */
+function isomorphic(array) {
+  let [string1, string2] = [...array];
+  string1 = string1.split("");
+  string2 = string2.split("");
+  const frequency1 = {};
+  const frequency2 = {};
+  string1.forEach(letter => {
+    if (!frequency1[letter]) {
+      frequency1[letter] = 1;
+    } else {
+      frequency1[letter] += 1;
+    }
+  });
 
-function isomorphic(...args) {
-  return args;
+  string2.forEach(letter => {
+    if (!frequency2[letter]) {
+      frequency2[letter] = 1;
+    } else {
+      frequency2[letter] += 1;
+    }
+  });
+
+  const frequencyCount1 = Object.values(frequency1);
+  const frequencyCount2 = Object.values(frequency2);
+  let flag = true;
+  frequencyCount1.forEach((frequency, index) => {
+    if (frequency !== frequencyCount2[index]) {
+      flag = false;
+    }
+  });
+
+  return flag;
 }
 
-export {
-  isomorphic,
-};
+export { isomorphic };
