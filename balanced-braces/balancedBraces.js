0ee77e0632cf857f9b7392c9e56de9b09ed860e0
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-19 17:14:09 +0530

    Solved linting errors

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,65 +1,65 @@
 function isBraces(character) {
-    switch (character) {
-        case '(':
-        case '{':
-        case '[':
-        case ']':
-        case '}':
-        case ')':
-            return true;
-        default:
-            return false;
-    }
+  switch (character) {
+    case '(':
+    case '{':
+    case '[':
+    case ']':
+    case '}':
+    case ')':
+      return true;
+    default:
+      return false;
+  }
 }
 
 function isClosingBraces(character) {
-    switch (character) {
-        case ']':
-        case '}':
-        case ')':
-            return true;
-        default:
-            return false;
-    }
+  switch (character) {
+    case ']':
+    case '}':
+    case ')':
+      return true;
+    default:
+      return false;
+  }
 }
 
 function isMatchingBraces(character1, character2) {
-    let result;
-    switch (character2) {
-        case ']':
-            result = character1 === '[';
-            break;
-        case '}':
-            result = character1 === '{';
-            break;
-        case ')':
-            result = character1 === '(';
-            break;
-        default:
-            result = false;
-    }
-    return result;
+  let result;
+  switch (character2) {
+    case ']':
+      result = character1 === '[';
+      break;
+    case '}':
+      result = character1 === '{';
+      break;
+    case ')':
+      result = character1 === '(';
+      break;
+    default:
+      result = false;
+  }
+  return result;
 }
 
 function balancedBraces(stringContainingBraces) {
-    const bracesStack = [];
-    for (const character of stringContainingBraces) {
-        if (isBraces(character)) {
-            if (isClosingBraces(character)) {
-                if (isMatchingBraces(bracesStack.pop(), character) === false) {
-                    return false;
-                }
-            } else {
-                bracesStack.push(character);
-            }
+  const bracesStack = [];
+  for (const character of stringContainingBraces) {
+    if (isBraces(character)) {
+      if (isClosingBraces(character)) {
+        if (isMatchingBraces(bracesStack.pop(), character) === false) {
+          return false;
         }
+      } else {
+        bracesStack.push(character);
+      }
     }
-    if (bracesStack.length !== 0) {
-        return false;
-    }
-    return true;
+  }
+  if (bracesStack.length !== 0) {
+    return false;
+  }
+  return true;
 }
 
 export {
-    balancedBraces,
+  balancedBraces,
 };
