31b361f29f66e000d37dad78938c50e210687d3e
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-19 16:47:32 +0530

    Balance Braces Solve

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,8 +1,25 @@
+function balancedBraces(stringValue) {
+  const bracesArray = [...stringValue];
 
-function balancedBraces(...args) {
-  return args;
+  const insertArray = ["{", "[", "("];
+  const removeArray = ["}", "]", ")"];
+
+  const bracketsArray = [];
+
+  for (let i = 0; i < bracesArray.length; i += 1) {
+    if (insertArray.includes(bracesArray[i])) {
+      bracketsArray.push(bracesArray[i]);
+    } else if (
+      removeArray.indexOf(bracesArray[i]) ===
+      insertArray.indexOf(bracketsArray[bracketsArray.length - 1])
+    ) {
+      bracketsArray.pop([bracketsArray.length - 1]);
+    }
+  }
+  if (bracketsArray.length === 0) {
+    return true;
+  }
+  return false;
 }
 
-export {
-  balancedBraces,
-};
+export { balancedBraces };
