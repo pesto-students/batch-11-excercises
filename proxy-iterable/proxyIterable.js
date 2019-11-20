866b59056f2fd9c79031406a38a59bf1713111f7
hitesh <hiteshchauhan.669@gmail.com>
2019-11-20 22:57:47 +0530

    completed proxy-iterable exercise

--- proxy-iterable/proxyIterable.js
+++ proxy-iterable/proxyIterable.js
@@ -1,8 +1,22 @@
+function proxyIterable(array) {
+  const handler = {
+    get: (target, prop) => {
+      return function iterableFunction(...args) {
+        const result = [];
+        for (let i = 0; i < target.length; i += 1) {
+          if (typeof target[i][prop] !== 'function') {
+            throw Error(
+              `Item ${i + 1} of the iterable is missing the ${prop}() method`,
+            );
+          }
+          result.push(target[i][prop](...args));
+        }
+        return result;
+      };
+    },
+  };
 
-function proxyIterable(...args) {
-  return args;
+  return new Proxy(array, handler);
 }
 
-export {
-  proxyIterable,
-};
+export { proxyIterable };
