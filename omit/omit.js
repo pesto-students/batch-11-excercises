1e953f0ba0c45b384eb09046cfaac93e9f84074a
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-21 19:55:36 +0530

    exercise omit done

--- omit/omit.js
+++ omit/omit.js
@@ -1,6 +1,11 @@
-
-function omit(...args) {
-  return args;
+function omit(listedKeys, object) {
+  const result = {};
+  for (const key in object) {
+    if (listedKeys.indexOf(key) === -1) {
+      result[key] = object[key];
+    }
+  }
+  return result;
 }
 
 export {
