b90fa2bb269f4c53c8aea10542f2dd5d8f20d38c
Harish Rana <r.shivi@yahoo.com>
2019-11-21 08:48:05 +0530

    Solve Higher order component proplem

--- advance-exercises/src/components/03-HigherOrder.js
+++ advance-exercises/src/components/03-HigherOrder.js
@@ -1,33 +1,65 @@
-/*
-  Exercise:
-
-  Make `withMouse` a "higher-order component" that sends the mouse position
-  to the component as props (hint: use `event.clientX` and `event.clientY`).
-
-*/
+// eslint-disable-next-line import/no-unresolved
 import React from 'react';
 import PropTypes from 'prop-types';
 
 function withMouse(Component) {
-  return Component;
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        mouse: {
+          x: 0,
+          y: 0,
+        },
+      };
+      this.handleMouseEvent = this.handleMouseEvent.bind(this);
+    }
+
+    componentDidMount() {
+      // eslint-disable-next-line no-undef
+      window.document.addEventListener('mousemove', this.handleMouseEvent);
+    }
+
+    handleMouseEvent(event) {
+      this.setState({
+        mouse: {
+          x: event.clientX,
+          y: event.clientY,
+        },
+      });
+    }
+
+    render() {
+      const { mouse } = this.state;
+      return (
+        <Component
+          mouse={mouse}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...this.props}
+        />
+      );
+    }
+  };
 }
 
-class App extends React.Component {
-  render() {
-    const { mouse } = this.props;
-
-    return (
-      <div className="container">
-        {mouse ? (
-          <h1>
-            The mouse position is ({mouse.x}, {mouse.y})
-          </h1>
-        ) : (
-          <h1>We don&#39;t know the mouse position yet :(</h1>
-        )}
-      </div>
-    );
-  }
+function App(props) {
+  const { mouse } = props;
+  return (
+    <div className="container">
+      {mouse ? (
+        <h1>
+          The mouse position is (
+          {mouse.x}
+          ,
+          {' '}
+          {mouse.y}
+        )
+        </h1>
+      ) : (
+        <h1>We don&#39;t know the mouse position yet :(</h1>
+      )}
+    </div>
+  );
 }
 
 App.propTypes = {
