dada5bac7d93085f9146cb5d7e79d01360f00668
yashpandit <yashpandit1995@gmail.com>
2019-11-21 16:35:33 +0530

    Solve merge-all

--- merge-all/mergeAll.js
+++ merge-all/mergeAll.js
@@ -1,8 +1,17 @@
-
-function mergeAll(...args) {
-  return args;
+/* eslint-disable arrow-parens */
+function mergeAll(objects) {
+  const mergedObject = {};
+  objects.forEach(object => {
+    const keys = Object.keys(object);
+    for (const key of keys) {
+      if (!mergedObject[key]) {
+        mergedObject[key] = object[key];
+      } else {
+        mergedObject[key] = object[key];
+      }
+    }
+  });
+  return mergedObject;
 }
 
-export {
-  mergeAll,
-};
+export { mergeAll };
