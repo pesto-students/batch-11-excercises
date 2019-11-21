288e72a62fa935d87bd1715ca872b365ee156b99
Lordtien <nitinpawarme@gmail.com>
2019-11-21 16:42:37 +0530

    Solve merge-all
    Adding mergeAll.js containing solved version of merge-all problem
    and exercisesSubmission.json

--- merge-all/mergeAll.js
+++ merge-all/mergeAll.js
@@ -1,6 +1,13 @@
 
-function mergeAll(...args) {
-  return args;
+function mergeAll(objects) {
+  const mergedObject = objects.reduce((accumulator = {}, obj) => {
+    Object.keys(obj).map((key) => {
+      accumulator[key] = obj[key];
+      return true;
+    });
+    return accumulator;
+  });
+  return mergedObject;
 }
 
 export {
