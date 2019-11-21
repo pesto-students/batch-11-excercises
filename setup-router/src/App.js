1783ef6d6eb90b6d35bf7a4c708fbfd6387acfae
yashpandit <yashpandit1995@gmail.com>
2019-11-21 19:35:25 +0530

    Solve setup-router

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,20 +1,22 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-
-import './styles/App.css';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { Route, Switch } from "react-router-dom";
+import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       </Fragment>
     );
   }
