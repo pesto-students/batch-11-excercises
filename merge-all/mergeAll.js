23a252be51310db4db06b2183192c3106c70ec97
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-21 16:59:39 +0530

    Completed mergeall and omit

--- merge-all/mergeAll.js
+++ merge-all/mergeAll.js
@@ -1,6 +1,12 @@
 
-function mergeAll(...args) {
-  return args;
+function mergeAll(objects) {
+  const resultObject = objects.reduce((acc, currentObj) => {
+    for (const key of Object.keys(currentObj)) {
+      acc[key] = currentObj[key];
+    }
+    return acc;
+  }, {});
+  return resultObject;
 }
 
 export {
--- omit/omit.js
+++ omit/omit.js
@@ -1,6 +1,13 @@
 
-function omit(...args) {
-  return args;
+function omit(blackListedKeys, obj) {
+  const resultMap = {};
+  for (const key in obj) {
+    if (!blackListedKeys.includes(key)) {
+      resultMap[key] = obj[key];
+    }
+  }
+
+  return resultMap;
 }
 
 export {
