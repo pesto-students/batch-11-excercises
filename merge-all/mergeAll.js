c17e75916cf8a3a4495d8617ef424a25d0fbf8e2
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-21 16:47:56 +0530

    exercise merge-all solved

--- merge-all/mergeAll.js
+++ merge-all/mergeAll.js
@@ -1,6 +1,6 @@
 
-function mergeAll(...args) {
-  return args;
+function mergeAll(objects) {
+  return objects.reduce((accumulator, object) => Object.assign(accumulator, object), {});
 }
 
 export {
