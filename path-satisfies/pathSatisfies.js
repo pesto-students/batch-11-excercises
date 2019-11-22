ebc9c8bc9b9efac42f24027294c530f806a68bfc
hitesh <hiteshchauhan.669@gmail.com>
2019-11-22 17:19:56 +0530

    completed path-satisfy exercise

--- path-satisfies/pathSatisfies.js
+++ path-satisfies/pathSatisfies.js
@@ -1,8 +1,26 @@
+const pathSatisfies = (predicate, pathList, object) => {
+  let isPathSatisfy = false;
 
-function pathSatisfies(...args) {
-  return args;
-}
-
-export {
-  pathSatisfies,
+  for (let i = 0; i < pathList.length; i += 1) {
+    if (pathList.length === 0) {
+      return false;
+    }
+    const property = pathList[i];
+    if (property in object) {
+      if (typeof object[property] === 'object') {
+        return pathSatisfies(
+          predicate,
+          pathList.slice(i + 1),
+          object[property],
+        );
+      }
+      isPathSatisfy = predicate(object[property]);
+    }
+  }
+  return isPathSatisfy;
 };
+// const isPositive = (n) => n > 0;
+// console.log(
+//   pathSatisfies(isPositive, ['x', 1, 'y'], { x: [{ y: -1 }, { y: 1 }] }),
+// );
+export { pathSatisfies };
--- path-satisfies/pathSatisfies.test.js
+++ path-satisfies/pathSatisfies.test.js
@@ -1,10 +1,12 @@
 import { pathSatisfies } from './pathSatisfies';
 
 describe('pathSatisfies', () => {
-  const isPositive = n => n > 0;
+  const isPositive = (n) => n > 0;
 
   it('returns true if the specified object path satisfies the given predicate', () => {
-    expect(pathSatisfies(isPositive, ['x', 1, 'y'], { x: [{ y: -1 }, { y: 1 }] })).toBe(true);
+    expect(
+      pathSatisfies(isPositive, ['x', 1, 'y'], { x: [{ y: -1 }, { y: 1 }] }),
+    ).toBe(true);
   });
 
   it('returns false if the specified path does not exist', () => {
