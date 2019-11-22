c35234fc7ed877275b1410bb04b72d625da44f40
yashpandit <yashpandit1995@gmail.com>
2019-11-22 14:22:49 +0530

    Solve reject

--- reject/reject.js
+++ reject/reject.js
@@ -1,8 +1,12 @@
-
-function reject(...args) {
-  return args;
+function reject(predicate, values) {
+  if (!Array.isArray(values) || values.length === 0) {
+    return [];
+  }
+  const predicateFilter = values.filter(predicate);
+  const filteredValues = values.filter(
+    value => !predicateFilter.includes(value)
+  );
+  return filteredValues;
 }
 
-export {
-  reject,
-};
+export { reject };
