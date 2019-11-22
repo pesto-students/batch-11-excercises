158d9711cdac6c2296a2b792bce23686d09cb916
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-22 13:58:32 +0530

    Solved pathSatifies

--- path-satisfies/pathSatisfies.js
+++ path-satisfies/pathSatisfies.js
@@ -1,6 +1,14 @@
 
-function pathSatisfies(...args) {
-  return args;
+function pathSatisfies(predicate, path, object) {
+  return path.reduce((acc, key, index) => {
+    if (key in object) {
+      if (typeof object[key] === 'object') {
+        return pathSatisfies(predicate, path.slice(index + 1), object[key]);
+      }
+      return predicate(object[key]);
+    }
+    return acc === true;
+  }, false);
 }
 
 export {
