7c3bf2ceffa9a6cbc68a0215a8058ccbd42c927f
Lordtien <nitinpawarme@gmail.com>
2019-11-22 15:12:48 +0530

    Solve reject

--- reject/reject.js
+++ reject/reject.js
@@ -1,6 +1,21 @@
 
-function reject(...args) {
-  return args;
+const objectFilter = (obj, compliment) => {
+  const objKeys = obj.keys();
+  const filteredObj = {};
+  objKeys.forEach((key) => {
+    if (compliment(obj[key])) {
+      filteredObj[key] = obj[key];
+    }
+  });
+  return filteredObj;
+};
+
+function reject(transformer, obj) {
+  const compliment = (value) => !transformer(value);
+  if (Array.isArray(obj)) {
+    return obj.filter(compliment);
+  }
+  return objectFilter(obj, compliment);
 }
 
 export {
