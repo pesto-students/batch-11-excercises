ebb5a18ce523b7eee6f9614562a8a9cd11986537
Sourav Das <souravdasslg95@gmail.com>
2019-11-22 13:29:40 +0530

    Complete exercise named reject

--- reject/reject.js
+++ reject/reject.js
@@ -1,6 +1,15 @@
-
-function reject(...args) {
-  return args;
+const isObject = (element) => element && element.constructor.name === 'Object';
+function reject(transducerFn, collection) {
+  const inducedArray = [];
+  if (!Array.isArray(collection) && !isObject(collection)) return inducedArray;
+  // eslint-disable-next-line guard-for-in
+  for (const collectionKey in collection) {
+    const selectedElement = collection[collectionKey];
+    if (!transducerFn.call(this, selectedElement)) {
+      inducedArray.push(selectedElement);
+    }
+  }
+  return inducedArray;
 }
 
 export {
