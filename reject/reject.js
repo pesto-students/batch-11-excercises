1a1dd343e35499ba05b42e748bc53fb65310027c
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-22 14:31:41 +0530

    Solved reject

--- reject/reject.js
+++ reject/reject.js
@@ -1,6 +1,18 @@
 
-function reject(...args) {
-  return args;
+function reject(predicate, object) {
+  const entries = Object.entries(object);
+  const accumulator = entries.reduce((acc, entry) => {
+    const [key, value] = [...entry];
+    if (predicate(value) === false) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+
+  if (Array.isArray(object)) {
+    return Object.values(accumulator);
+  }
+  return accumulator;
 }
 
 export {
