248ed67b3bc38072d28d205823c467e0c21db681
Basant Soni <basantech89@gmail.com>
2019-11-22 15:05:26 +0530

    Fix test-basics

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -7,12 +7,20 @@ class App extends Component {
   state = {
     on: false,
     input: '',
+    btnText: 'No!',
     mainColor: 'blue',
+  };
+  handleStrings(text) {
+    return Boolean(text);
   }
   render() {
+    const { mainColor, btnText, input } = this.state;
     return (
       <div className="App">
-        Welcome to React
+        <h1 className={mainColor}>Welcome to React</h1>
+        <button className="button-state" onClick={() => this.setState({ btnText: 'Yes!' })}>{btnText}</button>
+        <h2>{input}</h2>
+        <input type="text" value={input} onChange={() => this.setState({ input: 'Pesto' })} />
       </div>
     );
   }
@@ -20,6 +28,10 @@ class App extends Component {
 
 export class Link extends Component {
   render() {
+    const { hide, address } = this.props;
+    if (!hide) {
+      return <a href={address}> Link </a>;
+    }
     return null;
   }
 }
