096b07a8cf52948e2cc12c11ec96fe70aa262ade
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-22 14:56:10 +0530

    exercise reject done

--- reject/reject.js
+++ reject/reject.js
@@ -1,6 +1,24 @@
+function reject(conditionForRejection, list) {
 
-function reject(...args) {
-  return args;
+
+  if (Array.isArray(list)) {
+    const filter = list.filter(conditionForRejection);
+    const rejectedArray = [];
+    return rejectedArray.concat(list.filter((ele) => filter.indexOf(ele) === -1));
+  }
+  if (typeof list === 'object') {
+    const rejectObject = {};
+    if (Object.keys(list).length === 0) {
+      return {};
+    }
+    for (const key in list) {
+
+      if (conditionForRejection(list[key]) === false) {
+        rejectObject[key] = list[key];
+      }
+    }
+    return rejectObject;
+  }
 }
 
 export {
--- reject/reject.test.js
+++ reject/reject.test.js
@@ -2,7 +2,7 @@ import { reject } from './reject';
 
 describe('4 reject', () => {
   const even = x => x % 2 === 0;
-
+  const isOdd = (n) => n % 2 !== 0;
   it('reduces an array to those not matching a filter', () => {
     expect(reject(even, [1, 2, 3, 4, 5])).toEqual([1, 3, 5]);
   });
@@ -21,5 +21,8 @@ describe('4 reject', () => {
 
   it('returns an empty array if asked to filter an empty array', () => {
     expect(reject(x => x > 100, [])).toEqual([]);
+  });
+  it('returns an empty array if asked to filter an empty array', () => {
+    expect(reject(isOdd, {a: 1, b: 2, c: 3, d: 4})).toEqual({b: 2, d: 4});
   });
 });
