e03ab1eba69f473aae4e13faddfbd2b279960678
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-22 22:38:49 +0530

    Exercise diffArray done

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,7 @@
-
-function diffArray(...args) {
-  return args;
+function diffArray(arrayA, arrayB) {
+  return arrayA
+    .concat(arrayB)
+    .filter((item) => !arrayA.includes(item) || !arrayB.includes(item));
 }
 
 export {
