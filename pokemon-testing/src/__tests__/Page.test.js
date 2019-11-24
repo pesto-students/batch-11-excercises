c9e981fa622a248f247a3819b17542a5a17f6604
Basant Soni <basantech89@gmail.com>
2019-11-25 01:27:50 +0530

    Add Integration Tests

--- pokemon-testing/src/__tests__/Page.test.js
+++ pokemon-testing/src/__tests__/Page.test.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import { shallow, mount } from 'enzyme';
 
 import Page from '../containers/Page/Page';
+import Search from '../components/Search';
 
 describe('<Page />', () => {
+  it('renders the Page component', () => {
+    const props = {
+      pageActions: {
+        filterPokemons: jest.fn(),
+        fetchPokemons: jest.fn(),
+      },
+      page: {
+        displayedPokemons: [],
+        isFetched: false,
+      },
+    }
+    const wrapper = shallow(<Page {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it('calls componentDidMount, fetch pokemons', () => {
     const props = {
       pageActions: {
@@ -20,14 +35,44 @@ describe('<Page />', () => {
     expect(Page.prototype.componentDidMount.mock.calls.length).toBe(1)
     expect(props.pageActions.fetchPokemons).toHaveBeenCalled();
   });
-  
-  it('calls filterPokemon on input change', () => {
+});
+
+describe('Integration Tests for Page and Search', () => {
+  test('should render Search component', () => {
     const props = {
       pageActions: {
         filterPokemons: jest.fn(),
+        fetchPokemons: jest.fn(),
+      },
+      page: {
+        displayedPokemons: [],
+        isFetched: false,
       },
     }
-    const wrapper = shallow(<Page {...props} />);
-    wrapper.find('input')
+    const wrapper = mount(<Page {...props} />);
+    expect(wrapper.find(Search).length).toEqual(1);
   });
-})
+
+  it('allow user to search for pokemons, calls filterPokemons', () => {
+    const filterPokemons = jest.fn();
+    const props = {
+      pageActions: {
+        filterPokemons,
+        fetchPokemons: jest.fn(),
+      },
+      page: {
+        displayedPokemons: [],
+        isFetched: false,
+      },
+    }
+
+    const component = mount(<Page {...props} />);
+    const searchComponent = component.find(Search);
+    // expect(searchComponent.find('input')).toHaveLength(0);
+    searchComponent.find('input').simulate('change', {
+      target: { value: 'pikachu' }
+    });
+    // expect(searchComponent.find('input').props().value).toEqual('pikachu');
+    expect(filterPokemons).toHaveBeenCalled();
+  });
+});
