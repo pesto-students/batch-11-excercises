828e9121287a84f74df63d35e719af6166392c3e
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-24 18:37:40 +0530

    Completed basic test

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -9,18 +9,33 @@ class App extends Component {
     input: '',
     mainColor: 'blue',
   }
+  onInputChange = event => this.setState({ input: event.target.value });
+
+  handleStrings = string => string !== '';
   render() {
+    const { on, input, mainColor } = this.state;
     return (
-      <div className="App">
-        Welcome to React
+      <div className={mainColor}>
+        <h1>Welcome to React</h1>
+        <button onClick={() => this.setState({ on: true })}>Change state</button>
+        <p className="button-state">{on ? 'Yes!' : 'No!'}</p>
+        <input type="text" onChange={event => this.onInputChange(event)} />
+        <h2>{input}</h2>
       </div>
     );
   }
 }
 
 export class Link extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return null;
+    const { hide, address } = this.props;
+    if (hide) {
+      return null;
+    }
+    return <a href={address}></a>;
   }
 }
 
