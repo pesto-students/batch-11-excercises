a8c37cddcf53469876fa3cebc86fadca75f2010e
yashpandit <yashpandit1995@gmail.com>
2019-11-25 19:28:03 +0530

    Commit WIP files

--- shortest-common-supersequence/shortestCommonSupersequence.js
+++ shortest-common-supersequence/shortestCommonSupersequence.js
@@ -1,8 +1,20 @@
+function shortestCommonSupersequence(firstSequence, secondSequence) {
+  const firstUniqueSequence = [];
+  const secondUniqueSequence = [];
 
-function shortestCommonSupersequence(...args) {
-  return args;
+  firstSequence.forEach(element => {
+    if (!firstUniqueSequence.includes(element)) {
+      firstUniqueSequence.push(element);
+    }
+  });
+
+  secondSequence.forEach(element => {
+    if (!secondUniqueSequence.includes(element)) {
+      secondUniqueSequence.push(element);
+    }
+  });
+
+  return firstUniqueSequence.concat(secondUniqueSequence);
 }
 
-export {
-  shortestCommonSupersequence,
-};
+export { shortestCommonSupersequence };
--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,8 +1,15 @@
-
-function updateObject(...args) {
-  return args;
+function updateObject(index, value, numbers) {
+  const proxy = new Proxy(numbers, {
+    get(target, prop) {
+      let property = prop;
+      if (property < 0) {
+        property += target.length;
+      }
+      return target[property];
+    }
+  });
+  proxy[index] = value;
+  return proxy;
 }
 
-export {
-  updateObject,
-};
+export { updateObject };
