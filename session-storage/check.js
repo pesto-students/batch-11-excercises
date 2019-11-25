de97fcecdeaed79b601810215d786b645e53415e
yashpandit <yashpandit1995@gmail.com>
2019-11-25 21:07:59 +0530

    Solve sessionStorage

--- session-storage/check.js
+++ session-storage/check.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
-let value = '';
+showSessionStorage();
 
-window.setInterval(() => {
-  if (value === '' || value === null) {
-    value = sessionStorage.getItem('world');
-  } else {
-    document.getElementsByClassName('status')[0].innerText = 'Value add successful';
+function addToSessionStorage() {
+  const key = document.querySelector("#key").value;
+  const value = document.querySelector("#value").value;
+  sessionStorage.setItem(key, value);
+  showSessionStorage();
+}
+
+function createLi() {
+  return document.createElement("li");
+}
+
+function showSessionStorage() {
+  const status = document.querySelector(".status");
+  status.innerHTML = "";
+  const ul = document.createElement("ul");
+  ul.class = "list";
+  status.appendChild(ul);
+  if (sessionStorage.length) {
+    for (const key of Object.keys(sessionStorage)) {
+      const li = createLi();
+      li.innerHTML = `Key: ${key}, Value: ${sessionStorage.getItem(key)}`;
+      ul.appendChild(li);
+      const button = document.createElement("button");
+      button.style.margin = "10px";
+      button.innerHTML = "X";
+      button.setAttribute("type", "button");
+      button.setAttribute("value", key);
+      button.onclick = function(event) {
+        const key = event.target.value;
+        sessionStorage.removeItem(key);
+        showSessionStorage();
+      };
+      li.appendChild(button);
+    }
   }
-}, 200);
-
+}
