c64dcb9715ae4f01673956a154f548c132b73724
Harish Rana <r.shivi@yahoo.com>
2019-11-25 15:21:43 +0530

    Solve last index of problem

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,6 +1,8 @@
 
-function lastIndexOf(...args) {
-  return args;
+function lastIndexOf(numberToFind, numbers) {
+  const numbersReversed = numbers.slice().reverse();
+  const indexFromBack = (numbers.length - 1) - numbersReversed.indexOf(numberToFind);
+  return indexFromBack >= numbers.length ? -1 : indexFromBack;
 }
 
 export {
