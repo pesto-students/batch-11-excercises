e6fdbf4044c9cccdae785856f40e158e81e53d50
hitesh <hiteshchauhan.669@gmail.com>
2019-11-25 15:31:10 +0530

    Completed last-index-of exercise

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,8 +1,11 @@
-
-function lastIndexOf(...args) {
-  return args;
-}
-
-export {
-  lastIndexOf,
+const lastIndexOf = (item, list) => {
+  let lastIndex = -1;
+  list.forEach((i, currentIndex) => {
+    if (i === item) {
+      lastIndex = currentIndex;
+    }
+  });
+  return lastIndex;
 };
+
+export { lastIndexOf };
