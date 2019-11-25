fa9f95d7af8e0475c781e6249421fa1a38b02850
Sourav Das <souravdasslg95@gmail.com>
2019-11-25 15:57:57 +0530

    Exercise named lastIndexOf is completed

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,6 +1,10 @@
 
-function lastIndexOf(...args) {
-  return args;
+function lastIndexOf(searchElement, array) {
+  if (!Array.isArray(array)) throw new Error(`Invalid Value Provided As Second Argument.Expected Array, Received ${typeof Array}`);
+  const lastIndex = array.length - 1;
+  const reversedArray = array.slice().reverse();
+  const elementFoundIn = reversedArray.indexOf(searchElement);
+  return elementFoundIn === -1 ? elementFoundIn : lastIndex - elementFoundIn;
 }
 
 export {
