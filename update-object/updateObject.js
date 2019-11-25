814492e4a315e00b9136e7735bc7470c8acc7863
hitesh <hiteshchauhan.669@gmail.com>
2019-11-25 20:49:52 +0530

    completed update-object exercise

--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,8 +1,27 @@
-
-function updateObject(...args) {
-  return args;
-}
-
-export {
-  updateObject,
+const modifier = (index, value, list) => {
+  let positiveIndex = index;
+  if (index < 0) {
+    positiveIndex = list.length + index;
+  }
+  if (positiveIndex > list.length - 1 || positiveIndex < 0) {
+    return list;
+  }
+  const updatedList = [...list];
+  updatedList[positiveIndex] = value;
+  return updatedList;
 };
+
+const curry = (main) => {
+  const store = [];
+  return function nextCall(...args) {
+    store.push(...args);
+    if (args.length >= main.length) {
+      return main(...args);
+    }
+    return (...next) => nextCall(...args, ...next);
+  };
+};
+
+const updateObject = curry(modifier);
+// console.log(updateObject(-5, 4, [0, 1, 2, 3]));
+export { updateObject };
