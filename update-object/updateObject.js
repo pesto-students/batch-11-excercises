98d687e6e994e9483a50afd73d0a61993aaf6b7d
yashpandit <yashpandit1995@gmail.com>
2019-11-25 22:00:08 +0530

    Solve updateObject

--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,15 +1,26 @@
-function updateObject(index, value, numbers) {
-  const proxy = new Proxy(numbers, {
-    get(target, prop) {
-      let property = prop;
-      if (property < 0) {
-        property += target.length;
-      }
-      return target[property];
-    }
-  });
-  proxy[index] = value;
-  return proxy;
+function updateObjectFunction(index, number, actualArray) {
+  const modifiedArray = [...actualArray];
+  let idx = index;
+  if (idx < 0) {
+    idx += actualArray.length;
+  }
+  if (idx >= 0 && idx < actualArray.length) {
+    modifiedArray[idx] = number;
+  }
+
+  return modifiedArray;
 }
+
+function curry(actualFunction) {
+  return function curried(...args) {
+    if (args.length >= actualFunction.length) {
+      return actualFunction.apply(null, args);
+    }
+    return (...remainingArgs) =>
+      curried.apply(null, args.concat(remainingArgs));
+  };
+}
+
+const updateObject = curry(updateObjectFunction);
 
 export { updateObject };
