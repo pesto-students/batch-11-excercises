8709028ab8047667ce83b2392daec2ef07b582cd
hitesh <hiteshchauhan.669@gmail.com>
2019-11-25 23:22:28 +0530

    remove comments from local-storage file

--- local-storage/localStorage.js
+++ local-storage/localStorage.js
@@ -1,12 +1,3 @@
-// function loggedInChecker() {
-//   const urlsJson = localStorage.getItem('bundle_urls');
-//   const urlsObject = JSON.parse(urlsJson);
-//   if ('site.css' in urlsObject) {
-//     return false;
-//   }
-//   return true;
-// }
-// loggedInChecker();
 const urlsJson = localStorage.getItem('bundle-urls');
 const urlsObject = JSON.parse(urlsJson);
 const notSignedIn = 'site.css' in urlsObject;
