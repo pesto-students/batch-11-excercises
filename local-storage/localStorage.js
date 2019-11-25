f90aa0636ad561fe45df0a5b4b6d6898e681fc99
yashpandit <yashpandit1995@gmail.com>
2019-11-25 19:41:45 +0530

    Solve localStorage

---
+++ local-storage/localStorage.js
@@ -0,0 +1,5 @@
+// I was not able to find logged_in flag inside localStorage
+// it was present in cookies so I fetched the bundle-urls flag
+// which was present inside the localStorage.
+
+localStorage.getItem("bundle-urls");
