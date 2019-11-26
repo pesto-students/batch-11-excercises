14d5aee1e8a2564e45bd5147c86b3d2c2b50cb56
Harish Rana <r.shivi@yahoo.com>
2019-11-26 22:34:37 +0530

    Solve convertToFileUrl problem

--- convert-to-file-url/convertToFileUrl.js
+++ convert-to-file-url/convertToFileUrl.js
@@ -1,6 +1,14 @@
+const path = require('path');
 
-function convertToFileUrl(...args) {
-  return args;
+function convertToFileUrl(pathString) {
+  const separatedPaths = pathString.split(path.sep);
+  const absolutePathPredecessor = (index) => (!path.isAbsolute(pathString) && index === 0 ? '/' : '');
+  const predicate = (acc, item, index) => `${acc}${absolutePathPredecessor(index)}${index > 0 ? '/' : ''}${encodeURIComponent(item)}`;
+
+  const a = separatedPaths
+    .filter((item) => item)
+    .reduce(predicate, 'file:///');
+  return a;
 }
 
 export {
