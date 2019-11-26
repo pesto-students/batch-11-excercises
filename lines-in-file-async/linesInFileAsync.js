0cb564ded53941d97d1feeeb7fb6cfd04403a87e
jatin33 <jatinpanjwani12@gmail.com>
2019-11-26 16:12:14 +0530

    Solved linesInFileAsync

--- lines-in-file-async/linesInFileAsync.js
+++ lines-in-file-async/linesInFileAsync.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
 
-function linesInFileAsync(...args) {
-  return args;
+function linesInFileAsync(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const newLinesCount = data.split('\n').length - 1;
+        resolve(newLinesCount);
+      }
+    });
+  });
 }
 
 export {
