f54d33c468e8fdfbefbb14c912e42b1701f625fb
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-26 23:43:40 +0530

    solved Exercise stripPath

--- strip-path/stripPath.js
+++ strip-path/stripPath.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
-function stripPath(originalPath, input) {
-  path.parse(originalPath);
-  const trimedPath = originalPath.replace(input, '');
-  return path.normalize(trimedPath);
+function stripPath(filepath, input) {
+  if (typeof filepath !== 'string') {
+    throw new TypeError(`Expected a string, got ${typeof filePath}`);
+  }
+  if (input === null) {
+    return filepath;
+  }
+  const pathName = path.normalize(filepath);
+  const stripWith = path.normalize(input);
+  let trimedPath = pathName.replace(stripWith, '');
+  if (trimedPath[0] === '/') {
+    trimedPath = trimedPath.substring(1);
+  }
+  return trimedPath;
 }
 
 export {
