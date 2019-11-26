50527396ba55803464df5b09e2c781111134f9dd
Harish Rana <r.shivi@yahoo.com>
2019-11-27 00:07:18 +0530

    Solve csc-to-json problem

--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -1,6 +1,25 @@
+const fs = require('fs');
 
-function csvToJson(...args) {
-  return args;
+function csvToJson(csvFile) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(csvFile, 'utf8', (err, data) => {
+      if (!err) {
+        const lines = data.split('\n');
+        const keys = lines[0].split(',');
+        const avengers = lines.slice(1);
+        const avengersObjects = avengers.map((avenger) => {
+          const avengerProperties = avenger.split(',');
+          return avengerProperties.reduce((acc, item, index) => {
+            acc[keys[index]] = item;
+            return acc;
+          }, {});
+        });
+        resolve(avengersObjects);
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
 
 export {
