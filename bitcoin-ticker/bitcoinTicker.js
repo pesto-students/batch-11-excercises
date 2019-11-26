79dac6ff55555e6422da432cfbc72633527da243
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-26 21:44:00 +0530

    Completed bitcoin ticker

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,6 +1,21 @@
+const https = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
+function bitcoinTicker({ limit }) {
+  const bitcoinPromise = new Promise((resolve, reject) => {
+    https.get(`https://api.coinmarketcap.com/v2/ticker/?limit=${limit}`, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    }).on('error', (err) => {
+      reject(err.message);
+    });
+  });
+
+  return bitcoinPromise;
 }
 
 export {
