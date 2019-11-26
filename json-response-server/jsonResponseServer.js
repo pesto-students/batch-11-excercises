2c5671716579e613421e1412062f1dbd130a9f93
Basant Soni <basantech89@gmail.com>
2019-11-26 15:14:51 +0530

    Fix json-response-server test

--- json-response-server/jsonResponseServer.js
+++ json-response-server/jsonResponseServer.js
@@ -1,7 +1,16 @@
+const http = require('http');
 
-function jsonResponseServer(...args) {
-  return args;
-}
+const jsonResponseServer = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ data: 'Pesto Bootcamp!' }));
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`Error 404: ${req.method} not supported`);
+  }
+});
 
 export {
   jsonResponseServer,
