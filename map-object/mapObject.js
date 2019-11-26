46227ca290abe1a06d2486c0a572b3ed1f273ac7
Lordtien <nitinpawarme@gmail.com>
2019-11-26 10:54:35 +0530

    Solve map-object

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,12 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(obj, mapper) {
+  const keys = Object.keys(obj);
+  const mappedObj = {};
+  keys.map((key) => {
+    mappedObj[key] = mapper(obj[key]);
+    return key;
+  });
+  return mappedObj;
 }
 
 export {
