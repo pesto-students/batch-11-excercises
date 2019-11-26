6e2fd17bdc834ea91ff330bbd3478f2599358176
RakeshUP <parthiban.rakesh95@gmail.com>
2019-11-26 22:43:46 +0530

    Solved csvToJSON

--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -1,6 +1,20 @@
+import { readFile } from 'fs';
 
-function csvToJson(...args) {
-  return args;
+function csvToJson(csvFile) {
+  return new Promise((resolve) => {
+    readFile(csvFile, (err, data) => {
+      const csvLines = data.toString().split('\n');
+      const [csvHeaders, ...csvData] = csvLines;
+      resolve(csvData.reduce((acc, line) => {
+        acc.push(line.split(',').reduce((object, value, index) => {
+          // eslint-disable-next-line no-param-reassign
+          object[csvHeaders.split(',')[index]] = value;
+          return object;
+        }, {}));
+        return acc;
+      }, []));
+    });
+  });
 }
 
 export {
