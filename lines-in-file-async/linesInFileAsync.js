80bc4a9ac0fae12b4be0f68f9d64bcb3696cd4dc
Basant Soni <basantech89@gmail.com>
2019-11-26 15:52:36 +0530

    Fix lines-in-file-async test

--- lines-in-file-async/linesInFileAsync.js
+++ lines-in-file-async/linesInFileAsync.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+const os = require('os');
 
-function linesInFileAsync(...args) {
-  return args;
+function linesInFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'UTF-8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.split(os.EOL).length - 1);
+      }
+    });
+  });
 }
 
 export {
