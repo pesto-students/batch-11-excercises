5eadc3d31c59e4617cf08b18a89960b10e445d2f
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-26 15:21:52 +0530

    Completed lines in file async

--- lines-in-file-async/linesInFileAsync.js
+++ lines-in-file-async/linesInFileAsync.js
@@ -1,6 +1,17 @@
+import fs from 'fs';
 
-function linesInFileAsync(...args) {
-  return args;
+function linesInFileAsync(filePath) {
+  const getLinesInString = (string) => string.split('\n').length - 1;
+  const filePromise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(getLinesInString(data.toString()));
+    });
+  });
+
+  return filePromise;
 }
 
 export {
