c9c58909741a07e0b98c91e5418906c993bd8e04
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-26 20:22:38 +0530

    filteredNamesRecursively solved

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,6 +1,18 @@
+const path = require('path');
+const fs = require('fs');
 
-function filteredNamesRecursive(...args) {
-  return args;
+function filteredNamesRecursive(directoryPath, fileExtension) {
+  const files = fs.readdirSync(directoryPath);
+  return files.reduce((accumalator, currentFile) => {
+    if (fs.statSync(path.join(directoryPath, currentFile)).isDirectory()) {
+      return accumalator.concat(filteredNamesRecursive(path
+        .join(directoryPath, currentFile), fileExtension));
+    }
+    if (path.extname(currentFile) === `.${fileExtension}`) {
+      return accumalator.concat(currentFile);
+    }
+    return accumalator;
+  }, []);
 }
 
 export {
