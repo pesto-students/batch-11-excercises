6b93085bc13a4dd0dd7f07504a9b6bf42b8b423f
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-26 16:02:21 +0530

    Completed filtered names recursive

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,6 +1,18 @@
+import fs from 'fs';
+import path from 'path';
 
-function filteredNamesRecursive(...args) {
-  return args;
+
+function filteredNamesRecursive(pathToDir, extension) {
+  const fileList = fs.readdirSync(pathToDir);
+
+  return fileList.reduce((acc, current) => {
+    if (current.endsWith(extension)) {
+      return acc.concat(current);
+    } if (fs.lstatSync(path.join(pathToDir, current)).isDirectory()) {
+      return acc.concat(filteredNamesRecursive(path.join(pathToDir, current), extension));
+    }
+    return acc;
+  }, []);
 }
 
 export {
