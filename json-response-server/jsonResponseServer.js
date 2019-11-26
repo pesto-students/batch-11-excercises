0fc03877fb883a602d3498880a45f14f1f1d8e99
Sourav Das <souravdasslg95@gmail.com>
2019-11-26 15:49:10 +0530

    Exercise named json-response-server is complete

--- json-response-server/jsonResponseServer.js
+++ json-response-server/jsonResponseServer.js
@@ -1,8 +1,13 @@
 
-function jsonResponseServer(...args) {
-  return args;
-}
+const http = require('http');
 
+const jsonResponseServer = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ data: 'Pesto Bootcamp!' }));
+  }
+});
 export {
   jsonResponseServer,
 };
