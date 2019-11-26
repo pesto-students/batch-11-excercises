e1e27b8eb5fad57438b22ab20a6ed91318ea842c
Basant Soni <basantech89@gmail.com>
2019-11-26 15:02:59 +0530

    Fix http-server test

--- http-server/httpServer.js
+++ http-server/httpServer.js
@@ -1,7 +1,17 @@
+const http = require('http');
 
-function httpServer(...args) {
-  return args;
-}
+const httpServer = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('Pesto Bootcamp!\n');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`Error 404: ${req.method} not supported`);
+  }
+});
 
 export {
   httpServer,
