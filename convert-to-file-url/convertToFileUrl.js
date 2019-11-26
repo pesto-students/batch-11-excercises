b7c07e67c002d4e532584886787106a8dc70109b
Basant Soni <basantech89@gmail.com>
2019-11-26 22:18:50 +0530

    Fix convert-to-file-url test

--- convert-to-file-url/convertToFileUrl.js
+++ convert-to-file-url/convertToFileUrl.js
@@ -1,6 +1,21 @@
+const path = require('path');
 
-function convertToFileUrl(...args) {
-  return args;
+function convertToFileUrl(file) {
+  const pathPattern = /\w(?=[/\\])/;
+  let fileUrl;
+  if (pathPattern.test(file)) {
+    fileUrl = `file://${file}`;
+  } else {
+    fileUrl = `file://${path.join(__dirname, file)}`;
+  }
+  return fileUrl.replace('&', '&amp;')
+    .replace('>', '&gt;')
+    .replace('<', '&lt;')
+    .replace('\'', '&#39;')
+    .replace('"', '&quot;')
+    .replace('^', '%5E')
+    .replace('?', '%3F')
+    .replace('#', '%23');
 }
 
 export {
