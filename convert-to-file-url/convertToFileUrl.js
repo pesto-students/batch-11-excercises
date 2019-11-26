238920197370ff4971f51a90a7b6cdd96152e8af
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-26 22:39:41 +0530

    completed csv to json

--- convert-to-file-url/convertToFileUrl.js
+++ convert-to-file-url/convertToFileUrl.js
@@ -1,6 +1,9 @@
+import url from 'url';
 
-function convertToFileUrl(...args) {
-  return args;
+function convertToFileUrl(path) {
+  console.log( encodeURI( url.pathToFileURL(path).toString()));
+
+  return encodeURI((url.pathToFileURL(decodeURI(path)).toString()));
 }
 
 export {
--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -1,6 +1,30 @@
+import fs from 'fs';
 
-function csvToJson(...args) {
-  return args;
+function csvToJson(filePath) {
+  const mapObject = (keys, values) => keys.reduce((acc, key, index) => {
+    acc[key] = values[index];
+    return acc;
+  }, {});
+
+  const filePromise = new Promise((resolve, reject) => {
+    fs.readFile((filePath), (error, data) => {
+      if (error) {
+        reject(error);
+      }
+      const lines = data.toString().split('\n');
+      const keys = lines[0].split(',');
+      const resultJson = lines.reduce((acc, line, index) => {
+        if (index !== 0) {
+          acc.push(mapObject(keys, line.split(',')));
+        }
+        return acc;
+      }, []);
+
+      resolve(resultJson);
+    });
+  });
+
+  return filePromise;
 }
 
 export {
