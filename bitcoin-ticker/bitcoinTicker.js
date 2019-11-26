18f92627c372679aac19555d4fca45a459769f43
hitesh <hiteshchauhan.669@gmail.com>
2019-11-26 22:21:35 +0530

    completed bitcoin-ticker exercise

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,8 +1,26 @@
+const https = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
-}
-
-export {
-  bitcoinTicker,
+const bitcoinTicker = ({ limit }) => {
+  const httpPromise = new Promise((resolve, reject) => {
+    https.get(
+      `https://api.coinmarketcap.com/v2/ticker/?limit=${limit}`,
+      (res) => {
+        let rawData = '';
+        res.on('data', (chunk) => {
+          rawData += chunk;
+        });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            resolve(parsedData);
+          } catch (e) {
+            reject(e.message);
+          }
+        });
+      },
+    );
+  });
+  return httpPromise;
 };
+
+export { bitcoinTicker };
