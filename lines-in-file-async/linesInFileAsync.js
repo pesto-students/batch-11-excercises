a3cc8b7e5b0c4a9fbd154518ab455214c1277ee5
Sourav Das <souravdasslg95@gmail.com>
2019-11-26 16:08:50 +0530

    Exercise named lines-in-file-async is complete

--- lines-in-file-async/linesInFileAsync.js
+++ lines-in-file-async/linesInFileAsync.js
@@ -1,8 +1,20 @@
+const fs = require('fs');
 
-function linesInFileAsync(...args) {
-  return args;
+const isNewLineChar = (char) => char === '\n' || char === '\r\n';
+
+function linesInFileAsync(fileURL) {
+  return new Promise((resolve, reject) => {
+    if (!fileURL) reject(new Error(`Please provide valid file path.Received ${fileURL}`));
+    fs.readFile(fileURL, 'utf8', (err, fileContent) => {
+      if (err) {
+        reject(err);
+      } else {
+        const chars = fileContent.split('');
+        const newLineChars = chars.filter(isNewLineChar);
+        resolve(newLineChars.length);
+      }
+    });
+  });
 }
 
-export {
-  linesInFileAsync,
-};
+export { linesInFileAsync };
