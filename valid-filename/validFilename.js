0b99f88d662137d1c4d3819b3e1bcd9ac2f9dcf4
hitesh <hiteshchauhan.669@gmail.com>
2019-11-26 14:39:17 +0530

    Completed valid-filename exercise

--- valid-filename/validFilename.js
+++ valid-filename/validFilename.js
@@ -1,8 +1,26 @@
-
-function validFilename(...args) {
-  return args;
-}
-
-export {
-  validFilename,
+const invalidSpecialChar = () => {
+  const reg = /[<>:"/\\|?*]/g;
+  return reg;
 };
+const windowReservedNames = () => {
+  const names = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])$/i;
+  return names;
+};
+
+const validFilename = (string) => {
+  if (!string || string.length > 255) {
+    return false;
+  }
+
+  if (invalidSpecialChar().test(string) || windowReservedNames().test(string)) {
+    return false;
+  }
+
+  if (/^\.\.?$/.test(string)) {
+    return false;
+  }
+
+  return true;
+};
+
+export { validFilename };
