707a8dab38c1d585085758f7d79ce0e72015da5c
hitesh <hiteshchauhan.669@gmail.com>
2019-11-27 15:37:22 +0530

    Completed the express-101 without tests

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-// 1) Create Routes
+app.get('/', (req, res) => {
+  res.send('Hey, I am server response');
+});
 
-// 2) Start server on port 3000
+app.get('/movie/:name', (req, res) => {
+  const movieName = req.params.name;
+  res.send(`My favorite movie is ${movieName}`);
+});
+
+app.listen(port, () => {
+  console.log(`I am listening on Port ${port}`);
+});
 
 module.exports = app;
