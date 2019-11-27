03ff683b45a3e1425e92bc669c713f47e5ad15a7
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-27 15:51:41 +0530

    Express router partial

--- express-router/router/index.js
+++ express-router/router/index.js
@@ -2,4 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
+const languages = [];
+router.get('/', (req, res) => res.render('index', { languages }));
+
+router.get('/new', (req, res) => res.render('new'));
+
+router.post('/', (req, res) => {
+  const { name } = req.body;
+  languages.push({ name, id: languages.length });
+  res.render('index', { languages });
+});
+
+router.get('/:id/edit', (req, res) => {
+  const { id } = req.params;
+  let language = {};
+  for (const lang of languages) {
+    if (lang.id === parseInt(id, 10)) {
+      language = lang;
+      break;
+    }
+  }
+  res.render('edit', { language });
+});
+
 module.exports = router;
