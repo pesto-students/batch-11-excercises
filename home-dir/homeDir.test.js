434d71d607d557ae04a35dfd23b7049beec23eb3
Harish Rana <r.shivi@yahoo.com>
2019-11-27 14:58:19 +0530

    Add test for homeDir problem

--- home-dir/homeDir.test.js
+++ home-dir/homeDir.test.js
@@ -1,6 +1,8 @@
 import { homeDir } from './homeDir';
 
-describe('Template Test', () => {
-  test('Template Test', () => {
+describe('Home directory', () => {
+  test('should be of length greater than zero', () => {
+    const dir = homeDir();
+    expect(dir.length).toBeGreaterThan(0);
   });
 });
