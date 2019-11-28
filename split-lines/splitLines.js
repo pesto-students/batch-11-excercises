17193ccd4887536482d4385cb16c8bdaaa619a34
Aditya Vyas <vyasaditya41@yahoo.in>
2019-11-28 15:55:16 +0530

    Solve Splitlines

--- split-lines/splitLines.js
+++ split-lines/splitLines.js
@@ -1,8 +1,25 @@
+const splitLines = (...args) => {
+  const [valueArray, newLine] = args;
+  const charArray = [];
+  if (newLine) {
+    let temp = 0;
+    for (let i = 0; i < valueArray.length; i += 1) {
+      if (valueArray[i] === `\n`) {
+        charArray.push(valueArray.slice(temp, i + 1));
+        temp = i + 1;
+      }
+    }
+    if (temp < valueArray.length) {
+      charArray.push(valueArray.slice(temp));
+    }
 
-function splitLines(...args) {
-  return args;
-}
+    if (valueArray.endsWith('\n')) {
+      charArray.push('');
+    }
+    return charArray;
+  }
 
-export {
-  splitLines,
+  return valueArray.replace(/(\r\n)|(\n)/g, ',').split(',');
 };
+
+export { splitLines };
