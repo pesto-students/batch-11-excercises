9446e789ff6b3789f27f22cce0a36a16d0c0879f
Sourav Das <souravdasslg95@gmail.com>
2019-11-28 10:26:04 +0530

    Exercise named express-form is complete

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', (req, res) => res.redirect('/get-form'));
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
-});
+app.get('/get-form', (req, res) => res.render('form-with-get'));
+app.get('/submit-form-with-get', (req, res) => res.send(req.query));
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
-});
-
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
-});
+app.get('/post-form', (req, res) => res.render('form-with-post'));
+app.post('/submit-form-with-post', (req, res) => res.send(req.body));
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
