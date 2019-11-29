c418c27dd9f4656e598842bc876da4fb4e407b5a
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-29 14:59:51 +0530

    completed titleize

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,9 @@
 
-function titleize(...args) {
-  return args;
+function titleize(string) {
+  const seperator = string.indexOf('-') !== -1 ? '-' : ' ';
+  const strings = string.split(/[\s-]/);
+
+  return strings.map((ele) => ele.charAt(0).toUpperCase() + ele.substring(1).toLowerCase()).join(seperator);
 }
 
 export {
