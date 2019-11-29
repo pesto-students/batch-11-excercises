cb8737541e51fd866422f9b9a82c46cc34a98879
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-29 14:09:25 +0530

    solved unique-random

--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,6 +1,14 @@
-
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(lowerBound, upperBound) {
+  let prevNumber;
+  const recursiveUniqueRandom = () => {
+    const uniqueNumber = Math.ceil(Math.random() * (upperBound - lowerBound)) + lowerBound;
+    if (uniqueNumber === prevNumber) {
+      return recursiveUniqueRandom();
+    }
+    prevNumber = uniqueNumber;
+    return uniqueNumber;
+  };
+  return recursiveUniqueRandom;
 }
 
 export {
