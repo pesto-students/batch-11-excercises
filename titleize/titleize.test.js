d899cf7cb255d3b7169421b5cfeb4acbec99d25b
jatin33 <jatinpanjwani12@gmail.com>
2019-11-29 15:09:11 +0530

    Added new test cases for titleoze

--- titleize/titleize.test.js
+++ titleize/titleize.test.js
@@ -8,5 +8,7 @@ describe('titleize', () => {
     expect(titleize('unicorns-and-rainbows')).toBe('Unicorns-And-Rainbows');
     expect(titleize('UNICORNS-AND-RAINBOWS')).toBe('Unicorns-And-Rainbows');
     expect(titleize('unicorns   and rainbows')).toBe('Unicorns   And Rainbows');
+    expect(titleize('Unicorns8 and 9rainbows')).toBe('Unicorns8 And 9Rainbows');
+    expect(titleize('unicorns" and rainbows')).toBe('Unicorns" And Rainbows');
   });
 });
