edb36276a938f0c031120f6ae3bf51fb5de9668b
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-29 08:48:50 +0530

    completed mongo basic one

--- basic-mongo-one/src/index.js
+++ basic-mongo-one/src/index.js
@@ -54,7 +54,7 @@ const positionalActor = async (db) => db.collection(movieDetails).find({
   and less than or equal to 9.2
 */
 const comparisonOperator = async (db) => db.collection(movieDetails).find({
-  'actors.1': 'Jackie Chan',
+  'imdb.rating': { $gte: 9.0, $lte: 9.2 },
 }).count();
 
 /* Q8 (*)
@@ -62,47 +62,61 @@ const comparisonOperator = async (db) => db.collection(movieDetails).find({
   being "UNRATED" or having no "rated" field at all
 */
 const trimUnrated = async (db) => db.collection(movieDetails).find({
-  $and: [{ 'imdb.rating': { $gt: 8.9 } }, { 'imdb.rating': { $lt: 9.3 } }],
+  $and: [{ rated: { $exists: true } }, { rated: { $ne: 'UNRATED' } }],
 }).count();
 
 /* Q9 (*)
   Return number of movies in which "tomato" field exists but "tomato.rating" does not
 */
-const unratedByTomato = async () => {};
+const unratedByTomato = async (db) => db.collection(movieDetails).find({
+  $and: [{ tomato: { $exists: true } }, { 'tomato.rating': { $exists: false } }],
+}).count();
 
 /* Q10 (*)
   Return number of movies with higher imdb rating >= 9.0 OR
   metacritic >= 90
 */
-const goodMovies = async () => {};
+const goodMovies = async (db) => db.collection(movieDetails).find({
+  $or: [{ 'imdb.rating': { $gte: 9.0 } }, { metacritic: { $gte: 90 } }],
+}).count();
 
 /* Q11 (*)
   Return number of movies where tomato field exists AND
   is equal to null
 */
-const regexSearch = async () => {};
+const regexSearch = async (db) => db.collection(movieDetails).findOne({
+  plot: /Master Yoda/,
+}, { projection: { _id: 0, title: 1 } });
 
 /* Q12 (*)
   Return number of movies where 'Adventure' and 'Action'
   as genres in any order
 */
-const arrayAll = async () => {};
+const arrayAll = async (db) => db.collection(movieDetails).find({
+  genres: { $all: ['Adventure', 'Action'] },
+}).count();
 
 /* Q13 (*)
   Return number of movies that were filmed in exactly 4 countries
 */
-const fieldArraySize = async () => {};
+const fieldArraySize = async (db) => db.collection(movieDetails).find({
+  countries: { $size: 4 },
+}).count();
 
 /* Q14 (*)
   Add a field called "myRating" = 90 to the movie "Iron Man 3" in movieDetails collection
 */
-const addField = async () => {};
+const addField = async (db) => db.collection(movieDetails).findOneAndUpdate({
+  title: 'Iron Man 3',
+}, { $set: { myRating: 88 } });
 
 /* Q15 (*)
   Increment the metacritic rating by 5 for the movie "Gone Girl" with a single query.
   Note: Do not use find() or findOne() to look for the current metacritic rating for "Gone Girl"
 */
-const incrementalUpdate = async () => {};
+const incrementalUpdate = async (db) => db.collection(movieDetails).findOneAndUpdate({
+  title: 'Gone Girl',
+}, { $inc: { metacritic: 5 } });
 
 module.exports = {
   getMoviesCount,
@@ -113,4 +127,11 @@ module.exports = {
   positionalActor,
   comparisonOperator,
   trimUnrated,
+  unratedByTomato,
+  goodMovies,
+  regexSearch,
+  arrayAll,
+  fieldArraySize,
+  addField,
+  incrementalUpdate,
 };
