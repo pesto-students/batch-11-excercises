febfaf83de86b30217a26317d98cb6ce17a17c95
Koushik Mohan <koushikmohan1996@gmail.com>
2019-11-29 10:57:42 +0530

    completed mongo two

--- basic-mongo-two/src/projectRouter.js
+++ basic-mongo-two/src/projectRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongo = require('mongodb');
 const { getDb } = require('./database');
 
+const ObjectId = mongo.ObjectID;
 const router = express.Router();
 let db;
 const connectDB = async () => {
@@ -12,6 +14,27 @@ const projects = 'projects';
 router.get('/', (req, res) => {
   const cursor = db.collection(projects).find({});
   cursor.toArray((err, result) => {
+    res.send(result);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.collection(projects).findOne({ _id: new ObjectId(id) }).then((result) => {
+    res.send(result);
+  });
+});
+
+router.post('/', (req, res) => {
+  const { project, developer } = req.body;
+  db.collection(projects).insertOne({ project, developer }).then((result) => {
+    res.send(result);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db.collection(projects).deleteOne({ _id: new ObjectId(id) }).then((result) => {
     res.send(result);
   });
 });
--- basic-mongo-two/src/server.js
+++ basic-mongo-two/src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = require('./projectRouter');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello world');
