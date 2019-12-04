5f652c53fbf199686883de6ce6fab5b09749fa48
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-04 10:45:26 +0530

    solved with all test cases

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,11 +1,12 @@
 function cacheFunction(fn) {
   const cache = {};
   return function cbFunction(...args) {
-    if (cache[args]) {
-      return cache[args];
+    const n = args[0];
+    if (n in cache) {
+      return cache[n];
     }
 
-    const result = fn.apply(this, args);
+    const result = fn(n);
     cache[args] = result;
 
     return result;
