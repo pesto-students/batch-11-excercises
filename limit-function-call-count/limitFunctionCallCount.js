61bb1b4ab344a45fd33f861bb201e3aade0f30d0
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-05 02:51:38 +0530

    Solve limit function call count

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,15 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(returnFunction, limit) {
+  let restriction = limit;
+
+  function limitFunction(...args) {
+    if (restriction) {
+      restriction -= 1;
+      return returnFunction(...args);
+    }
+    return null;
+  }
+  return limitFunction;
 }
 
 export {
