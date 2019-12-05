3cc63299c3abb59c65695783a6a4d788966f86e5
hitesh <hiteshchauhan.669@gmail.com>
2019-12-05 11:30:27 +0530

    Completed apply operator exercises

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,5 +1,33 @@
-function applyOperator(...args) {
-  return args;
-}
+const applyOperator = (...args) => {
+  const validOperator = ['+', '-', '*', '/', '%'];
+  const operator = args[0];
+  if (!validOperator.includes(operator)) {
+    throw new Error('Not a valid operator');
+  }
+  args.shift();
+  const list = [...args];
+  if (list.length === 0) {
+    return 0;
+  }
+  switch (operator) {
+    case '+':
+      return list.reduce((acc, val) => acc + val);
 
+    case '-':
+      return list.map((i) => -i).reduce((acc, val) => acc + val);
+
+    case '*':
+      return list.reduce((acc, val) => acc * val);
+
+    case '/':
+      return +list.reduce((acc, val) => acc / val).toFixed(4);
+
+    case '%':
+      return list.reduce((acc, val) => acc % val);
+    default:
+      throw new Error('No operator is provided');
+  }
+};
+
+// console.log(applyOperator('/', 1, 2, 3, 4, 5));
 export { applyOperator };
