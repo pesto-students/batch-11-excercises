cbc2b808f5b8f6631d8081dedd2d85ea80fefb69
Basant Soni <basantech89@gmail.com>
2019-12-06 02:33:29 +0530

    Fix reject test

--- reject/reject.js
+++ reject/reject.js
@@ -1,6 +1,8 @@
 
-function reject(...args) {
-  return args;
+function reject(transformer, list) {
+  const matches = list.filter(transformer);
+  const nonMatches = list.filter((item) => !matches.includes(item));
+  return nonMatches;
 }
 
 export {
