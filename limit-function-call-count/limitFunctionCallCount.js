90e2e7e9afdbc707d8dad04183e0e8bcce8bb815
RakeshUP <parthiban.rakesh95@gmail.com>
2019-12-05 11:42:20 +0530

    Solved 3 questions

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,13 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(callback, limit) {
+  return (...args) => {
+    if (limit > 0) {
+      // eslint-disable-next-line no-param-reassign
+      limit -= 1;
+      return callback(...args);
+    }
+    return null;
+  };
 }
 
 export {
