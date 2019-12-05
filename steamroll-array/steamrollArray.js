0c8a2b1bb7f35b23026ed4a3bbe526bf0934e7d2
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-05 20:41:20 +0530

    steamrollArray done

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,8 +1,13 @@
-
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(nestedArray) {
+  let flattendArray = [];
+  for (let i = 0; i < nestedArray.length; i += 1) {
+    if (Array.isArray(nestedArray[i])) {
+      flattendArray = flattendArray.concat(steamrollArray(nestedArray[i]));
+    } else {
+      flattendArray.push(nestedArray[i]);
+    }
+  }
+  return flattendArray;
 }
 
-export {
-  steamrollArray,
-};
+export { steamrollArray };
