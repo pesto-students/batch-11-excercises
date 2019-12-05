a2694030030646a7e4bf1b5f05cbbfdfbb8e5cc9
jatin33 <jatinpanjwani12@gmail.com>
2019-12-05 11:38:50 +0530

    Solved rotated strings and added new test cases

--- rotated-string/rotatedString.test.js
+++ rotated-string/rotatedString.test.js
@@ -3,9 +3,14 @@ import { rotatedString } from './rotatedString';
 describe('rotatedString', () => {
   it('rotatedString returns true for the given strings', () => {
     expect(rotatedString('abcde', 'cdeab')).toBeTruthy();
+    expect(rotatedString('', '')).toBeTruthy();
+    expect(rotatedString('123', '231'));
   });
 
   it('rotatedString returns false for the given strings', () => {
-    expect(rotatedString('abcde', 'abcde')).toBeFalsy();
+    expect(rotatedString('abcde', 'abdce')).toBeFalsy();
+    expect(rotatedString('abcd', 'abc')).toBeFalsy();
+    expect(rotatedString(123, 231)).toBeFalsy();
+    expect(rotatedString(123, '231')).toBeFalsy();
   });
 });
