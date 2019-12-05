de8ebf2b3b94460c4a441908ed4c238bdfe5237e
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-05 11:00:20 +0530

    convert to numera solved

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,6 +1,31 @@
+function convertToRoman(number) {
+  const decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const romanNumeral = [
+    'M',
+    'CM',
+    'D',
+    'CD',
+    'C',
+    'XC',
+    'L',
+    'XL',
+    'X',
+    'IX',
+    'V',
+    'IV',
+    'I',
+  ];
 
-function convertToRoman(...args) {
-  return args;
+  let numeral = '';
+  let digit = number;
+  for (let i = 0; i < decimalValue.length; i += 1) {
+    while (decimalValue[i] <= digit) {
+      numeral += romanNumeral[i];
+      digit -= decimalValue[i];
+    }
+  }
+
+  return numeral;
 }
 
 export {
