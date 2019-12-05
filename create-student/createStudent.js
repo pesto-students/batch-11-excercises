796b73450f058f07c39d12b07f943031d7f72d0c
yashpandit <yashpandit1995@gmail.com>
2019-12-05 21:17:40 +0530

    solve createStudent

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,15 +1,16 @@
 
-function createStudent({ likesES2015, likesJavaScript } = {}) {
-  if (!likesES2015) {
+function createStudent(...args) {
+  if (args.length === 0) {
+    return 'The student likes JavaScript and ES2015';
+  }
+  const { likesES2015, likesJavaScript } = args[0];
+  if (likesES2015 === false && likesJavaScript === undefined) {
     return 'The student likes JavaScript!';
   }
-  if (!likesJavaScript) {
+  if (likesJavaScript === false && likesES2015 === undefined) {
     return 'The student likes ES2015!';
   }
-  if (!likesES2015 && !likesJavaScript) {
-    return 'The student does not like much...';
-  }
-  return 'The student likes JavaScript and ES2015';
+  return 'The student does not like much...';
 }
 
 export {
