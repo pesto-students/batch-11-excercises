53ecab38fccc31517437eccaef50b0e94530b6dc
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-06 12:02:50 +0530

    Solve getIndexToIns

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,16 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(inputArray, numberToBeInserted) {
+  const sortedArray = inputArray.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
+  let indexToInsert = 0;
+  let index = 0;
+  for (index = 0; index < sortedArray.length; index += 1) {
+    if (sortedArray[index] >= numberToBeInserted) {
+      break;
+    }
+  }
+  indexToInsert = index;
+
+  return indexToInsert;
 }
 
 export {
