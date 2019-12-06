d76b766236db914c056e38b97a0717bb6a767eda
jatin33 <jatinpanjwani12@gmail.com>
2019-12-06 13:12:41 +0530

    Solved telephone check problem

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,7 @@
 
-function telephoneCheck(...args) {
-  return args;
+function telephoneCheck(phoneNumber) {
+  const validUSPhoneNumberPattern = /^(?:(?:\+?1\s*(?:[-]\s*)?)?(?:\(\s*([2-9][02-8][02-9])\s*\)|([2-9][02-8][02-9]))\s*(?:[-]\s*)?)?([2-9][02-9]{2})\s*(?:[-]\s*)?([0-9]{4})?$/gm;
+  return phoneNumber.length >= 10 && validUSPhoneNumberPattern.test(phoneNumber);
 }
 
 export {
