2c552568d991c50e0f0d43a185ec3d669977c0e2
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-06 10:32:25 +0530

    telephone check solved

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,7 @@
 
-function telephoneCheck(...args) {
-  return args;
+function telephoneCheck(string) {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(string);
 }
 
 export {
