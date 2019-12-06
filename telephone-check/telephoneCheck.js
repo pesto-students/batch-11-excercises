c39cb674c1829942b03b8996b842a70308d41c68
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-06 10:34:02 +0530

    telephone check solved

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,5 +1,6 @@
 
 function telephoneCheck(string) {
+  // eslint-disable-next-line no-useless-escape
   const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
   return regex.test(string);
 }
