eec86739cf13ca6fbaeb0710892aa1ae53cb4218
Harish Rana <r.shivi@yahoo.com>
2019-12-09 13:05:39 +0530

    feat: solve steamrollArray problem

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,14 @@
 
-function steamrollArray(...args) {
-  return args;
+function flatten(arrayItem) {
+  if (!(arrayItem instanceof Array)) {
+    return [arrayItem];
+  }
+
+  return arrayItem.reduce((acc, item) => [...acc, ...flatten(item)], []);
+}
+
+function steamrollArray(arr) {
+  return arr.reduce((acc, item) => [...acc, ...flatten(item)], []);
 }
 
 export {
