630c113f5305abd26a0cc419b2aff2a15b6b4251
Basant Soni <basantech89@gmail.com>
2019-12-10 18:07:33 +0530

    Fix negative-index test

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,30 @@
+const handler = {
+  get(target, index, receiver) {
+    if (typeof index === 'symbol') {
+      return Reflect.get(target, index, receiver);
+    }
+    if (index < 0) {
+      return Reflect.get(target, target.length - Math.abs(index), receiver);
+    }
+    return Reflect.get(target, index, receiver);
+  },
+  set(target, index, val, receiver) {
+    if (typeof index === 'symbol') {
+      return Reflect.set(target, index, val, receiver);
+    }
+    if (index < 0) {
+      return Reflect.set(target, target.length - Math.abs(index), val, receiver);
+    }
+    return Reflect.set(target, index, val, receiver);
+  },
+};
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(items) {
+  if (!Array.isArray(items)) {
+    throw new TypeError('/Only arrays are supported/');
+  }
+  const proxy = new Proxy(items, handler);
+  return proxy;
 }
 
 export {
