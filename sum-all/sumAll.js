dce894223a8b80d8abec9b81482d6f354b17e50c
Harish Rana <r.shivi@yahoo.com>
2019-12-10 12:06:11 +0530

    feat: solve sum all problem

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,15 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll([number1, number2]) {
+  const lowerBound = number1 > number2 ? number2 : number1;
+  const upperBound = number2 >= number1 ? number2 : number1;
+
+  let i = lowerBound;
+  let sum = 0;
+  while (i <= upperBound) {
+    sum += i;
+    i += 1;
+  }
+  return sum;
 }
 
 export {
