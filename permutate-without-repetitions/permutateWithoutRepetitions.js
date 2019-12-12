8b2a2d049d71fb2c6552222676433f42c03cc423
Harish Rana <r.shivi@yahoo.com>
2019-12-12 12:26:04 +0530

    solve permutate without repetition partially

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,6 +1,21 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
 
-function permutateWithoutRepetitions(...args) {
-  return args;
+function permutateWithoutRepetitions(arr) {
+  if (arr.length === 1) {
+    return [arr];
+  }
+  const permutedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+    const restPermutatedArray = permutateWithoutRepetitions(arr.slice(1));
+    restPermutatedArray.forEach((item) => {
+      permutedArray.push([arr[0], ...item]);
+    });
+  }
+  return permutedArray;
 }
 
 export {
