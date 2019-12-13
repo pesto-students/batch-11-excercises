de12f480a15e9ab1b8aebf4358cfa2516d3ff608
Basant Soni <basantech89@gmail.com>
2019-12-13 23:33:26 +0530

    Fix parse-ms test

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,7 +1,33 @@
 
-function parseMs(...args) {
-  return args;
+function parseMs(number) {
+  const positiveNumber = Math.abs(number);
+  const milliseconds = positiveNumber % (10 ** 3);
+  let seconds = Math.floor(positiveNumber / 10 ** 3);
+  let minutes = 0;
+  let hours = 0;
+  let days = 0;
+  if (seconds >= 60) {
+    minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+  }
+  if (minutes >= 60) {
+    hours = Math.floor(minutes / 60);
+    minutes %= 60;
+  }
+  if (hours >= 24) {
+    days = Math.floor(hours / 24);
+    hours %= 24;
+  }
+  if (number > 0) {
+    return {
+      days, hours, minutes, seconds, milliseconds,
+    };
+  }
+  return {
+    days: -days, hours: -hours, minutes: -minutes, seconds: -seconds, milliseconds: -milliseconds,
+  };
 }
+
 
 export {
   parseMs,
