6f1d1df553c04fd29fb025f3f296396a03f1fe51
Basant Soni <basantech89@gmail.com>
2019-12-15 19:49:26 +0530

    Fix add-big-integers test

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,27 @@
 
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(string) {
+  const splitted = string.split('\n');
+  const bigNumbers = [...splitted.slice(1)];
+  const reversedNumbers = bigNumbers.map((number) => number.split('').reverse().join(''));
+  const maxLength = reversedNumbers.reduce((len, number) => (
+    number.length > len ? number.length : len
+  ), 0);
+
+  let i = 0;
+  let addition = ''; let carry = 0;
+  while (i < maxLength) {
+    // eslint-disable-next-line no-loop-func
+    const sumOfDigits = reversedNumbers.reduce((sum, number) => (
+      number[i] ? sum + Number(number[i]) : sum
+    ), 0);
+    const stringSum = (sumOfDigits + carry).toString(10);
+    carry = Number(stringSum.substring(0, stringSum.length - 1));
+    addition += stringSum.substring(stringSum.length - 1);
+    i += 1;
+  }
+
+  const result = addition.split('').reverse().join('');
+  return carry === 0 ? result : carry + result;
 }
 
 export {
