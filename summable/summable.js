79eb254cf0bfdde004f5435239f8618c2791e752
Harish Rana <r.shivi@yahoo.com>
2019-12-19 21:32:27 +0530

    feat: Solve summable

--- summable/summable.js
+++ summable/summable.js
@@ -1,6 +1,16 @@
 
-function summable(...args) {
-  return args;
+function summable({ target, array }) {
+  const filteredArray = array.filter((item) => item < target);
+  const isSubsetSum = (arr1, n, sum) => {
+    if (sum === 0) {
+      return true;
+    }
+    if (n === 0 && sum !== 0) {
+      return false;
+    }
+    return isSubsetSum(arr1, n - 1, sum) || isSubsetSum(arr1, n - 1, sum - arr1[n - 1]);
+  };
+  return isSubsetSum(filteredArray, filteredArray.length, target);
 }
 
 export {
