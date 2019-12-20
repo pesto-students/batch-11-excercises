489f61ee0bb8b0f95e837087c9e1bf72e9591580
Harish Rana <r.shivi@yahoo.com>
2019-12-21 01:11:39 +0530

    feat: Solve primality test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,18 @@
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(num) {
+  const abstractNumber = Math.abs(num);
+
+  if (abstractNumber === 1 || abstractNumber === 0) {
+    return false;
+  }
+  let i = 2;
+  while (i < abstractNumber) {
+    if (abstractNumber % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+  return true;
 }
 
 export {
