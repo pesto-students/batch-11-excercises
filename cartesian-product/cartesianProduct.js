4e71e56b365965f276e73588c6a47a310975688d
Basant Soni <basantech89@gmail.com>
2019-12-21 22:59:59 +0530

    Fix cartesian-product test

--- cartesian-product/cartesianProduct.js
+++ cartesian-product/cartesianProduct.js
@@ -1,6 +1,11 @@
-
-function cartesianProduct(...args) {
-  return args;
+function cartesianProduct(product1, product2) {
+  if (!product1 || !product2) {
+    return null;
+  }
+  return product1.reduce((result, item1) => {
+    product2.forEach((item2) => result.push([item1, item2]));
+    return result;
+  }, []);
 }
 
 export {
