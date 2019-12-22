f8a8f0c74c0ec7f7cbd530f4142978aae6451623
Basant Soni <basantech89@gmail.com>
2019-12-22 22:07:03 +0530

    Fix does-end-with test

--- does-end-with/doesEndWith.js
+++ does-end-with/doesEndWith.js
@@ -1,6 +1,8 @@
 
-function doesEndWith(...args) {
-  return args;
+function doesEndWith(item1, item2) {
+  const str1 = Array.isArray(item1) ? item1.join('') : item1;
+  const str2 = Array.isArray(item2) ? item2.join('') : item2;
+  return str2.endsWith(str1);
 }
 
 export {
