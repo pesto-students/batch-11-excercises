7b4d1be89e5effe3ad342515bea1813abbc06e1b
Harish Rana <r.shivi@yahoo.com>
2019-12-22 23:48:30 +0530

    feat: Solve sumEvenArgs

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,8 @@
 
 function sumEvenArgs(...args) {
-  return args;
+  const arrOfArgs = Array.from(args);
+  console.log(arrOfArgs);
+  return arrOfArgs.filter(item => item % 2 === 0).reduce((acc, item) => acc + item, 0);
 }
 
 export {
